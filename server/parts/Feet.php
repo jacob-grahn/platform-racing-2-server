778dab2c439accfbdfbc51eaf798961d7b28d2d4
Ben <bls1999@users.noreply.github.com>
2018-01-05 04:42:29 -0500

    Fixed spacing

--- server/parts/Feet.php
+++ server/parts/Feet.php
@@ -32,12 +32,12 @@ class Feet {
 	const BUBBLE = 28;
 	const TACO = 29;
 	const SLENDER = 30;
-	const Santa = 34;
-	const Frost Djinn = 35;
-	const Reindeer = 36;
-	const Crocodile = 37;
-	const Valentine = 38;
-	const Bunny = 39;
+	const SANTA = 34;
+	const FROST_DJINN = 35;
+	const REINDEER = 36;
+	const CROCODILE = 37;
+	const VALENTINE = 38;
+	const BUNNY = 39;
 	
 }
 
