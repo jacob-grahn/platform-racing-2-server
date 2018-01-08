c8cf735b3a7c4aec6fc48c4750a19cda93134304
Jacob Grahn <contact@jacobgrahn.com>
2018-01-08 10:02:00 -0500

    add files

---
+++ env.example.php
@@ -0,0 +1,14 @@
+<?php
+
+$DB_ADDRESS = 'localhost';
+$DB_PASS = '123';
+$DB_USER = 'user';
+$DB_NAME = 'pr2';
+$DB_PORT = 1234;
+
+$PASS_SALT = 'lala';
+
+$PROCESS_PASS = 'abc';
+$COMM_PASS = 'def';
+
+?>
