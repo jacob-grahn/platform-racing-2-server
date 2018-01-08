a2a7c678310c62e1c2c4547e19cd9dfbda660d64
Ben <bls1999@users.noreply.github.com>
2018-01-08 17:25:38 -0500

    Update demod.php

--- server/commands/demod.php
+++ server/commands/demod.php
@@ -5,6 +5,15 @@ require_once(__DIR__ . '/../fns/db_fns.php');
 
 $port = $argv[1];
 $user_name = $argv[2];
+$admin = $argv[3]; // player passed as argument for use with confirmation/error messages
+$caught_exception = false;
+
+// if the user isn't an admin, kill the script
+if($admin->group != 3) {
+	echo $admin->name." lacks the power to demote. Quitting...";
+	$admin->write("message`Error: You lack the power to demote $user_name.");
+	exit;
+}
 
 try {
 	$connection = user_connect();
@@ -16,7 +25,7 @@ try {
 	$result = $connection->query("DELETE FROM mod_power
 									WHERE user_id = '$safe_user_id'");
 	if(!$result) {
-		throw new Exception('could not delete mod row');
+		throw new Exception('Could not delete the moderator type from database.');
 	}
 
 
@@ -25,14 +34,23 @@ try {
 									SET power = 1
 									WHERE user_id = '$safe_user_id'");
 	if(!$result) {
-		throw new Exception('Could not set user power');
+		throw new Exception("Could not demote $user_name to a member.");
 	}
 }
 
 catch(Exception $e){
+	$caught_exception = true;
 	$message = $e->getMessage();
 	echo $message;
+	$admin->write("message`Error: $message");
 	exit;
 }
+
+if(!$caught_exception) {
+	$admin->write("message`$user_name has been successfully demoted to a member.");
+	exit;
+}
+
+exit;
 
 ?>
