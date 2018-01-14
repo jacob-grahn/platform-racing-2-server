f7d21d9c7827e85d65573d327853bbf263bbf5d9
Ben <bls1999@users.noreply.github.com>
2018-01-14 17:18:11 -0500

    Update demod.php

--- server/commands/demod.php
+++ server/commands/demod.php
@@ -1,43 +1,6 @@
 #!/usr/bin/php
 <?php
 
-<<<<<<< HEAD
-require_once('/home/jiggmin/pr2/server/fns/db_fns.php');
-
-$port = $argv[1];
-$user_name = $argv[2];
-
-try {
-	$connection = user_connect();
-	$user_id = name_to_id($connection, $user_name);
-	$safe_user_id = addslashes($user_id);
-	
-	
-	//delete mod entry
-	$result = $connection->query("DELETE FROM mod_power
-									WHERE user_id = '$safe_user_id'");
-	if(!$result) {
-		throw new Exception('could not delete mod row');
-	}
-	
-	
-	//set power to 1
-	$result = $connection->query("UPDATE users
-									SET power = 1
-									WHERE user_id = '$safe_user_id'");
-	if(!$result) {
-		throw new Exception('Could not set user power');
-	}
-}
-
-catch(Exception $e){
-	$message = $e->getMessage();
-	echo $message;
-	exit;
-}
-
-?>
-=======
 require_once(__DIR__ . '/../fns/db_fns.php');
 
 function demote_mod($port, $user_name, $admin, $demoted_player) {
@@ -110,4 +73,3 @@ function demote_mod($port, $user_name, $admin, $demoted_player) {
 }
 
 ?>
->>>>>>> shell-fix-prodemote
