16250a25e79b7ec1365da529fc5d54df769c293c
Jacob Grahn <contact@jacobgrahn.com>
2018-01-17 12:37:46 -0500

    split no longer exists in php7

--- http_server/www/login.php
+++ http_server/www/login.php
@@ -185,10 +185,10 @@ Thanks for playing, I hope you enjoy.
 	}
 
 
-	$hat_array = split( ',', $stats->hat_array );
-	$head_array = split( ',', $stats->head_array );
-	$body_array = split( ',', $stats->body_array );
-	$feet_array = split( ',', $stats->feet_array );
+	$hat_array = explode( ',', $stats->hat_array );
+	$head_array = explode( ',', $stats->head_array );
+	$body_array = explode( ',', $stats->body_array );
+	$feet_array = explode( ',', $stats->feet_array );
 
 
 	//--- santa set
