a4a3fc36929f8a3c58905589e892bd78dda6fca3
Ben <bls1999@users.noreply.github.com>
2018-01-17 18:29:05 -0500

    Reflect changes to index.php in output_header()
    If the page is index.php, the server will show the `above_game_ad`.

--- http_server/fns/output_fns.php
+++ http_server/fns/output_fns.php
@@ -10,9 +10,14 @@ function output_header($title='', $mod=false) {
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>PR2 Hub - <?php echo $title; ?></title>
     <link href="//pr2hub.com/pr2hub.css" rel="stylesheet" type="text/css"/>
+	<?php if(basename($_SERVER['PHP_SELF']) == "index.php") { ?>
+		<link rel="preload" href="https://adservice.google.com/adsid/integrator.js?domain=pr2hub.com" as="script">
+		<script src="https://pagead2.googlesyndication.com/pub-config/r20160913/ca-pub-1320797949858618.js"></script>
+		<script type="text/javascript" src="https://adservice.google.com/adsid/integrator.js?domain=pr2hub.com"></script>
+	<?php } ?>
 	<?php if($mod) { ?>
-	<script src="https://code.jquery.com/jquery-latest.min.js"></script>
-	<script src="https://malsup.github.com/jquery.form.js"></script>
+		<script src="https://code.jquery.com/jquery-latest.min.js"></script>
+		<script src="https://malsup.github.com/jquery.form.js"></script>
 	<?php } ?>
 </head>
 
@@ -24,6 +29,24 @@ function output_header($title='', $mod=false) {
 	</div>
 
 	<div id="body">
+			<?php if(basename($_SERVER['PHP_SELF']) == "index.php") { ?>
+				<div class="above_game_ad">
+					<script type="text/javascript"><!--
+						google_ad_client = "ca-pub-1320797949858618";
+						/* Above Game */
+						google_ad_slot = "8735302549";
+						google_ad_width = 728;
+						google_ad_height = 90;
+						//-->
+					</script>
+					<script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+					<ins id="aswift_0_expand" style="display:inline-table;border:none;height:90px;margin:0;padding:0;position:relative;visibility:visible;width:728px;background-color:transparent;">
+						<ins id="aswift_0_anchor" style="display:block;border:none;height:90px;margin:0;padding:0;position:relative;visibility:visible;width:728px;background-color:transparent;">
+							<iframe width="728" height="90" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" allowfullscreen="true" onload="var i=this.id,s=window.google_iframe_oncopy,H=s&amp;&amp;s.handlers,h=H&amp;&amp;H[i],w=this.contentWindow,d;try{d=w.document}catch(e){}if(h&amp;&amp;d&amp;&amp;(!d.body||!d.body.firstChild)){if(h.call){setTimeout(h,0)}else if(h.match){try{h=s.upd(h,i)}catch(e){}w.location.replace(h)}}" id="aswift_0" name="aswift_0" style="left:0;position:absolute;top:0;width:728px;height:90px;"></iframe>
+						</ins>
+					</ins>
+				</div>
+			<?php } ?>
 			<div class="content">
 
 <?php
