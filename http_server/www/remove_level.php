2b4bb5f1f912c72accb432c9929ae8b327a9014c
Ben <bls1999@users.noreply.github.com>
2018-01-18 15:00:54 -0500

    Add remove_level.php to mod action log

--- http_server/www/remove_level.php
+++ http_server/www/remove_level.php
@@ -3,6 +3,8 @@
 require_once('../fns/all_fns.php');
 
 $level_id = find('level_id', 'none');
+$ip = get_ip();
+$user_id = $mod->user_id;
 
 try{	
 	//error check
@@ -22,6 +24,13 @@ try{
 	
 	//unpublish the level
 	$db->call('level_unpublish', array($level_id));
+	
+	//htmlspecialchars
+	$html_name = htmlspecialchars($mod->name);
+	$html_level_id = htmlspecialchars($level_id);
+	
+	//record the change
+	$db->call('mod_action_insert', array($user_id, "$html_name unpublished level $html_level_id from $ip.", $user_id, $ip));
 	
 	//tell it to the world
 	echo 'message=This level has been removed successfully. It may take up to 60 seconds for this change to take effect.';
