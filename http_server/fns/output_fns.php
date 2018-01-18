9d57344b65bb57a4fe216a826b409be7e969cfab
Ben <bls1999@users.noreply.github.com>
2018-01-18 12:08:30 -0500

    Update output_fns.php
    Fix footer links, add output_mod_navigation() to this page instead of individual pages. (removal of individual pages' mod nav is coming in a future pull)

--- http_server/fns/output_fns.php
+++ http_server/fns/output_fns.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once('../../fns/all_fns.php');
+
+$formatting_for_mods = check_moderator($db, false);
+
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function output_header($title='', $mod=false) {
 ?>
@@ -10,9 +14,9 @@ function output_header($title='', $mod=false) {
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>PR2 Hub - <?php echo $title; ?></title>
     <link href="//pr2hub.com/pr2hub.css" rel="stylesheet" type="text/css"/>
-	<?php if($mod) { ?>
-	<script src="https://code.jquery.com/jquery-latest.min.js"></script>
-	<script src="https://malsup.github.com/jquery.form.js"></script>
+	<?php if($formatting_for_mods) { ?>
+		<script src="https://code.jquery.com/jquery-latest.min.js"></script>
+		<script src="https://malsup.github.com/jquery.form.js"></script>
 	<?php } ?>
 </head>
 
@@ -24,10 +28,27 @@ function output_header($title='', $mod=false) {
 	</div>
 
 	<div id="body">
+			<?php if(basename($_SERVER['PHP_SELF']) == "index.php") { ?>
+				<div class="above_game_ad">
+					<script type="text/javascript"><!--
+						google_ad_client = "ca-pub-1320797949858618";
+						/* Above Game */
+						google_ad_slot = "8735302549";
+						google_ad_width = 728;
+						google_ad_height = 90;
+						//-->
+					</script>
+					<script type="text/javascript"
+					src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+					</script>
+				</div>
+			<?php } ?>
 			<div class="content">
 
 <?php
-
+	if($formatting_for_mods) {
+		output_mod_navigation();
+	}
 }
 
 
@@ -47,9 +68,9 @@ function output_footer() {
 	<div id="footer">
 		<ul class="footer_links">
 			<li><a href="//pr2hub.com/backups">Backups</a></li>
-			<li><a href="//jiggmin.com/threads/6330-What-is-Team-Jiggmin">f@h</a></li>
-			<li><a href="//jiggmin.com">Jiggmin.com</a></li>
-			<li><a href="//jiggmin.com/privacy-policy.html">Privacy Policy</a></li>
+			<li><a href="https://jiggmin2.com/forums/showthread.php?tid=19">Folding@Home</a></li>
+			<li><a href="//pr2hub.com/terms-of-use.php">Terms of Use</a></li>
+			<li><a href="https://jiggmin2.com/forums/showthread.php?tid=385">Rules</a></li>
 		</ul>
 	</div>
 </div>
