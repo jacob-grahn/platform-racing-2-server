442256b9a98523e98f5b70509d8334d5a47ced00
Ben <bls1999@users.noreply.github.com>
2018-01-18 12:01:34 -0500

    Convert index.html -> index.php
    See output_fns.php for updated header/footer code

---
+++ http_server/www/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('../fns/output_fns.php');
+
+	output_header( 'Platform Racing 2' );
+	echo "<div class=\"game_holder\">
+		<embed width=\"550\" height=\"400\" src=\"//cdn.jiggmin.com/games/platform-racing-2/platform-racing-2-loader-v13.swf\" type=\"application/x-shockwave-flash\"></embed>
+			</div>";
+	output_footer();
+?>
