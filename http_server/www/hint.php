2d68255784727024d2549abceef54646b9061f20
Jacob Grahn <jacob@grahn.io>
2018-01-19 08:10:28 -0500

    use test stylesheet

--- http_server/www/hint.php
+++ http_server/www/hint.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Platform Racing 2 Hint</title>
-    <link href="pr2hub.css" rel="stylesheet" type="text/css"/>
+    <link href="hint.css" rel="stylesheet" type="text/css"/>
 </head>
 
 <body>
