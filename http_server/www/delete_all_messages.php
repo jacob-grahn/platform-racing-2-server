5218712e9ceb94ca24fbeb40d436d4f7e1234701
Ben <bls1999@users.noreply.github.com>
2018-01-22 18:41:42 -0500

    Add security against requests without token in uri
    Gerry (@Myst1cZ) decided to make a script that automatically deletes people's PMs, because he's angry he's not getting unbanned for DDoSing and abusing exploits.  This fixes that exploit.

--- http_server/www/delete_all_messages.php
+++ http_server/www/delete_all_messages.php
@@ -1,12 +1,13 @@
 <?php
 
 require_once('../fns/all_fns.php');
+$token = mysqli_real_escape_string($_GET['token']);
 
 try {
 	$db = new DB();
-	$user_id = token_login($db);
+	$user_id = use_login_token($db, $token);
 	
-	$db->call('messages_delete_all', array($user_id) );
+	$db->call( 'messages_delete_all', array($user_id) );
 }
 
 catch(Exception $e){
