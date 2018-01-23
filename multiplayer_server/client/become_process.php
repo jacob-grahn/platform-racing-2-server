c6206e76a0ae7fc1df157d5c7d0423909419722c
Jacob Grahn <contact@jacobgrahn.com>
2018-01-23 11:28:37 -0500

    allow localhost to become a process

--- multiplayer_server/client/become_process.php
+++ multiplayer_server/client/become_process.php
@@ -2,7 +2,7 @@
 
 function client_become_process ($socket, $data) {
   global $PROCESS_PASS, $PROCESS_IP;
-	if($data === $PROCESS_PASS && $socket->ip === $PROCESS_IP) {
+	if($data === $PROCESS_PASS && ($socket->ip === $PROCESS_IP || $socket->ip === '127.0.0.1')) {
 		$socket->process = true;
 	}
 }
