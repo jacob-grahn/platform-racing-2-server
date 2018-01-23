66c720e5ff66d5bec5e311f009d253a801f824a9
Ben <bls1999@users.noreply.github.com>
2018-01-22 19:24:10 -0500

    security patch for GET requests on guild_join.php

--- http_server/www/guild_join.php
+++ http_server/www/guild_join.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once( '../fns/all_fns.php' );
+$token = mysqli_real_escape_string($_GET['token']);
 
 try {
 	
@@ -14,7 +15,7 @@ try {
 	
 	
 	//--- gather information
-	$user_id = token_login($db, false);
+	$user_id = use_login_token($db, $token);
 	$account = $db->grab_row( 'user_select_expanded', array( $user_id ) );
 	$guild = $db->grab_row( 'guild_select', array( $guild_id ) );
 	
