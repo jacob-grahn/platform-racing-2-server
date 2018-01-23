8cf67681eae513c2b49cdb4d3e77c08c95a22d52
Ben <bls1999@users.noreply.github.com>
2018-01-22 22:17:16 -0500

    Security update on GET; messages_delete_all.php
    turns out token_login() is sufficient; just have to add false as the second argument.

--- http_server/www/messages_delete_all.php
+++ http_server/www/messages_delete_all.php
@@ -4,10 +4,10 @@ require_once('../fns/all_fns.php');
 
 try {
 	$db = new DB();
-	$user_id = token_login($db);
+	$user_id = token_login($db, false);
 	
-	// $db->call('messages_delete_all', array($user_id) );
-	echo 'success=true';
+	$db->call('messages_delete_all', array($user_id) );
+	echo 'message=All of your PMs have been deleted!'; // if successful, echo a success message
 }
 
 catch(Exception $e){
