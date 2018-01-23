0dd96156dec0d273742a06c8c6346a556151b898
Jacob Grahn <contact@jacobgrahn.com>
2018-01-23 12:11:46 -0500

    this is a process function, not a client function

--- multiplayer_server/process/send_message_to_player.php
+++ multiplayer_server/process/send_message_to_player.php
@@ -1,6 +1,6 @@
 <?php
 
-function client_send_message_to_player ($socket, $data) {
+function process_send_message_to_player ($socket, $data) {
 	if($socket->process == true) {
 		$obj = json_decode( $data );
 		$user_id = $obj->user_id;
