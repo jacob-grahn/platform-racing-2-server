118e73499e1730c2d859b9ab88b9b7f6d42473c8
Ben <bls1999@users.noreply.github.com>
2018-01-28 15:49:34 -0500

    Add tournament_status function
    Will display to users when typing '/t status' with future commit

--- multiplayer_server/fns/announce_tournament.php
+++ multiplayer_server/fns/announce_tournament.php
@@ -13,4 +13,17 @@ function announce_tournament( $chat ) {
 	}
 }
 
+function tournament_status( $requester ) {
+	if( pr2_server::$tournament ) {
+		$requester->write('systemChat`Tournament mode is on!<br/>'
+			.'Hat: '.Hats::id_to_str(pr2_server::$tournament_hat).'<br/>'
+			.'Speed: '.pr2_server::$tournament_speed.'<br/>'
+			.'Accel: '.pr2_server::$tournament_acceleration.'<br/>'
+			.'Jump: '.pr2_server::$tournament_jumping);
+	}
+	else {
+		$requester->write('systemChat`Tournament mode is off.');
+	}
+}
+
 ?>
