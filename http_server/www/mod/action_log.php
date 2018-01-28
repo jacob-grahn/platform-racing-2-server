e85f0bf55d33320f9e7d674cf6b05076f1a72bcf
Jacob Grahn <jacob@grahn.io>
2018-01-27 19:40:36 -0500

    Add mod check

--- http_server/www/mod/action_log.php
+++ http_server/www/mod/action_log.php
@@ -11,7 +11,9 @@ try {
 
 	//connect
 	$db = new DB();
-
+	
+	//make sure you're a moderator
+	$mod = check_moderator($db);
 
 	//get actions for this page
 	$actions = $db->call( 'mod_actions_select', array( $start, $count ) );
@@ -23,13 +25,10 @@ try {
 	output_pagination($start, $count);
 	echo('<p>---</p>');
 
-
 	//output actions
 	while( $row = $actions->fetch_object() ) {
-		//$formatted_time = date('M j, Y g:i A', $row->time);
 		echo("<p><span class='date'>$row->time</span> -- ".htmlspecialchars($row->message)."</p>");
 	}
-
 
 	echo('<p>---</p>');
 	output_pagination($start, $count);
