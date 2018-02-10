c457a37be3f880bbb968038b5f4a088ba1c9b1f8
Ben <bls1999@users.noreply.github.com>
2018-02-10 18:33:05 -0500

    Optimize server_status.php

--- http_server/www/server_status.php
+++ http_server/www/server_status.php
@@ -1,83 +1,91 @@
 <?php
 
-if (!headers_sent()) {
-	header("Content-Type: text/html; charset=UTF-8");
-}
-
-else {
-	echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>";
-}
-
+require_once('../fns/data_fns.php');
 require_once('../fns/output_fns.php');
 
-output_header( 'Server Status' );
-
-echo "<center>";
-
-echo '<font face="Gwibble" class="gwibble">-- Server Status --</font>';
-
-echo "<br /><br />";
-
-echo "<style>
-table, th, td {
-    background-color: #f2f2f2;
-    padding: 5px;
-    border: 1px solid #000000;
-    border-collapse: collapse;
-}
-</style>";
-
-echo "<table>
-  <tr>
-    <th>Server Name</th>
-    <th>Population</th>
-    <th>Status</th>
-    <th>Happy Hour</th>
-    <th>Tournament</th>
-  </tr>";
+output_header('Server Status');
 
 try {
 
-$decode = json_decode(file_get_contents("http://pr2hub.com/files/server_status_2.txt"), true);
+	$data = json_decode(file_get_contents("http://pr2hub.com/files/server_status_2.txt"));
 
-foreach ($decode["servers"] as $jarray) {
+	if (array_key_exists('error', $data)) {
+		throw new Exception($data->error);
+	}
+	
+	// make a heading
+	echo '
+	<center>
+	<font face="Gwibble" class="gwibble">-- Server Status --</font>
+	<br /><br />
+	';
+	
+	// make a table and the headers
+	echo "
+	<table>
+	  <tr>
+		<th>Server Name</th>
+		<th>Players</th>
+		<th>Happy Hour</th>
+		<th>Tournament</th>
+	  </tr>
+	";
+	
+	foreach ($data->servers as $server) {
+	
+		// echo this when it's yes/no
+		$yes = "<strong>Yes</strong>";
+		$no = "No";
+	
+		// make some variables
+		$happy_hour = check_value($server->happy_hour, 1, $yes, $no);
+		$tournament = check_value($server->tournament, 1, $yes, $no);
+		$server_name = htmlspecialchars($server->server_name);
+		$guild_id = (int) $server->guild_id;
+		$population = (int) $server->population;
+		$status = $server->status;
+	
+		// start row
+		echo "<tr>";
+	
+		// echo the server name (in bold if a guild-only server)
+		if ($guild_id === 0) {
+			echo "<td>$server_name</td>";
+		}
+		else {
+			echo "<td><strong>$server_name</strong></td>";
+		}
+	
+		// if open, echo the population
+		if (strtolower($status) == 'open' || $population > 0) {
+			echo "<td>$population online</td>";
+		}
+		else {
+			echo "<td><strong>down</strong></td>";
+		}
+	
+		// echo status of a happy hour
+		echo "<td>$happy_hour</td>";
+	
+		// echo status of a tournament
+		echo "<td>$tournament</td>";
+	
+		// end row
+		echo "</tr>";
 
-    $hh = "No";
-    $tournament = "No";
-    
-    if (strval($jarray["happy_hour"]) == "1") {
-        $hh = "Yes";
-    }
-    
-    if (strval($jarray["tournament"]) == "1") {
-        $tournament = "Yes";
-    }
-
-    echo "<tr>";
-
-    echo "<td>" . htmlspecialchars($jarray["server_name"]) . "</td>";
-
-    echo "<td>" . htmlspecialchars($jarray["population"]) . "</td>";
-
-    echo "<td>" . htmlspecialchars($jarray["status"]) . "</td>";
-
-    echo "<td>" . htmlspecialchars($hh) . "</td>";
-
-    echo "<td>" . htmlspecialchars($tournament) . "</td>";
-
-    echo "</tr>";
+	}
+	
+	echo "</table>";
+	
 }
 
-echo "</table>";
+catch (Exception $e) {
+	$safe_message = htmlspecialchars($e->getMessage());
+	echo "Error: $safe_message";
+}
 
 echo "</center>";
-
-}
-
-catch (Exception $emsg) {
-    echo "<center><span>" . "There was an error getting the PR2 servers: " . htmlspecialchars($emsg->getMessage) . "</span></center>";
-}
-
 output_footer();
+die();
 
 ?>
