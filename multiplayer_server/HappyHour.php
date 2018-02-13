7e9e20c44322271211f7320f02e46aa84f487157
Ben <bls1999@users.noreply.github.com>
2018-02-13 00:42:35 -0500

    Disable HH on tournament, formatting
    This SHOULD disable HH on servers with tournament mode enabled... but do check my code to make sure it works.

--- multiplayer_server/HappyHour.php
+++ multiplayer_server/HappyHour.php
@@ -2,21 +2,26 @@
 
 class HappyHour {
 
-  private static $active_until = 0;
-  public static $random_hour = 0;
+	private static $active_until = 0;
+	public static $random_hour = 0;
 
-  public static function activate ($duration = 3600) {
-    $time = time();
-    if (self::$active_until < $time) {
-      self::$active_until = $time;
-    }
-    self::$active_until += $duration;
-  }
+	public static function activate ($duration = 3600) {
+		if (pr2_server::$tournament === false) {
+			$time = time();
+			
+			if (self::$active_until < $time) {
+				self::$active_until = $time;
+			}
+			
+			self::$active_until += $duration;
+		}
+	}
 
-  public static function isActive () {
-    $current_hour = (int) date('G');
-    return self::$active_until >= time() || $current_hour === self::$random_hour;
-  }
+	public static function isActive () {
+		$current_hour = (int) date('G');
+		return self::$active_until >= time() || $current_hour === self::$random_hour;
+	}
+
 }
 
 HappyHour::$random_hour = rand(0, 36);
