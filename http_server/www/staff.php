7f2bb3b22fde3f8f0a68e3b31ea9756c939bdc22
Jacob Grahn <contact@jacobgrahn.com>
2018-02-12 22:02:26 -0500

    staff info should come from the db

--- http_server/www/staff.php
+++ http_server/www/staff.php
@@ -1,116 +1,56 @@
 <?php
 
-require_once('../fns/output_fns.php');
+require_once('../fns/all_fns.php');
 
-$staff_array = array(
-				"Jiggmin",
-				"1python64",
-				"bls1999",
-				"Eternal",
-				"inuyasharox",
-				"Dangevin",
-				"cod4fan",
-				"Dev52",
-				"TRUC",
-				"Nemo Nation",
-				"Stxtics",
-				"a7x3"
-			);
-
-// start the page
+$group_names = ['Guest', 'Member', 'Moderator', 'Admin'];
+$group_colors = ['7e7f7f', '047b7b', '1c369f', '870a6f'];
 
 output_header('PR2 Staff Team');
 
-echo "<center>";
-
-echo '<font face="Gwibble" class="gwibble">-- PR2 Staff Team --</font>
-		<br /><br />';
-
 try {
+	$db = new DB();
+	$staff_result = $db->query('
+		SELECT power, status, name, login_date, register_date
+		FROM users
+		WHERE power > 1
+		ORDER BY loginDate DESC
+	')
 
-	echo "<table>
+	echo '
+	<center>
+	<font face="Gwibble" class="gwibble">-- PR2 Staff Team --</font>
+	<br>
+	<br>
+	<table>
 	  <tr>
 	    <th>Username</th>
 	    <th>Status</th>
 	    <th>Guild</th>
-	    <th>Rank</th>
-	    <th>Hats</th>
-	    <th>Joined</th>
 	    <th>Last Login</th>
-	  </tr>";
-	  
-	foreach ($staff_array as $player) {
-	
-		// get data
-		$decode = json_decode(file_get_contents("https://pr2hub.com/get_player_info_2.php?name=" . urlencode($player)));
+	  </tr>';
 
-		// make sure the data came back without an error
-		if (array_key_exists("error", $decode)) {
-			throw new Exception($decode->error);
-		}
-
+	while ($row = $staff_result->fetch_object()) {
 		// make nice variables for our data
-		$safe_name = htmlspecialchars($decode->name);
-		$group = (int) $decode->group;
-		$status = $decode->status;
-		$rank = (int) $decode->rank;
-		$hats = (int) $decode->hats;
-		$register_date = $decode->registerDate;
-		$active_date = $decode->loginDate;
-		
-		if ($group <= 1) {
-			continue;
-		}
+		$safe_name = htmlspecialchars($row->name);
+		$group = (int) $row->group;
+		$status = $row->status;
+		$active_date = $row->login_date;
+		$register_date = $row->register_date;
+		$group_name = $group_names[$group];
+		$group_color = $group_colors[$group];
 
-		// make nice group variables
-		switch($group) {
-			case 0:
-				$group_name = "Guest";
-				$group_color = "7e7f7f";
-				break;
-			case 1:
-				$group_name = "Member";
-				$group_color = "047b7b";
-				break;
-			case 2:
-				$group_name = "Moderator";
-				$group_color = "1c369f";
-				break;
-			case 3:
-				$group_name = "Admin";
-				$group_color = "870a6f";
-				break;
-			default:
-				throw new Exception("Error fetching group data.");
-		}
-		
 		// start the row
 		echo "<tr>";
-		
+
 		// display the name with the color
 		echo "<td><font color='#$group_color'><u>$safe_name</u></font></td>";
 		if (empty($safe_name) && strlen(trim($safe_name)) === 0) {
 			throw new Exception("Invalid name.");
 		}
-	
+
 		// display the status
 		echo "<td>$status</td>";
-		if (empty($status)) {
-			throw new Exception("Invalid status.");
-		}
-	
-		// display the rank
-		echo "<td>$rank</td>";
-		if (empty($rank) && $rank !== 0) {
-			throw new Exception("Rank not received.");
-		}
-	
-		// display the hats
-		echo "<td>$hats</td>";
-		if (empty($hats) && $hats !== 0) {
-			throw new Exception("Number of hats not received.");
-		}
-	
+
 		// display the register date
 		if ($register_date == "1/Jan/1970") {
 			echo "<td>Age of Heroes</td>";
@@ -121,25 +61,26 @@ try {
 		else {
 			throw new Exception("No register date received.");
 		}
-	
+
 		// display the active date
 		echo "<td>$active_date</td>";
 
 		// end the row
 		echo "</tr>";
-	
 	}
-	
+
 	// end the table
-	echo "</table>";
-	
+	echo '
+	</table>
+	</center>
+	';
+}
+catch(Exception $e){
+	$reply = new stdClass();
+	$reply->error = $e->getMessage();
+	echo json_encode( $reply );
 }
 
-catch (Exception $e) {
-	echo "Error: " . $e->getMessage();
-}
-
-echo "</center>";
 output_footer();
 
 ?>
