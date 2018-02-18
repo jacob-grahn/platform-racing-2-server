85b0a657cebc9bb1048b5df74f7962bf8e0c4f4a
Ben <bls1999@users.noreply.github.com>
2018-02-18 01:20:23 -0500

    Plain text header -> remove_level.php

--- http_server/www/remove_level.php
+++ http_server/www/remove_level.php
@@ -1,33 +1,35 @@
 <?php
 
+header("Content-type: text/plain");
 require_once('../fns/all_fns.php');
 
 $level_id = find('level_id', 'none');
 
 try{	
-	//error check
+	// sanity check: was a level ID specified?
 	if($level_id == 'none'){
-		throw new Exception('No level id was given.');
+		throw new Exception('No level ID was specified.');
 	}
 	
-	//connect
+	// connect
 	$db = new DB();
 	
-	//make sure the user is a moderator
+	// make sure the user is a moderator
 	$mod = check_moderator($db);
 	
+	// make sure the user is a permanent moderator
 	if($mod->can_unpublish_level != 1) {
 		throw new Exception('You can not unpublish levels.');
 	}
 	
-	//check for the level's information
+	// check for the level's information
 	$level = $db->grab_row( 'level_select', array($level_id) );
 	$l_title = $level->title;
-	$l_creator = $level->user_id;
+	$l_creator = id_to_name($level->user_id);
 	$l_note = $level->note;
 	
 	
-	//unpublish the level
+	// unpublish the level
 	$db->call('level_unpublish', array($level_id));
 	
 	//tell it to the world
@@ -39,12 +41,13 @@ try{
 	$ip = $mod->ip;
 	
 	//record the change
-	$db->call('mod_action_insert', array($user_id, "$name unpublished level $level_id from $ip {level_title: $l_title, creator_id: $l_creator, level_note: $l_note}", $user_id, $ip));
+	$db->call('mod_action_insert', array($user_id, "$name unpublished level $level_id from $ip {level_title: $l_title, creator: $l_creator, level_note: $l_note}", $user_id, $ip));
 	
 }
 
-catch(Exception $e){
-	echo 'error='.$e->getMessage();
+catch (Exception $e){
+	$error = $e->getMessage();
+	echo "error=$error";
 }
 
 ?>
