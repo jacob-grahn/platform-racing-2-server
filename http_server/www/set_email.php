b395972aa7e7176fae650008f5efdd42c7ab144d
Ben <bls1999@users.noreply.github.com>
2018-02-18 01:24:31 -0500

    Plain text header -> set_email.php

--- http_server/www/set_email.php
+++ http_server/www/set_email.php
@@ -1,30 +1,34 @@
 <?php
 
+header("Content-type: text/plain");
 require_once('../fns/all_fns.php');
 
 $email = $_POST['email'];
 
-try{
+try {
 
+	// sanity check: was anything submitted?
 	if(strlen($email) <= 0){
 		throw new Exception('You must enter an email.');
 	}
 
+	// connect
 	$db = new DB();
 	
-	//check thier login
+	// check their login
 	$user_id = token_login($db);
 	$user = $db->grab_row('user_select', array($user_id));
 	
-	//ignore this if they are a guest
+	// sanity check: are they a guest?
 	if($user->power < 1){
-		throw new Exception('Guests don\'t even really have accounts...');
+		throw new Exception("Guests don't even really have accounts...");
 	}
 	
-	//change thier email
+	// change their email
 	$safe_email = $db->escape($email);
 	$safe_user_id = $db->escape($user_id);
 	
+	// do it
 	$query = "update users
 				set email = '$safe_email'
 				where user_id = '$safe_user_id'
@@ -32,14 +36,18 @@ try{
 				limit 1";
 	$result = $db->query($query);
 	
+	// if you couldn't do it, tell them
 	if(!$result){
 		throw new Exception('Could not update your email. Sorries.');
 	}
 	
+	// tell the world
 	echo 'message=Your email has been changed successfully!';
+	
 }
-catch(Exception $e){
-	echo 'error='.$e->getMessage();
+catch (Exception $e) {
+	$error = $e->getMessage();
+	echo "error=$error";
 }
 
 
