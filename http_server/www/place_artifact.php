4e429d66b00366c2d57dbb414f5a5788a7bb983c
Ben <bls1999@users.noreply.github.com>
2018-02-18 01:13:55 -0500

    Small updates to place_artifact.php

--- http_server/www/place_artifact.php
+++ http_server/www/place_artifact.php
@@ -1,5 +1,6 @@
 <?php
 
+header("Content-type: text/plain");
 require_once('../fns/all_fns.php');
 
 $x = find('x', 0);
@@ -7,25 +8,33 @@ $y = find('y', 0);
 $level_id = find('levelId', 0);
 
 try {
-	//error check
+	// sanity check: was a level ID specified?
 	if($level_id == 0){
-		throw new Exception('No level id was given.');
+		throw new Exception('No level ID was specified.');
 	}
 	
-	//connect
+	// connect
 	$db = new DB();
 	
-	//check thier login
+	// check their login
 	$user_id = token_login($db);
+	
+	// sanity check: are they Fred?
 	if( $user_id != 1 && $user_id != 4291976 ) {
 		throw new Exception( 'You are not Fred.' );
 	}
 	
+	// update the artifact location in the database
 	$db->call( 'artifact_location_update', array( $level_id, $x, $y ) );
+	
+	// tell the world
+	echo "message=Great success! The artifact location will be updated at the top of the next minute.";
+	
 }
 
-catch(Exception $e) {
-	echo 'error='.$e->getMessage();
+catch (Exception $e) {
+	$error = $e->getMessage();
+	echo "error=$error";
 }
 
 ?>
