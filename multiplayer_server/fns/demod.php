7c2320f6f6798086adf9f3ebc30dcfef5a99b9f6
Ben <bls1999@users.noreply.github.com>
2018-02-19 16:38:34 -0500

    Let admins demote temp mods; excl. server owners
    Restores functionality to the admins' demotion of temp mods.  Doesn't allow admins to demote the server's guild_owner.

--- multiplayer_server/fns/demod.php
+++ multiplayer_server/fns/demod.php
@@ -3,13 +3,22 @@
 require_once(__DIR__ . '/db_fns.php');
 
 function demote_mod($port, $user_name, $admin, $demoted_player) {
-	global $db;
-	global $server_name;
+	global $db, $server_name, $guild_owner;
+	
+	// safety first
+	$safe_user_name = htmlspecialchars($user_name);
 
 	// if the user isn't an admin on the server, kill the function (2nd line of defense)
 	if($admin->group != 3) {
-		echo $admin->name." lacks the server power to demote $user_name.";
-		$admin->write("message`Error: You lack the power to demote $user_name.");
+		echo $admin->name." lacks the server power to demote $safe_user_name.";
+		$admin->write("message`Error: You lack the power to demote $safe_user_name.");
+		return false;
+	}
+	
+	// don't let the admin demote the server owner
+	if($demoted_player->user_id == $guild_owner) {
+		echo $admin->name." lacks the power to demote the server owner.";
+		$admin->write("message`Error: The server owner reigns supreme!";
 		return false;
 	}
 
@@ -53,10 +62,9 @@ function demote_mod($port, $user_name, $admin, $demoted_player) {
 			throw new Exception("Could not demote $user_name due to a database error.");
 		}
 		
-		// if the user was a mod or higher, log it in the action log
+		// demote trial/perma mod and log it in the action log
 		if($user_row->power >= 2) {
-		
-			//action log
+			// action log variables
 			$ip = $admin->ip;
 			$admin_id = $admin->user_id;
 			$admin_name = $admin->name;
@@ -70,8 +78,15 @@ function demote_mod($port, $user_name, $admin, $demoted_player) {
 				$demoted_player->group = 1;
 				$demoted_player->write('setGroup`1');
 			}
-			echo $admin->name." demoted $user_name.";
-			$admin->write("message`$user_name has been demoted.");
+			echo $admin->name." demoted $safe_user_name.";
+			$admin->write("message`$safe_user_name has been demoted.");
+		}
+		// demote temp mod
+		else if(isset($demoted_player) && $demoted_player->temp_mod === true && $demoted_player->group == 2) {
+			$demoted_player->group = 1;
+			$demoted_player->write('setGroup`1');
+			$demoted_player->temp_mod = false;
+			$admin->write("message`$safe_user_name has been demoted.");
 		}
 		else {
 			throw new Exception("$user_name isn't a moderator.");
@@ -80,7 +95,7 @@ function demote_mod($port, $user_name, $admin, $demoted_player) {
 	}
 
 	catch(Exception $e){
-		$message = $e->getMessage();
+		$message = htmlspecialchars($e->getMessage());
 		echo "Error: $message";
 		$admin->write("message`Error: $message");
 		return false;
