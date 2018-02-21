c1c49599cc350ca0466e41a06b45693dd3399444
Jacob Grahn <jacob@grahn.io>
2018-02-21 09:13:01 -0500

    should be array

--- http_server/www/admin/set_campaign.php
+++ http_server/www/admin/set_campaign.php
@@ -6,7 +6,7 @@ require_once('../../fns/output_fns.php');
 $action = find('action', 'lookup');
 $message = find('message', '');
 $campaign_id = 6; // 1 = Original, 2 = Speed, 3 = Luna, 4 = Timeline, 5 = Legendary, 6 = Custom
-$campaign = $db->to_array( $db->call('campaign_select_by_id', $campaign_id) ); 
+$campaign = $db->to_array( $db->call('campaign_select_by_id', [$campaign_id]) ); 
 
 try {
 	
