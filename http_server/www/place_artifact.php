c78664a93b6e344bf9e7b0cdcfcd2f1e884e25fc
Ben <bls1999@users.noreply.github.com>
2018-02-27 15:13:42 -0500

    rl, ref, sc -> place_artifact.php

--- http_server/www/place_artifact.php
+++ http_server/www/place_artifact.php
@@ -3,15 +3,26 @@
 header("Content-type: text/plain");
 require_once('../fns/all_fns.php');
 
-$x = find('x', 0);
-$y = find('y', 0);
-$level_id = find('levelId', 0);
+$x = (int) find('x', 0);
+$y = (int) find('y', 0);
+$level_id = (int) find('levelId', 0);
+$ip = get_ip();
 
 try {
-	// sanity check: was a level ID specified?
-	if($level_id == 0){
-		throw new Exception('No level ID was specified.');
+	
+	// sanity check: is data missing?
+	if (is_empty($x, false) || is_empty($y, false) || is_empty($level_id, false)) {
+		throw new Exception("Some data is missing.");
 	}
+	
+	// check referrer
+	$ref = check_ref();
+	if ($ref !== true || $ref == '') {
+		throw new Exception("It looks like you're using PR2 from a third-party website. For security reasons, you may only set the artifact from an approved site such as pr2hub.com.");
+	}
+	
+	// rate limiting
+	rate_limit( 'place-artifact-attempt-'.$ip, 30, 1, "Please wait at least 30 seconds before trying to set a new artifact location again." );
 	
 	// connect
 	$db = new DB();
@@ -22,6 +33,12 @@ try {
 	// sanity check: are they Fred?
 	if( $user_id != 1 && $user_id != 4291976 ) {
 		throw new Exception( 'You are not Fred.' );
+	}
+	
+	// more rate limiting
+	rate_limit( 'place-artifact-'.$ip, 3600, 5, "The artifact can only be placed a maximum of 5 times per hour. Try again later." );
+	if ($user_id != 1) {
+		rate_limit( 'place-artifact-'.$user_id, 3600, 5, "The artifact can only be placed a maximum of 5 times per hour. Try again later." );
 	}
 	
 	// update the artifact location in the database
