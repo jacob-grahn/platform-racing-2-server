e54ca25e0b6bfad2e0bbd207b4fa0ecd3a1532b1
Jacob Grahn <jacob@grahn.io>
2018-02-27 08:06:59 -0500

    Add connection rate limit

--- http_server/fns/db_fns.php
+++ http_server/fns/db_fns.php
@@ -1,7 +1,17 @@
 <?php
 
+require_once(__DIR__ . '/data_fns.php');
+
 function pr2_connect() {
 	global $DB_ADDRESS, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT;
+	
+	// rate limit anything that uses the db
+	$ip = get_ip();
+	if (isset($ip)) {
+		rate_limit($ip, 30, 15, 'Max 15 connections in 30 seconds');
+	}
+	
+	// connect to the db
 	$mysqli = new mysqli($DB_ADDRESS, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
 	if ($mysqli->connect_error) {
 		throw new Exception( 'Could not connect to pr2\'s database. ' );
