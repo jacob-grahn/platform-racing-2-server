1c72f21673b78ff9edf539aa43ce52fe2e305227
Ben <bls1999@users.noreply.github.com>
2018-02-27 13:57:40 -0500

    rl -> guild_delete.php

--- http_server/www/guild_delete.php
+++ http_server/www/guild_delete.php
@@ -1,37 +1,42 @@
 <?php
 
+header("Content-type: text/plain");
 require_once( '../fns/all_fns.php' );
 
+$guild_id = find_no_cookie('guild_id');
+$ip = get_ip();
+
 try {
-
-	//--- import data
-	$guild_id = find( 'guild_id' );
-
-
-	//--- connect to the db
+	
+	// rate limiting
+	rate_limit('guild-delete-'.$ip, 3, 1);
+	
+	// connect to the db
 	$db = new DB();
 
-
-	//--- check their login and make some rad variables
+	// check their login and make some rad variables
 	$mod = check_moderator($db);
 	$mod_name = $mod->name;
 	$mod_id = $mod->user_id;
 	$ip = $mod->ip;
 	
-	//--- check if the guild exists and make some rad variables
-	$guild = $db->grab_row( 'guild_select', array($guild_id), 'Could not find a guild with that id.' );
+	// more rate limiting
+	rate_limit('guild-delete-'.$mod_id, 3, 1);
+	
+	// check if the guild exists and make some rad variables
+	$guild = $db->grab_row('guild_select', array($guild_id), 'Could not find a guild with that id.');
 	$guild_name = $guild->guild_name;
 	$guild_note = $guild->note;
 	$guild_owner = $guild->owner_id;
 
-	//--- edit guild in db
-	$db->call( 'guild_delete', array($guild_id), 'Could not delete the guild.' );
+	// edit guild in db
+	$db->call('guild_delete', array($guild_id), 'Could not delete the guild.');
 	
-	//record the deletion in the action log
+	// record the deletion in the action log
 	$db->call('mod_action_insert', array($mod_id, "$mod_name deleted guild $guild_id from $ip {guild_name: $guild_name, guild_prose: $guild_note, owner_id: $guild_owner}", $mod_id, $ip));
 
 
-	//--- tell it to the world
+	// tell the world
 	$reply = new stdClass();
 	$reply->success = true;
 	$reply->message = 'Guild deleted.';
@@ -39,7 +44,7 @@ try {
 }
 
 
-catch(Exception $e){
+catch(Exception $e) {
 	$reply = new stdClass();
 	$reply->error = $e->getMessage();
 	echo json_encode( $reply );
