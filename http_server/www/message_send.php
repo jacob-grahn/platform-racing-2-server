0dec224343277690c719939bc58706a53d8fcde6
Ben <bls1999@users.noreply.github.com>
2018-02-27 14:33:51 -0500

    post, rl -> message_send.php

--- http_server/www/message_send.php
+++ http_server/www/message_send.php
@@ -3,18 +3,33 @@
 require_once('../fns/all_fns.php');
 require_once('../fns/pr2_fns.php');
 
+header("Content-type: text/plain");
+
 $to_name = $_POST['to_name'];
 $message = $_POST['message'];
+$ip = get_ip();
 
 try {
 	
+	// POST check
+	if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+		throw new Exception('Invalid request method.');
+	}
+	
+	// rate limiting
+	rate_limit('send-pm-attempt-'.$ip, 5, 1);
+	
+	// connect
 	$db = new DB();
 	
+	// variables
 	$from_user_id = token_login($db, false);
 	$to_user_id = name_to_id($db, $to_name);
 	
+	// send it
 	send_pm( $db, $from_user_id, $to_user_id, $message );
 	
+	// tell the world
 	echo 'message=Your message was sent successfully!';		
 }
 
