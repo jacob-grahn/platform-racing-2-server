446c58858470e1cc841dc78adf65634d913b4a87
Ben <bls1999@users.noreply.github.com>
2018-02-27 13:47:21 -0500

    rl -> get_levels.php

--- http_server/www/get_levels.php
+++ http_server/www/get_levels.php
@@ -1,18 +1,31 @@
 <?php
 
 header("Content-type: text/plain");
+
 require_once('../fns/all_fns.php');
 
-$count = find('count', 100);
+$count = find_no_cookie('count', 100);
+$ip = get_ip();
 
 try {
 	
+	// rate limiting
+	rate_limit('get-levels-'.$ip, 15, 1, "Please wait at least 15 seconds before loading your levels again.");
+	
+	// connect
 	$db = new DB();
+	
+	// check login
 	$user_id = token_login($db);
 	
+	// more rate limiting
+	rate_limit('get-levels-'.$user_id, 15, 1, "Please wait at least 15 seconds before loading your levels again.");
+	
+	// get levels
 	$result = $db->call('levels_select_by_owner', array($user_id));
 	$str = format_level_list($result, $count);
 	
+	// tell the world
 	echo $str;
 }
 catch (Exception $e) {
