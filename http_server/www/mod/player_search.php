c3545464ccef9e559e9703929e45c02b58aff0f9
Ben <bls1999@users.noreply.github.com>
2018-02-27 23:15:39 -0500

    post, rl, fn, try/catch -> mod/player_search.php

--- http_server/www/mod/player_search.php
+++ http_server/www/mod/player_search.php
@@ -3,26 +3,47 @@
 require_once('../../fns/all_fns.php');
 require_once('../../fns/output_fns.php');
 
-$message = find('message', '');
+$message = default_val($_GET['message'], '');
+$ip = get_ip();
 
 try {
 
+	// rate limiting
+	rate_limit('mod-player-search-'.$ip, 5, 1);
+	rate_limit('mod-player-search-'.$ip, 30, 5);
+	
 	// connect
 	$db = new DB();
 
 	// make sure you're a moderator
 	$mod = check_moderator($db, false);
+	
+}
+catch(Exception $e) {
+	$error = $e->getMessage();
+	output_header("Error");
+	echo "Error: $error";
+	output_footer();
+	die();
+}
 
+try {
+	
 	// header
 	output_header('Player Search', true);
+	
+	// rate limiting
+	$mod_id = $mod->user_id;
+	rate_limit('mod-player-search-'.$mod_id, 5, 1);
+	rate_limit('mod-player-search-'.$mod_id, 30, 5);
 
-	//
-	if($message != '') {
+	// error message
+	if(!is_empty($message)) {
 		echo "<p><b>$message</b></p>";
 	}
 
 	?>
-	<form action="do_player_search.php" method="get">
+	<form action="do_player_search.php" method="post">
 		Name <input type="text" value="" name="name" />
 		<input type="submit" value="Search" />
 	</form>
@@ -31,8 +52,9 @@ try {
 }
 
 catch(Exception $e){
-	echo 'Error: '.$e->getMessage();
+	$error = $e->getMessage();
+	echo "Error: $error";
+	output_footer();
 }
 
-output_footer();
 ?>
