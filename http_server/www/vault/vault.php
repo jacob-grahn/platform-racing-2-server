dfff19645811c87495c02af32775ff1fbe0ded21
Ben <bls1999@users.noreply.github.com>
2018-02-27 23:40:26 -0500

    rl -> vault.php

--- http_server/www/vault/vault.php
+++ http_server/www/vault/vault.php
@@ -5,23 +5,34 @@ header("Content-type: text/plain");
 require_once( '../../fns/all_fns.php' );
 require_once( 'vault_descriptions.php' );
 
+$ip = get_ip();
+
 try {
-	//--- connect
+	
+	// rate limiting
+	rate_limit('vault-listing-'.$ip, 3, 1);
+	rate_limit('vault-listing-'.$ip, 15, 4);
+	
+	// connect
 	$db = new DB();
 	
+	// get login
+	$user_id = token_login($db);
 	
-	//--- create listings
-	$user_id = token_login($db, true);
+	// more rate limiting
+	rate_limit('vault-listing-'.$user_id, 5, 2);
+	rate_limit('vault-listing-'.$user_id, 30, 10);
+	
+	// create listing
 	$raw_listings = describeVault( $db, $user_id, array('stats-boost', 'epic-everything', 'guild-fred', 'guild-ghost', 'guild-artifact', 'happy-hour', 'rank-rental', 'djinn-set', 'king-set', 'queen-set', 'server-1-day', 'server-30-days') );
-
 	
-	//--- weed out only the info we want to return
+	// weed out only the info we want to return
 	$listings = array();
 	foreach( $raw_listings as $raw ) {
 		$listings[] = makeListing( $raw );
 	}
 
-	//--- reply		
+	// reply		
 	$r = new stdClass();
 	$r->success = true;
 	$r->listings = $listings;
