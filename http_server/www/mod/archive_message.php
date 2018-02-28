4357806c7faa9802813942ce398e53dc71d196ed
Ben <bls1999@users.noreply.github.com>
2018-02-27 22:39:59 -0500

    rl, mres, fn, try/catch -> archive_message.php

--- http_server/www/mod/archive_message.php
+++ http_server/www/mod/archive_message.php
@@ -5,40 +5,65 @@ require_once('../../fns/output_fns.php');
 
 $message_id = (int) default_val($_GET['message_id'], 0);
 $safe_message_id = mysqli_real_escape_string($message_id);
+$ip = get_ip();
 
 try {
 
 	// rate limiting
-	rate_limit('mod-archive-message-'.$ip, 5, 1);
-	rate_limit('mod-archive-message-'.$ip, 30, 5);
+	rate_limit('mod-archive-message-'.$ip, 3, 1);
+	rate_limit('mod-archive-message-'.$ip, 15, 3);
 
 	// connect
-	$db = new DB();	
+	$db = new DB();
 	
 	// make sure you're a moderator
 	$mod = check_moderator($db);
-		
-	// look for a player with provided name
+	
+}
+catch(Exception $e) {
+	$error = $e->getMessage();
+	output_header("Error");
+	echo "Error: $error";
+	output_footer();
+	die();
+}
+
+try {
+	
+	// more rate limiting
+	$mod_id = $mod->user_id;
+	rate_limit('mod-archive-message-'.$mod_id, 3, 1);
+	rate_limit('mod-archive-message-'.$mod_id, 15, 3);
+	
+	// archive the message
 	$result = $db->query("UPDATE messages_reported
 							SET archived = 1
 							WHERE message_id = '$safe_message_id'
 							LIMIT 1");
 	if(!$result) {
-		throw new Exception('Could not mark message as archived.');
+		throw new Exception('Could not archive the message.');
 	}
-	
-	//action log
+
+	// action log
 	$name = $mod->name;
 	$ip = $mod->ip;
-		
-	//record the change
+
+	// record the change
 	$db->call('mod_action_insert', array($mod->user_id, "$name archived the report of PM $safe_message_id from $ip", $mod->user_id, $ip));
+
+	// tell the sorry saps trying to debug
+	$ret = new stdClass();
+	$ret->success = true;
+	$ret->message_id = $message_id;
+	echo json_encode($ret);
 
 }
 catch(Exception $e) {
-	$error = $e->getMessage();
-	echo "Error: $error";
-	die();
+	$ret = new stdClass();
+	$ret->success = false;
+	$ret->error = $e->getMessage();
+	$ret->message_id = $message_id;
+	echo json_encode($ret);
 }
 
 ?>
