68088e59495830cfcfe1e2bbf4ac0506b41ab963
Ben <bls1999@users.noreply.github.com>
2018-03-01 18:56:25 -0500

    Create action_log.php
    The MySQL queries need to be fixed, unless those are the columns in the db for the action log tables.

---
+++ http_server/queries/mod/action_log.php
@@ -0,0 +1,43 @@
+<?php
+
+function log_mod_action($pdo, $string, $user_id, $ip) {
+	$time = (int) time();
+
+	$stmt = $pdo->prepare('INSERT INTO mod_action SET
+								string = ?,
+								mod_user_id = ?,
+								mod_ip = ?,
+								time = ?');
+
+	// bind the parameters
+	$stmt->bindParam(1, $string, PDO::PARAM_STR);
+	$stmt->bindParam(2, $mod_user_id, PDO::PARAM_INT);
+	$stmt->bindParam(3, $mod_ip, PDO::PARAM_STR);
+	$stmt->bindParam(4, $time, PDO::PARAM_INT);
+	
+	// execute the PDO
+	$stmt->execute();
+	
+}
+
+function log_admin_action($pdo, $string, $user_id, $ip) {
+	$time = (int) time();
+
+	$stmt = $pdo->prepare('INSERT INTO admin_action SET
+								string = ?,
+								user_id = ?,
+								ip = ?,
+								time = ?');
+
+	// bind the parameters
+	$stmt->bindParam(1, $string, PDO::PARAM_STR);
+	$stmt->bindParam(2, $user_id, PDO::PARAM_INT);
+	$stmt->bindParam(3, $ip, PDO::PARAM_STR);
+	$stmt->bindParam(4, $time, PDO::PARAM_INT);
+	
+	// execute the PDO
+	$stmt->execute();
+	
+}
+
+?>
