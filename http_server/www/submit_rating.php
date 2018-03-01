b232764356485cdeeef8c649d229e9ba2ab96c55
Ben <bls1999@users.noreply.github.com>
2018-03-01 01:04:56 -0500

    Fix rl -> submit_rating.php

--- http_server/www/submit_rating.php
+++ http_server/www/submit_rating.php
@@ -37,7 +37,7 @@ try {
 	$user_id = token_login($db, false);
 	
 	// rate limiting
-	rate_limit('submit-rating-'.$user_id, 5, 1);
+	rate_limit('submit-rating-'.$user_id, 5, 2);
 
 	// see if they made this level
 	$result = $db->query("SELECT level_id
