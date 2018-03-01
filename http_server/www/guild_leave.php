bc9ca3a493eef40eb53c91147f994b40e07180c9
Ben <bls1999@users.noreply.github.com>
2018-03-01 00:30:13 -0500

    Fix rl -> guild_leave.php

--- http_server/www/guild_leave.php
+++ http_server/www/guild_leave.php
@@ -14,29 +14,29 @@ try {
 	}
 	
 	// rate limiting
-	rate_limit('guild-leave-attempt-'.$ip, 30, 1, "Please wait at least 30 seconds before attempting to leave your guild again.");
+	rate_limit('guild-leave-attempt-'.$ip, 5, 1);
 	
-	//--- connect to the db
+	// connect to the db
 	$db = new DB();
 	
 	
-	//--- get thier login
+	// get their login
 	$user_id = token_login($db, false);
 	$account = $db->grab_row( 'user_select_expanded', array($user_id) );
 	
 	
-	//--- sanity check
+	// sanity check
 	if( $account->guild == 0 ) {
 		throw new Exception( 'You are not a member of a guild.' );
 	}
 	
 	
-	//--- leave the guild
+	// leave the guild
 	$db->call( 'guild_increment_member', array( $account->guild, -1 ) );
 	$db->call( 'user_update_guild', array( $user_id, 0 ) );
 	
 	
-	//--- tell it to the world
+	// tell it to the world
 	$reply = new stdClass();
 	$reply->success = true;
 	$reply->message = 'You have left the guild.';
