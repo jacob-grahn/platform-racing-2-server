aeda60a7e17a28e5ea9c46d7fe873943c998c38b
Ben <bls1999@users.noreply.github.com>
2018-03-01 21:03:21 -0500

    Finalize PDO (finally fixed everything lol)

--- http_server/queries/get_info/user.php
+++ http_server/queries/get_info/user.php
@@ -2,14 +2,15 @@
 
 function user_select_by_name($pdo, $name) {
 	$stmt = $pdo->prepare('SELECT * FROM users WHERE name = ?');
-	$stmt->execute($name);
+	$stmt->bindValue(1, $name, PDO::PARAM_STR);
+	$stmt->execute();
 	$user = $stmt->fetchAll(PDO::FETCH_OBJ);
 	return $user;
 }
 
 function user_select_by_id($pdo, $user_id) {
 	$stmt = $pdo->prepare('SELECT * FROM users WHERE user_id = ?');
-	$stmt->bindParam(1, $user_id, PDO::PARAM_INT);
+	$stmt->bindValue(1, $user_id, PDO::PARAM_INT);
 	$stmt->execute();
 	$user = $stmt->fetchAll(PDO::FETCH_OBJ);
 	return $user;
