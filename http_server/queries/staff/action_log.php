e7307908a71f43a06a6beabf19afcb417e8de4c6
Ben <bls1999@users.noreply.github.com>
2018-03-01 21:37:24 -0500

    Fix syntax -> pdo in queries/staff/action_log.php

--- http_server/queries/staff/action_log.php
+++ http_server/queries/staff/action_log.php
@@ -3,41 +3,31 @@
 function log_mod_action($pdo, $string, $user_id, $ip) {
 	$time = (int) time();
 
-	$stmt = $pdo->prepare('INSERT INTO mod_action SET
-								string = ?,
-								user_id = ?,
-								ip = ?,
-								time = ?');
+	$stmt = $pdo->prepare('INSERT INTO mod_action (string, user_id, ip, time) VALUES (?, ?, ?, ?)');
 
-	// bind the parameters
-	$stmt->bindParam(1, $string, PDO::PARAM_STR);
-	$stmt->bindParam(2, $user_id, PDO::PARAM_INT);
-	$stmt->bindParam(3, $ip, PDO::PARAM_STR);
-	$stmt->bindParam(4, $time, PDO::PARAM_INT);
+	// bind the values
+	$stmt->bindValue(1, $string, PDO::PARAM_STR);
+	$stmt->bindValue(2, $user_id, PDO::PARAM_INT);
+	$stmt->bindValue(3, $ip, PDO::PARAM_STR);
+	$stmt->bindValue(4, $time, PDO::PARAM_INT);
 	
 	// execute the PDO
 	$stmt->execute();
-	
 }
 
 function log_admin_action($pdo, $string, $user_id, $ip) {
 	$time = (int) time();
 
-	$stmt = $pdo->prepare('INSERT INTO admin_action SET
-								string = ?,
-								user_id = ?,
-								ip = ?,
-								time = ?');
+	$stmt = $pdo->prepare('INSERT INTO admin_action (string, user_id, ip, time) VALUES (?, ?, ?, ?)');
 
-	// bind the parameters
-	$stmt->bindParam(1, $string, PDO::PARAM_STR);
-	$stmt->bindParam(2, $user_id, PDO::PARAM_INT);
-	$stmt->bindParam(3, $ip, PDO::PARAM_STR);
-	$stmt->bindParam(4, $time, PDO::PARAM_INT);
+	// bind the values
+	$stmt->bindValue(1, $string, PDO::PARAM_STR);
+	$stmt->bindValue(2, $user_id, PDO::PARAM_INT);
+	$stmt->bindValue(3, $ip, PDO::PARAM_STR);
+	$stmt->bindValue(4, $time, PDO::PARAM_INT);
 	
 	// execute the PDO
 	$stmt->execute();
-	
 }
 
 ?>
