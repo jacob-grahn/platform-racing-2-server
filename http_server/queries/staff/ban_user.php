cb30a5cb3b1ee5ca5648b83f32f129a299c764f7
Ben <bls1999@users.noreply.github.com>
2018-03-01 21:27:16 -0500

    fix pdo -> queries/staff/ban_user.php

--- http_server/queries/staff/ban_user.php
+++ http_server/queries/staff/ban_user.php
@@ -4,8 +4,8 @@ function throttle_bans($pdo, $mod_user_id) {
 	$time = (int) (time() - 3600);
   
 	$stmt = $pdo->prepare('SELECT COUNT(*) as recent_ban_count FROM bans WHERE mod_user_id = ? AND time > ?');
-	$stmt->bindParam(1, $mod_user_id, PDO::PARAM_INT);
-	$stmt->bindParam(2, $time, PDO::PARAM_INT);
+	$stmt->bindValue(1, $mod_user_id, PDO::PARAM_INT);
+	$stmt->bindValue(2, $time, PDO::PARAM_INT);
 	$stmt->execute();
 	$row = $stmt->fetchAll(PDO::FETCH_OBJ);
 	return $row;
@@ -15,37 +15,25 @@ function ban_user($pdo, $banned_ip, $banned_user_id, $mod_user_id, $expire_time,
 	$time = (int) time();
 	
 	// the SQL we want
-	$stmt = $pdo->prepare('INSERT INTO bans SET
-							banned_ip = ?,
-							banned_user_id = ?,
-							mod_user_id = ?,
-							time = ?,
-							expire_time = ?,
-							reason = ?,
-							record = ?,
-							banned_name = ?,
-							mod_name = ?,
-							ip_ban = ?,
-							account_ban = ?');
-	
+	$stmt = $pdo->prepare('INSERT INTO bans (banned_ip, banned_user_id, mod_user_id, time, expire_time, reason, record, banned_name, mod_name, ip_ban, account_ban) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+
 	// bind the parameters
-	$stmt->bindParam(1, $banned_ip, PDO::PARAM_STR);
-	$stmt->bindParam(2, $banned_user_id, PDO::PARAM_INT);
-	$stmt->bindParam(3, $mod_user_id, PDO::PARAM_INT);
-	$stmt->bindParam(4, $time, PDO::PARAM_INT);
-	$stmt->bindParam(5, $expire_time, PDO::PARAM_INT);
-	$stmt->bindParam(6, $reason, PDO::PARAM_STR);
-	$stmt->bindParam(7, $record, PDO::PARAM_STR);
-	$stmt->bindParam(8, $banned_name, PDO::PARAM_STR);
-	$stmt->bindParam(9, $mod_name, PDO::PARAM_STR);
-	$stmt->bindParam(10, $ip_ban, PDO::PARAM_INT);
-	$stmt->bindParam(11, $account_ban, PDO::PARAM_INT);
+	$stmt->bindValue(1, $banned_ip, PDO::PARAM_STR);
+	$stmt->bindValue(2, $banned_user_id, PDO::PARAM_INT);
+	$stmt->bindValue(3, $mod_user_id, PDO::PARAM_INT);
+	$stmt->bindValue(4, $time, PDO::PARAM_INT);
+	$stmt->bindValue(5, $expire_time, PDO::PARAM_INT);
+	$stmt->bindValue(6, $reason, PDO::PARAM_STR);
+	$stmt->bindValue(7, $record, PDO::PARAM_STR);
+	$stmt->bindValue(8, $banned_name, PDO::PARAM_STR);
+	$stmt->bindValue(9, $mod_name, PDO::PARAM_STR);
+	$stmt->bindValue(10, $ip_ban, PDO::PARAM_INT);
+	$stmt->bindValue(11, $account_ban, PDO::PARAM_INT);
 	
 	// execute the PDO and get the results
 	$stmt->execute();
-	
-	return true;
 
+	return true;
 }
 
 ?>
