330d0c16b38797d10ad02fc4a3e410805cb0df24
Ben <bls1999@users.noreply.github.com>
2018-03-01 21:19:31 -0500

    add todo

--- http_server/queries/get_info/user.php
+++ http_server/queries/get_info/user.php
@@ -4,6 +4,7 @@ function user_select_by_name($pdo, $name) {
 	$stmt = $pdo->prepare('SELECT * FROM users WHERE name = ?');
 	$stmt->bindValue(1, $name, PDO::PARAM_STR);
 	$stmt->execute();
+	// TODO: Add check for more than 1 result and log critical failure if so.
 	$user = $stmt->fetchAll(PDO::FETCH_OBJ);
 	return $user;
 }
@@ -12,6 +13,7 @@ function user_select_by_id($pdo, $user_id) {
 	$stmt = $pdo->prepare('SELECT * FROM users WHERE user_id = ?');
 	$stmt->bindValue(1, $user_id, PDO::PARAM_INT);
 	$stmt->execute();
+	// TODO: Add check for more than 1 result and log critical failure if so.
 	$user = $stmt->fetchAll(PDO::FETCH_OBJ);
 	return $user;
 }
