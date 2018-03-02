f64d7c23d7f6598ff1d0c52273ac7b5b6e3d0c8b
Ben <bls1999@users.noreply.github.com>
2018-03-01 21:18:47 -0500

    adjust pdo -> retrieve_ban_list.php

--- http_server/queries/bans/retrieve_ban_list.php
+++ http_server/queries/bans/retrieve_ban_list.php
@@ -1,10 +1,12 @@
 <?php
 
 function retrieve_ban_list($pdo, $start, $count) {
-  $stmt = $pdo->prepare('SELECT * FROM bans ORDER BY time DESC LIMIT ?, ?');
-  $stmt->bindValue(1, $start, PDO::PARAM_INT);
-  $stmt->bindValue(2, $count, PDO::PARAM_INT);
-  $stmt->execute();
+	$stmt = $pdo->prepare('SELECT * FROM bans ORDER BY time DESC LIMIT ?, ?');
+	$stmt->bindValue(1, (int) $start, PDO::PARAM_INT);
+	$stmt->bindValue(2, (int) $count, PDO::PARAM_INT);
+	$stmt->execute();
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	return $result;
 }
 
 ?>
