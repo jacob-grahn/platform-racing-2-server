68be2a50366fe5439fb1bde9f890af32c10d29f3
Ben <bls1999@users.noreply.github.com>
2018-03-03 04:33:25 -0500

    add pr2_insert to register_user.php

--- http_server/www/register_user.php
+++ http_server/www/register_user.php
@@ -64,9 +64,25 @@ try {
 	// more rate limiting (check if too many accounts have been made from this ip today)
 	rate_limit('register-account-'.$ip, 86400, 5, 'You may create a maximum of five accounts from the same IP address per day.');
 
-	// everything looks good, so register the user
+	// --- begin user registration --- \\
+	
+	// user insert
 	$pass_hash = to_hash($password);
 	$db->call('user_insert', array($name, $pass_hash, $ip, $time, $email));
+	
+	// pr2 insert
+	$user_id = name_to_id($db, $name);
+	$db->call('pr2_insert', array($user_id));
+
+	// compose a welcome pm
+	$welcome_message = "Welcome to Platform Racing 2, $user_name!\n\n"
+				."<a href='https://grahn.io' target='_blank'><u><font color='#0000FF'>Click here</font></u></a> to read about the latest Platform Racing news on my blog.\n\n"
+				."If you have any questions or comments, send me an email at <a href='mailto:jacob@grahn.io?subject=Questions or Comments about PR2' target='_blank'><u><font color='#0000FF'>jacob@grahn.io</font></u></a>.\n\n"
+				."Thanks for playing, I hope you enjoy.\n\n"
+				."- Jacob";
+	
+	// welcome them
+	$db->call('message_insert', array($user_id, 1, $welcome_message, '0'));
 
 	$ret = new stdClass();
 	$ret->result = 'success';
