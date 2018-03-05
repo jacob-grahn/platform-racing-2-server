e732312e79e4c12214792e6509b0714691c797dd
Ben <bls1999@users.noreply.github.com>
2018-03-05 15:39:12 -0500

    name bound values -> queries -> user.php

--- http_server/queries/get_info/user.php
+++ http_server/queries/get_info/user.php
@@ -1,29 +1,41 @@
 <?php
 
 function user_select_by_name($pdo, $name) {
-	$stmt = $pdo->prepare('SELECT * FROM users WHERE name = ?');
-	$stmt->bindValue(1, $name, PDO::PARAM_STR);
+	$stmt = $pdo->prepare('SELECT * FROM users WHERE name = :name');
+	$stmt->bindValue(':name', $name, PDO::PARAM_STR);
 	$stmt->execute();
+	
+	// get number of results
 	$row_count = $stmt->rowCount();
 	if ($row_count < 1) {
 		return false;
 	}
-	// TODO: Add check for more than 1 result and log critical failure if so.
-	$user = $stmt->fetchAll(PDO::FETCH_OBJ);
-	return $user;
+	if ($row_count > 1) {
+		// critical failure
+	}
+	
+	// return result
+	$result = $stmt->fetchAll(PDO::FETCH_OBJ);
+	return $result;
 }
 
 function user_select_by_id($pdo, $user_id) {
-	$stmt = $pdo->prepare('SELECT * FROM users WHERE user_id = ?');
-	$stmt->bindValue(1, $user_id, PDO::PARAM_INT);
+	$stmt = $pdo->prepare('SELECT * FROM users WHERE user_id = :id');
+	$stmt->bindValue(':id', $user_id, PDO::PARAM_INT);
 	$stmt->execute();
+	
+	// get number of results
 	$row_count = $stmt->rowCount();
 	if ($row_count < 1) {
 		return false;
 	}
-	// TODO: Add check for more than 1 result and log critical failure if so.
-	$user = $stmt->fetchAll(PDO::FETCH_OBJ);
-	return $user;
+	if ($row_count > 1) {
+		// critical failure
+	}
+	
+	// return result
+	$result = $stmt->fetchAll(PDO::FETCH_OBJ);
+	return $result;
 }
 
 ?>
