74ad259433a6a56ab5c569e20ba7e22d53e77ee5
Ben <bls1999@users.noreply.github.com>
2018-03-05 16:02:19 -0500

    name pdo values

--- http_server/queries/bans/retrieve_ban_list.php
+++ http_server/queries/bans/retrieve_ban_list.php
@@ -1,9 +1,9 @@
 <?php
 
 function retrieve_ban_list($pdo, $start, $count) {
-	$stmt = $pdo->prepare('SELECT * FROM bans ORDER BY time DESC LIMIT ?, ?');
-	$stmt->bindValue(1, (int) $start, PDO::PARAM_INT);
-	$stmt->bindValue(2, (int) $count, PDO::PARAM_INT);
+	$stmt = $pdo->prepare('SELECT * FROM bans ORDER BY time DESC LIMIT :start, :count');
+	$stmt->bindValue(':start', (int) $start, PDO::PARAM_INT);
+	$stmt->bindValue(':count', (int) $count, PDO::PARAM_INT);
 	$stmt->execute();
 	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	return $result;
