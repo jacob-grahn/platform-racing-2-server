e00f3a4fe3921d9bb2a6373b1ee39dfb034d896b
Ben <bls1999@users.noreply.github.com>
2018-03-16 14:35:32 -0400

    Update user_select.php

--- http_server/queries/users/user_select.php
+++ http_server/queries/users/user_select.php
@@ -2,19 +2,29 @@
 
 function user_select($pdo, $user_id)
 {
-	$stmt = $pdo->prepare('
-        SELECT user_id, name, email, register_ip, ip, time, register_time,
-               power, status, read_message_id, guild, server_id
-        FROM users
-        WHERE user_id = :user_id
-        LIMIT 1
+    $stmt = $pdo->prepare('
+        SELECT user_id,
+               name,
+               email,
+               register_ip,
+               ip,
+               time,
+               register_time,
+               power,
+               status,
+               read_message_id,
+               guild,
+               server_id
+          FROM users
+         WHERE user_id = :user_id
+         LIMIT 1
     ');
-	$stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-	$stmt->execute();
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->execute();
     $result = $stmt->fetch(PDO::FETCH_OBJ);
 
-    if (!$result) {
-        throw new Exception('user not found');
+    if ($result === false) {
+        throw new Exception('Could not find a user with that ID.');
     }
 
     return $result;
