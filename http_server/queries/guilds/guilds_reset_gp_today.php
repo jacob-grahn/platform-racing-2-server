49583140062df8f66ed0c07b7b15d71cd3d0469d
Ben <bls1999@users.noreply.github.com>
2018-03-16 13:21:53 -0400

    Update guilds_reset_gp_today.php

--- http_server/queries/guilds/guilds_reset_gp_today.php
+++ http_server/queries/guilds/guilds_reset_gp_today.php
@@ -2,13 +2,10 @@
 
 function guilds_reset_gp_today($pdo)
 {
-	$result = $pdo->exec('
-        UPDATE guilds
-        SET gp_today = 0
-    ');
+    $result = $pdo->exec('UPDATE guilds SET gp_today = 0');
 
     if ($result === false) {
-        throw new Exception('could not reset gulids gp_today');
+        throw new Exception('Could not reset guilds gp_today');
     }
 
     return $result;
