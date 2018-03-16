9625f2ca2b3a608b8cb566497638b7ad93b911f3
Ben <bls1999@users.noreply.github.com>
2018-03-16 13:55:26 -0400

    Update new_levels_delete_old.php

--- http_server/queries/new_levels/new_levels_delete_old.php
+++ http_server/queries/new_levels/new_levels_delete_old.php
@@ -2,13 +2,10 @@
 
 function new_levels_delete_old($pdo)
 {
-	$result = $pdo->exec('
-        DELETE FROM pr2_new_levels
-        WHERE time < UNIX_TIMESTAMP(NOW() - INTERVAL 1 DAY)
-    ');
+    $result = $pdo->exec('DELETE FROM pr2_new_levels WHERE time < UNIX_TIMESTAMP(NOW() - INTERVAL 1 DAY)');
 
     if ($result === false) {
-        throw new Exception('could not delete old pr2_new_levels');
+        throw new Exception('Could not delete old pr2_new_levels');
     }
 
     return $result;
