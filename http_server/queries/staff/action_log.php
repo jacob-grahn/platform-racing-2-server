0ebd2a68621faa2900eebd9bbc23da08f6af4e3a
Ben <bls1999@users.noreply.github.com>
2018-03-16 14:14:15 -0400

    Update action_log.php

--- http_server/queries/staff/action_log.php
+++ http_server/queries/staff/action_log.php
@@ -2,38 +2,32 @@
 
 function log_mod_action($pdo, $string, $user_id, $ip)
 {
-    $time = (int) time();
-
-    $stmt = $pdo->prepare(
-        'INSERT INTO mod_action (string, user_id, ip, time)
-					   VALUES (:string, :user_id, :ip, :time)'
-    );
-
-    // bind the values
+    $stmt = $pdo->prepare('INSERT INTO mod_action (string, user_id, ip, time) VALUES (:string, :user_id, :ip, :time)');
     $stmt->bindValue(':string', $string, PDO::PARAM_STR);
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':ip', $ip, PDO::PARAM_STR);
-    $stmt->bindValue(':time', $time, PDO::PARAM_INT);
-    
-    // execute the PDO
-    $stmt->execute();
+    $stmt->bindValue(':time', time(), PDO::PARAM_INT);
+    $result = $stmt->execute();
+
+    if ($result === false) {
+        throw new Exception('Could not log mod action');
+    }
+
+    return $result;
 }
 
 function log_admin_action($pdo, $string, $user_id, $ip)
 {
-    $time = (int) time();
-
-    $stmt = $pdo->prepare(
-        'INSERT INTO admin_action (string, user_id, ip, time)
-						 VALUES (:string, :user_id, :ip, :time)'
-    );
-
-    // bind the values
+    $stmt = $pdo->prepare('INSERT INTO admin_action (string, user_id, ip, time) VALUES (:string, :user_id, :ip, :time)');
     $stmt->bindValue(':string', $string, PDO::PARAM_STR);
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':ip', $ip, PDO::PARAM_STR);
-    $stmt->bindValue(':time', $time, PDO::PARAM_INT);
-    
-    // execute the PDO
-    $stmt->execute();
+    $stmt->bindValue(':time', time(), PDO::PARAM_INT);
+    $result = $stmt->execute();
+
+    if ($result === false) {
+        throw new Exception('Could not log admin action');
+    }
+
+    return $result;
 }
