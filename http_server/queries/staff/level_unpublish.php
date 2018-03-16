7306307bf2898e97093c869936c23be618f788d0
Ben <bls1999@users.noreply.github.com>
2018-03-16 12:57:25 -0400

    error reporting -> level_unpublish.php

--- http_server/queries/staff/level_unpublish.php
+++ http_server/queries/staff/level_unpublish.php
@@ -4,5 +4,8 @@ function level_unpublish($pdo, $level_id)
 {
     $stmt = $pdo->prepare('UPDATE pr2_levels SET live = 0, pass = NULL WHERE level_id = ?');
     $stmt->bindValue(1, $level_ID, PDO::PARAM_INT);
-    $stmt->execute();
+    $result = $stmt->execute();
+    if ($result !== true) {
+        throw new Exception("Could not unpublish level.");
+    }
 }
