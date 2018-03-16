bd74b19b377bacde88e56c0af446fcd4808dedae
Ben <bls1999@users.noreply.github.com>
2018-03-16 13:27:13 -0400

    Update level_select.php

--- http_server/queries/levels/level_select.php
+++ http_server/queries/levels/level_select.php
@@ -2,18 +2,13 @@
 
 function level_select($pdo, $level_id)
 {
-	$stmt = $pdo->prepare('
-        SELECT *
-        FROM pr2_levels
-        WHERE level_id = :level_id
-        LIMIT 1
-    ');
-	$stmt->bindValue(':level_id', $level_id, PDO::PARAM_INT);
-	$stmt->execute();
+    $stmt = $pdo->prepare('SELECT * FROM pr2_levels WHERE level_id = :level_id LIMIT 1');
+    $stmt->bindValue(':level_id', $level_id, PDO::PARAM_INT);
+    $stmt->execute();
     $result = $stmt->fetch(PDO::FETCH_OBJ);
 
-    if (!$result) {
-        throw new Exception('level not found');
+    if ($result === false) {
+        throw new Exception('Could not find a level with that ID.');
     }
 
     return $result;
