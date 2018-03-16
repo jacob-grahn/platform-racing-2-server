2928c01a0e4889f31db13a4f9c839d9aa1e26684
Ben <bls1999@users.noreply.github.com>
2018-03-16 13:57:33 -0400

    Update part_awards_insert.php

--- http_server/queries/part_awards/part_awards_insert.php
+++ http_server/queries/part_awards/part_awards_insert.php
@@ -2,7 +2,7 @@
 
 function part_awards_insert($pdo, $user_id, $type, $part_id)
 {
-	$stmt = $pdo->prepare('
+    $stmt = $pdo->prepare('
         INSERT INTO part_awards
         SET user_id = :user_id,
             type = :type,
@@ -12,10 +12,10 @@ function part_awards_insert($pdo, $user_id, $type, $part_id)
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':type', $type, PDO::PARAM_STR);
     $stmt->bindValue(':part', $part, PDO::PARAM_STR);
-	$result = $stmt->execute();
+    $result = $stmt->execute();
 
-    if (!$result) {
-        throw new Exception('could not insert part_award');
+    if ($result === false) {
+        throw new Exception('Could not insert part_award');
     }
 
     return $result;
