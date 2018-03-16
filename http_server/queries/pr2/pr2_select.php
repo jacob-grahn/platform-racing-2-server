565967e809d52b122356e92c5eb33405cedcbd50
Ben <bls1999@users.noreply.github.com>
2018-03-16 14:00:10 -0400

    Update pr2_select.php

--- http_server/queries/pr2/pr2_select.php
+++ http_server/queries/pr2/pr2_select.php
@@ -2,21 +2,17 @@
 
 function pr2_select($pdo, $user_id)
 {
-	$stmt = $pdo->prepare('
-        SELECT *
-        FROM pr2
-        WHERE user_id = :user_id
-        LIMIT 1
-    ');
-	$stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-	$result = $stmt->execute();
-	if (!$result) {
-        throw new Exception('could not fetch from pr2');
+    $stmt = $pdo->prepare('SELECT * FROM pr2 WHERE user_id = :user_id LIMIT 1');
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception('Could not perform query to select all from pr2');
     }
 
     $row = $stmt->fetch(PDO::FETCH_OBJ);
-    if (!$row) {
-        throw new Exception('user pr2 row not found');
+    if ($row === false) {
+        throw new Exception('Could not find a pr2 row for this user');
     }
 
     return $row;
