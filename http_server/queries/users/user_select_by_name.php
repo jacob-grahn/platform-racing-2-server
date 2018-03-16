75c3414ed0900530dc09ec21a17dea9bc78882a8
Ben <bls1999@users.noreply.github.com>
2018-03-16 14:36:58 -0400

    Update user_select_by_name.php

--- http_server/queries/users/user_select_by_name.php
+++ http_server/queries/users/user_select_by_name.php
@@ -2,19 +2,29 @@
 
 function user_select_by_name($pdo, $name)
 {
-	$stmt = $pdo->prepare('
-        SELECT user_id, name, email, register_ip, ip, time, register_time,
-               power, status, read_message_id, guild, server_id
-        FROM users
-        WHERE name = :name
-        LIMIT 1
+    $stmt = $pdo->prepare('
+        SELECT user_id,
+               name,
+               email,
+               register_ip,
+               ip,
+               time,
+               register_time,
+               power,
+               status,
+               read_message_id,
+               guild,
+               server_id
+          FROM users
+          WHERE name = :name
+          LIMIT 1
     ');
-	$stmt->bindValue(':name', $name, PDO::PARAM_STR);
-	$stmt->execute();
+    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    $stmt->execute();
     $result = $stmt->fetch(PDO::FETCH_OBJ);
 
-    if (!$result) {
-        throw new Exception('user not found');
+    if ($result === false) {
+        throw new Exception('Could not find a user with that name.');
     }
 
     return $result;
