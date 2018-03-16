a326fb3b33dcfb08d28e515b39464ba08067804b
Ben <bls1999@users.noreply.github.com>
2018-03-16 14:03:35 -0400

    Update rank_token_upsert.php

--- http_server/queries/rank_tokens/rank_token_upsert.php
+++ http_server/queries/rank_tokens/rank_token_upsert.php
@@ -2,18 +2,17 @@
 
 function rank_token_upsert($pdo, $user_id, $token_count)
 {
-	$stmt = $pdo->prepare('
-        INSERT INTO rank_tokens
-        SET user_id = :user_id,
-            available_tokens = :token_count
-        ON DUPLICATE KEY UPDATE
-            available_tokens = :token_count
+    $stmt = $pdo->prepare('
+                    INSERT INTO rank_tokens
+                            SET user_id = :user_id,
+                                available_tokens = :token_count
+        ON DUPLICATE KEY UPDATE available_tokens = :token_count
     ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-	$stmt->bindValue(':token_count', $token_count, PDO::PARAM_INT);
-	$result = $stmt->execute();
+    $stmt->bindValue(':token_count', $token_count, PDO::PARAM_INT);
+    $result = $stmt->execute();
 
-    if (!$result) {
+    if ($result === false) {
         throw new Exception('rank_token_upsert error');
     }
 
