42937561f4cd828d6062483a90ae5c2b39300ac4
Ben <bls1999@users.noreply.github.com>
2018-03-16 14:05:52 -0400

    Update server_update_status.php

--- http_server/queries/servers/server_update_status.php
+++ http_server/queries/servers/server_update_status.php
@@ -2,22 +2,22 @@
 
 function server_update_status($pdo, $server_id, $status, $population, $happy_hour)
 {
-	$stmt = $pdo->prepare('
+    $stmt = $pdo->prepare('
         UPDATE servers
-		SET status = :status,
-        	population = :population,
-			happy_hour = :happy_hour
-        WHERE server_id = :server_id
-        LIMIT 1
+           SET status = :status,
+               population = :population,
+               happy_hour = :happy_hour
+         WHERE server_id = :server_id
+         LIMIT 1
     ');
-	$stmt->bindValue(':server_id', $server_id, PDO::PARAM_INT);
+    $stmt->bindValue(':server_id', $server_id, PDO::PARAM_INT);
     $stmt->bindValue(':status', $status, PDO::PARAM_STR);
-	$stmt->bindValue(':population', $population, PDO::PARAM_INT);
-	$stmt->bindValue(':happy_hour', $happy_hour, PDO::PARAM_INT);
-	$result = $stmt->execute();
+    $stmt->bindValue(':population', $population, PDO::PARAM_INT);
+    $stmt->bindValue(':happy_hour', $happy_hour, PDO::PARAM_INT);
+    $result = $stmt->execute();
 
-    if (!$result) {
-        throw new Exception('could not update server status');
+    if ($result === false) {
+        throw new Exception('Could not update server status');
     }
 
     return $result;
