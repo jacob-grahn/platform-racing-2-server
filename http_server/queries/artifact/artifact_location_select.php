0827a65186ea0ba5c99cba721408b0e201d07d20
Ben <bls1999@users.noreply.github.com>
2018-03-16 12:59:33 -0400

    slight changes -> artifact_location_select.php

--- http_server/queries/artifact/artifact_location_select.php
+++ http_server/queries/artifact/artifact_location_select.php
@@ -2,16 +2,12 @@
 
 function artifact_location_select($pdo)
 {
-	$stmt = $pdo->prepare('
-        SELECT *
-        FROM artifact_location
-        LIMIT 1
-    ');
-	$stmt->execute();
+    $stmt = $pdo->prepare('SELECT * FROM artifact_location LIMIT 1');
+    $stmt->execute();
     $result = $stmt->fetch(PDO::FETCH_OBJ);
 
-    if (!$result) {
-        throw new Exception('artifact not found');
+    if ($result == false) {
+        throw new Exception('Could not retrieve artifact location.');
     }
 
     return $result;
