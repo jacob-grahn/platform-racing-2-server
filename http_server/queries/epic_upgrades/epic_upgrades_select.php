38cea45f433080ca958a4145ff3584d375f1dae5
Ben <bls1999@users.noreply.github.com>
2018-03-16 13:08:38 -0400

    Update epic_upgrades_select.php

--- http_server/queries/epic_upgrades/epic_upgrades_select.php
+++ http_server/queries/epic_upgrades/epic_upgrades_select.php
@@ -2,21 +2,16 @@
 
 function epic_upgrades_select($pdo, $user_id)
 {
-	$stmt = $pdo->prepare('
-        SELECT *
-        FROM epic_upgrades
-        WHERE user_id = :user_id
-        LIMIT 1
-    ');
-	$stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-	$result = $stmt->execute();
-	if (!$result) {
-        throw new Exception('could not fetch from epic_upgrades');
+    $stmt = $pdo->prepare('SELECT * FROM epic_upgrades WHERE user_id = :user_id LIMIT 1');
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $result = $stmt->execute();
+    if ($result === false) {
+        throw new Exception('Could not get data from epic_upgrades');
     }
 
     $row = $stmt->fetch(PDO::FETCH_OBJ);
-    if (!$row) {
-        throw new Exception('epic_upgrade row not found');
+    if ($row === false) {
+        throw new Exception('Could not find row in epic_upgrades');
     }
 
     return $row;
