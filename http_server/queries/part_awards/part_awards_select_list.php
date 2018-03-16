9acd8c49eb96ba9b00ec39553a7a2e1692b2eb90
Ben <bls1999@users.noreply.github.com>
2018-03-16 13:58:37 -0400

    Update part_awards_select_list.php

--- http_server/queries/part_awards/part_awards_select_list.php
+++ http_server/queries/part_awards/part_awards_select_list.php
@@ -2,15 +2,12 @@
 
 function part_awards_select_list($pdo)
 {
-	$stmt = $pdo->prepare('
-        SELECT user_id, type, part
-        FROM part_awards
-    ');
-	$stmt->execute();
+    $stmt = $pdo->prepare('SELECT user_id, type, part FROM part_awards');
+    $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-    if (!$result) {
-        throw new Exception('could not fetch part award list');
+    if ($result === false) {
+        throw new Exception('Could not fetch part award list');
     }
 
     return $result;
