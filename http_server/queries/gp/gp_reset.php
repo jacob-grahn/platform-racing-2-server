9b7747ef3d79256649afec6a2bac5e2cb3f34fee
Ben <bls1999@users.noreply.github.com>
2018-03-16 13:20:18 -0400

    Update gp_reset.php

--- http_server/queries/gp/gp_reset.php
+++ http_server/queries/gp/gp_reset.php
@@ -2,13 +2,10 @@
 
 function gp_reset($pdo)
 {
-	$result = $pdo->exec('
-        UPDATE gp
-        SET gp_today = 0
-    ');
+    $result = $pdo->exec('UPDATE gp SET gp_today = 0');
 
     if ($result === false) {
-        throw new Exception('could not reset gp gp_today');
+        throw new Exception('Could not reset gp_today');
     }
 
     return $result;
