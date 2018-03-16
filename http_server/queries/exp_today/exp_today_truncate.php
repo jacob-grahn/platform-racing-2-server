a3ee5a7a7eba0363ecb34e5606bbd7a73ce0c3fa
Ben <bls1999@users.noreply.github.com>
2018-03-16 13:10:21 -0400

    Update exp_today_truncate.php

--- http_server/queries/exp_today/exp_today_truncate.php
+++ http_server/queries/exp_today/exp_today_truncate.php
@@ -2,10 +2,10 @@
 
 function exp_today_truncate($pdo)
 {
-	$result = $pdo->exec('TRUNCATE TABLE exp_today');
+    $result = $pdo->exec('TRUNCATE TABLE exp_today');
 
     if ($result === false) {
-        throw new Exception('could not truncate exp_today');
+        throw new Exception('Could not truncate exp_today');
     }
 
     return $result;
