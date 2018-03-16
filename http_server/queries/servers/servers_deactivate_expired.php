61505b471afb61e889aa0e92243e5bfd824cb526
Ben <bls1999@users.noreply.github.com>
2018-03-16 14:06:44 -0400

    Update servers_deactivate_expired.php

--- http_server/queries/servers/servers_deactivate_expired.php
+++ http_server/queries/servers/servers_deactivate_expired.php
@@ -2,16 +2,11 @@
 
 function servers_deactivate_expired($pdo)
 {
-	$stmt = $pdo->prepare('
-		UPDATE servers
-		SET active = 0,
-			status = "offline"
-		WHERE expire_date < NOW()
-    ');
-	$result = $stmt->execute();
+    $stmt = $pdo->prepare('UPDATE servers SET active = 0, status = "offline" WHERE expire_date < NOW()');
+    $result = $stmt->execute();
 
-    if (!$result) {
-        throw new Exception('could deactivate expired servers');
+    if ($result === false) {
+        throw new Exception('Could not deactivate expired servers');
     }
 
     return $result;
