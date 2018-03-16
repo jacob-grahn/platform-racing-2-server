fc9f12ed608c0f32a0f7274ee1351798f4ff23e8
Ben <bls1999@users.noreply.github.com>
2018-03-16 13:53:31 -0400

    Update messages_select_recent.php

--- http_server/queries/messages/messages_select_recent.php
+++ http_server/queries/messages/messages_select_recent.php
@@ -2,18 +2,19 @@
 
 function messages_select_recent($pdo, $min_message_id)
 {
-	$stmt = $pdo->prepare('
-        SELECT to_user_id, message_id
-        FROM messages
-        WHERE message_id > :min_message_id
-		ORDER BY message_id ASC
-        LIMIT 50
+    $stmt = $pdo->prepare('
+        SELECT to_user_id,
+               message_id
+          FROM messages
+         WHERE message_id > :min_message_id
+      ORDER BY message_id ASC
+         LIMIT 50
     ');
     $stmt->bindValue(':min_message_id', $min_message_id, PDO::PARAM_INT);
-	$result = $stmt->execute();
+    $result = $stmt->execute();
 
-    if (!$result) {
-        throw new Exception('could not select recent messages');
+    if ($result === false) {
+        throw new Exception('Could not select recent messages');
     }
 
     return $stmt->fetchAll(PDO::FETCH_OBJ);
