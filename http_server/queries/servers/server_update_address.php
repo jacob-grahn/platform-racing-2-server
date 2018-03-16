a6e923a4d97f0965e4a7bd162c728ca14be28962
Ben <bls1999@users.noreply.github.com>
2018-03-16 14:04:51 -0400

    Update server_update_address.php

--- http_server/queries/servers/server_update_address.php
+++ http_server/queries/servers/server_update_address.php
@@ -2,18 +2,18 @@
 
 function server_update_address($pdo, $server_id, $address)
 {
-	$stmt = $pdo->prepare('
+    $stmt = $pdo->prepare('
         UPDATE servers
-        SET address = :address
-        WHERE server_id = :server_id
-        LIMIT 1
+           SET address = :address
+         WHERE server_id = :server_id
+         LIMIT 1
     ');
     $stmt->bindValue(':server_id', $server_id, PDO::PARAM_INT);
-	$stmt->bindValue(':address', $address, PDO::PARAM_STR);
-	$result = $stmt->execute();
+    $stmt->bindValue(':address', $address, PDO::PARAM_STR);
+    $result = $stmt->execute();
 
-    if (!$result) {
-        throw new Exception('could not update server address');
+    if ($result === false) {
+        throw new Exception('Could not update server address');
     }
 
     return $result;
