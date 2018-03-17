33492d59035b5e2d587f7205cacfc8063f2374bd
Ben <bls1999@users.noreply.github.com>
2018-03-17 16:16:12 -0400

    move users_reset_status from monthly

--- http_server/cron/monthly.php
+++ http_server/cron/monthly.php
@@ -1,14 +1,12 @@
 <?php
 
 require_once __DIR__ . '/../fns/all_fns.php';
-require_once __DIR__ . '/../queries/users/users_reset_status.php';
 require_once __DIR__ . '/../queries/best_levels/best_levels_populate.php';
 require_once __DIR__ . '/../queries/messages/messages_delete_old.php';
 require_once __DIR__ . '/../queries/misc/all_optimize.php';
 
 $pdo = pdo_connect();
 
-users_reset_status($pdo);
 best_levels_populate($pdo);
 messages_delete_old($pdo);
 all_optimize($pdo);
