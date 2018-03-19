af165ad3d9193192b3ac132a8e25e78a995be29e
Ben <bls1999@users.noreply.github.com>
2018-03-18 20:53:26 -0400

    Create admin_account_update.php

---
+++ http_server/queries/staff/admin/admin_account_update.php
@@ -0,0 +1,73 @@
+<?php
+
+function admin_user_update($pdo, $user_id, $name, $email, $guild_id)
+{
+    $stmt = $pdo->prepare('
+        UPDATE users
+           SET name = :name,
+               email = :email,
+               guild_id = :guild_id
+         WHERE user_id = :user_id
+        ');
+    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+    $stmt->bindValue(':guild_id', $guild_id, PDO::PARAM_INT);
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception("Could not update user data.");
+    }
+    
+    return true;
+}
+
+function admin_pr2_update($pdo, $user_id, $hats, $heads, $bodies, $feet)
+{
+    $stmt = $pdo->prepare('
+        UPDATE pr2
+           SET hat_array = :hats,
+               head_array = :heads,
+               body_array = :bodies,
+               feet_array = :feet
+         WHERE user_id = :user_id
+        ');
+    $stmt->bindValue(':hats', $hats, PDO::PARAM_STR);
+    $stmt->bindValue(':heads', $heads, PDO::PARAM_STR);
+    $stmt->bindValue(':bodies', $bodies, PDO::PARAM_STR);
+    $stmt->bindValue(':feet', $feet, PDO::PARAM_STR);
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception("Could not update PR2 data.");
+    }
+    
+    return true;
+}
+
+function admin_epic_upgrades_update($pdo, $user_id, $ehats, $eheads, $ebodies, $efeet)
+{
+    $stmt = $pdo->prepare('
+        UPDATE pr2
+           SET epic_hats = :ehats,
+               epic_heads = :eheads,
+               epic_bodies = :ebodies,
+               epic_feet = :efeet
+         WHERE user_id = :user_id
+        ');
+    $stmt->bindValue(':ehats', $ehats, PDO::PARAM_STR);
+    $stmt->bindValue(':eheads', $eheads, PDO::PARAM_STR);
+    $stmt->bindValue(':ebodies', $ebodies, PDO::PARAM_STR);
+    $stmt->bindValue(':efeet', $efeet, PDO::PARAM_STR);
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception("Could not update epic upgrades data.");
+    }
+    
+    return true;
+}
+
+?>
