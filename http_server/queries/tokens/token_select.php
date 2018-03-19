fb3e36788bd207920290825393917654e6e48c63
Jacob Grahn <contact@jacobgrahn.com>
2018-03-19 08:17:34 -0400

    fix query?

--- http_server/queries/tokens/token_select.php
+++ http_server/queries/tokens/token_select.php
@@ -2,7 +2,7 @@
 
 function token_select($pdo, $token_id)
 {
-    $result = $pdo->prepare('SELECT user_id, token FROM tokens WHERE token = :token_id LIMIT 0, 1');
+    $stmt = $pdo->prepare('SELECT user_id, token FROM tokens WHERE token = :token_id LIMIT 0, 1');
     $stmt->bindValue(':token_id', $token_id, PDO::PARAM_STR);
     $stmt->execute();
     $row = $stmt->fetch(PDO::FETCH_OBJ);
