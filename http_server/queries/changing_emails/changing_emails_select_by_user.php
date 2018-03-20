6a551d5fb109b1cdb78b76949bada8a794f9ae56
Ben <bls1999@users.noreply.github.com>
2018-03-20 14:37:13 -0400

    Order by change_id

--- http_server/queries/changing_emails/changing_emails_select_by_user.php
+++ http_server/queries/changing_emails/changing_emails_select_by_user.php
@@ -3,9 +3,10 @@
 function changing_emails_select_by_user($pdo, $user_id, $suppress_error = false)
 {
     $stmt = $pdo->prepare('
-        SELECT change_id, old_email, new_email, code, date, request_ip, confirm_ip, status
-          FROM changing_emails
-         WHERE user_id = :user_id
+          SELECT change_id, old_email, new_email, code, date, request_ip, confirm_ip, status
+            FROM changing_emails
+           WHERE user_id = :user_id
+        ORDER BY change_id ASC
     ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
 
