723a5ef30e148d98ca6f97e971c51e378a7f6829
Ben <bls1999@users.noreply.github.com>
2018-03-19 23:37:25 -0400

    Create guild_transfer_select.php

---
+++ http_server/queries/guild_transfers/guild_transfer_select.php
@@ -0,0 +1,25 @@
+<?php
+
+function guild_transfer_select($pdo, $code)
+{
+    $stmt = $pdo->prepare('
+        SELECT *
+          FROM guild_transfers
+         WHERE code = :code
+           AND status = "pending"
+         LIMIT 1
+    ');
+    $stmt->bindValue(':code', $code, PDO::PARAM_STR);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception('Could fetch guild transfer request');
+    }
+    
+    $row = $stmt->fetch(PDO::FETCH_OBJ);
+    if ($row === false) {
+        throw new Exception('No pending guild transfer found for that code.');
+    }
+    
+    return $row;
+}
