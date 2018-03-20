a47c1dfd6ff1db45d124c48004c0db6f9cb32e40
Ben <bls1999@users.noreply.github.com>
2018-03-19 23:49:39 -0400

    Create guild_transfer_complete.php

---
+++ http_server/queries/guild_transfers/guild_transfer_complete.php
@@ -0,0 +1,20 @@
+<?php
+
+function guild_transfer_complete($pdo, $transfer_id, $ip)
+{
+    $stmt = $pdo->prepare('
+        UPDATE guild_transfers
+           SET status = "complete",
+               confirm_ip = :ip
+         WHERE transfer_id = :transfer_id
+    ');
+    $stmt->bindValue(':transfer_id', $change_id, PDO::PARAM_INT);
+    $stmt->bindValue(':ip', $ip, PDO::PARAM_STR);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception('Could not complete the guild transfer.');
+    }
+
+    return $result;
+}
