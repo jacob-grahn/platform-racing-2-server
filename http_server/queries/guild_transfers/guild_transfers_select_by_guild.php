a1f7ef122bb8eacc175c595b5d7539a1e3417140
Ben <bls1999@users.noreply.github.com>
2018-03-20 11:23:51 -0400

    Update guild_transfers_select_by_guild.php

--- http_server/queries/guild_transfers/guild_transfers_select_by_guild.php
+++ http_server/queries/guild_transfers/guild_transfers_select_by_guild.php
@@ -1,19 +1,28 @@
 <?php
 
-function guild_transfers_select_by_guild($pdo, $guild_id)
+function guild_transfers_select_by_guild($pdo, $guild_id, $suppress_error = false)
 {
     $stmt = $pdo->prepare('
         SELECT transfer_id, old_owner_id, new_owner_id, code, date, request_ip, confirm_ip, status
           FROM guild_transfers
          WHERE guild_id = :guild_id
-           AND status = "complete";
     ');
     $stmt->bindValue(':guild_id', $guild_id, PDO::PARAM_INT);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
-        throw new Exception('Could not fetch guild transfer history');
+        throw new Exception('Could not fetch guild transfer history.');
+    }
+    
+    $transfers = $stmt->fetchAll(PDO::FETCH_OBJ);
+    
+    if ($transfers === false) {
+        if ($suppress_error === false) {
+            throw new Exception('Could not find any guild transfers for this guild.');
+        } else {
+            return false;
+        }
     }
 
-    return $stmt->fetchAll(PDO::FETCH_OBJ);
+    return $transfers;
 }
