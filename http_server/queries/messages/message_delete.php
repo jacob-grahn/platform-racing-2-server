fe7fb58b35711d15b7cdb6c46da545e3428d9459
Ben <bls1999@users.noreply.github.com>
2018-03-21 16:31:36 -0400

    fix message deletion

--- http_server/queries/messages/message_delete.php
+++ http_server/queries/messages/message_delete.php
@@ -8,12 +8,12 @@ function message_delete($pdo, $user_id, $message_id)
         AND message_id = :message_id
         LIMIT 1
     ');
-    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->bindValue(':to_user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':message_id', $message_id, PDO::PARAM_INT);
 
     $result = $stmt->execute();
-    if (!$result) {
-        throw new Exception('could not delete message');
+    if ($result === false) {
+        throw new Exception('Could not delete message.');
     }
 
     return $result;
