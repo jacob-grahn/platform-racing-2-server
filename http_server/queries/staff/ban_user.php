22cc58e3cfaa20dfd37da9bc708386dd9af4303b
Ben <bls1999@users.noreply.github.com>
2018-03-21 00:59:41 -0400

    clean up queries -> ban_user.php

--- http_server/queries/staff/ban_user.php
+++ http_server/queries/staff/ban_user.php
@@ -2,13 +2,22 @@
 
 function throttle_bans($pdo, $mod_user_id)
 {
-    $time = (int) (time() - 3600);
+    $throttle_time = (int) (time() - 3600);
   
-    $stmt = $pdo->prepare('SELECT COUNT(*) as recent_ban_count FROM bans WHERE mod_user_id = :mod AND time > UNIX_TIMESTAMP(NOW() - INTERVAL 1 HOUR)');
+    $stmt = $pdo->prepare("
+        SELECT COUNT(*) as recent_ban_count
+	  FROM bans
+	 WHERE mod_user_id = :mod
+	   AND time > $throttle_time
+    ");
     $stmt->bindValue(':mod', $mod_user_id, PDO::PARAM_INT);
-    $stmt->execute();
-    $row = $stmt->fetchAll(PDO::FETCH_OBJ);
-    return $row;
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+    	throw new Exception("Could not query ban throttle.");
+    }
+
+    return $stmt->fetchAll(PDO::FETCH_OBJ);
 }
 
 function ban_user($pdo, $banned_ip, $banned_user_id, $mod_user_id, $expire_time, $reason, $record, $banned_name, $mod_name, $ip_ban, $account_ban)
