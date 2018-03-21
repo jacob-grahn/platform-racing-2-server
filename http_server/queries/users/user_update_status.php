0b795507e55db8be52e925a395ed9d04ed44b314
Jacob Grahn <contact@jacobgrahn.com>
2018-03-21 12:59:08 -0400

    add missing query

--- http_server/queries/users/user_update_status.php
+++ http_server/queries/users/user_update_status.php
@@ -1,18 +1,21 @@
 <?php
 
-function users_update_status($pdo, $user_id, $status, $server_id)
+function user_update_status($pdo, $user_id, $status, $server_id)
 {
     $stmt = $pdo->prepare('
         UPDATE users
         SET time = UNIX_TIMESTAMP(NOW()),
     		active_date = NOW(),
-    		status = p_status,
-    		server_id = p_server_id
-        WHERE user_id = p_user_id
+    		status = :status,
+    		server_id = :server_id
+        WHERE user_id = :user_id
         LIMIT 1'
     );
-    $result = $stmt->execute();
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->bindValue(':status', $status, PDO::PARAM_STR);
+    $stmt->bindValue(':server_id', $server_id, PDO::PARAM_INT);
 
+    $result = $stmt->execute();
     if ($result === false) {
         throw new Exception('Error updating user status');
     }
