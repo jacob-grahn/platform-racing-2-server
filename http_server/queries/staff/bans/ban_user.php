2dfddaa59563cad0b3a18ef67c207500655ac6bd
Ben <bls1999@users.noreply.github.com>
2018-03-21 01:06:51 -0400

    Update and rename http_server/queries/staff/ban_user.php to http_server/queries/staff/bans/ban_user.php

--- http_server/queries/staff/bans/ban_user.php
+++ http_server/queries/staff/bans/ban_user.php
@@ -1,25 +1,5 @@
 <?php
 
-function throttle_bans($pdo, $mod_user_id)
-{
-    $throttle_time = (int) (time() - 3600);
-  
-    $stmt = $pdo->prepare("
-        SELECT COUNT(*) as recent_ban_count
-	  FROM bans
-	 WHERE mod_user_id = :mod
-	   AND time > $throttle_time
-    ");
-    $stmt->bindValue(':mod', $mod_user_id, PDO::PARAM_INT);
-    $result = $stmt->execute();
-    
-    if ($result === false) {
-    	throw new Exception("Could not query ban throttle.");
-    }
-
-    return $stmt->fetchAll(PDO::FETCH_OBJ);
-}
-
 function ban_user($pdo, $banned_ip, $banned_user_id, $mod_user_id, $expire_time, $reason, $record, $banned_name, $mod_name, $ip_ban, $account_ban)
 {
     $stmt = $pdo->prepare('
