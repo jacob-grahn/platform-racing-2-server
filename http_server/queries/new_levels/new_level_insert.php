c5e10d0124144001be6af697f0904716e5096992
Jacob Grahn <contact@jacobgrahn.com>
2018-03-22 12:49:08 -0400

    fix typo

--- http_server/queries/new_levels/new_level_insert.php
+++ http_server/queries/new_levels/new_level_insert.php
@@ -2,7 +2,7 @@
 
 function new_level_insert ($pdo, $level_id, $time, $ip)
 {
-    $result = $pdo->exec('
+    $stmt = $pdo->prepare('
         REPLACE INTO pr2_new_levels
         SET level_id = :level_id,
             time = :time,
