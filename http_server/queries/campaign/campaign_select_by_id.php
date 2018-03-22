509f01a16385832267aeaea3a849031d48cb9654
Ben <bls1999@users.noreply.github.com>
2018-03-21 22:41:31 -0400

    fix campaign_select_by_id

--- http_server/queries/campaign/campaign_select_by_id.php
+++ http_server/queries/campaign/campaign_select_by_id.php
@@ -9,11 +9,11 @@ function campaign_select_by_id ($pdo, $campaign_id)
         ORDER BY level_num
     ');
     $stmt->bindValue(':campaign_id', $campaign_id, PDO::PARAM_INT);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
-        throw new Exception('Could not select campaign.');
+        throw new Exception('Could not perform query campaign_select_by_id.');
     }
 
-    return $stmt->fetchAll();
+    return $stmt->fetchAll(PDO::FETCH_OBJ);
 }
