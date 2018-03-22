875a86b42c183cd11c85f9537667dff2575677d1
Ben <bls1999@users.noreply.github.com>
2018-03-22 13:17:46 -0400

    hopefully fix messages_select?

--- http_server/queries/messages/messages_select.php
+++ http_server/queries/messages/messages_select.php
@@ -2,20 +2,23 @@
 
 function messages_select($pdo, $to_user_id, $start, $count)
 {
+    $start = (int) $start;
+    $count = (int) $count;
+    
     $stmt = $pdo->prepare('
-        SELECT messages.message_id, messages.message, messages.time, messages.from_user_id
+        SELECT message_id, message, time, from_user_id
         FROM messages
-        WHERE messages.to_user_id = :to_user_id
-        ORDER BY messages.time desc
+        WHERE to_user_id = :to_user_id
+        ORDER BY time desc
         LIMIT :start, :count
     ');
     $stmt->bindValue(':to_user_id', $to_user_id, PDO::PARAM_INT);
     $stmt->bindValue(':start', $start, PDO::PARAM_INT);
     $stmt->bindValue(':count', $count, PDO::PARAM_INT);
-
     $result = $stmt->execute();
-    if (!$result) {
-        throw new Exception('could not select messages');
+    
+    if ($result === false) {
+        throw new Exception('Could not retrieve your private messages.');
     }
 
     return $stmt->fetchAll(PDO::FETCH_OBJ);
