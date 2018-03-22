4bf88bbfe778987818eec71f27dbe48ffe1aa6c1
Ben <bls1999@users.noreply.github.com>
2018-03-21 22:21:07 -0400

    update errors -> guild_select_by_name.php

--- http_server/queries/guilds/guild_select_by_name.php
+++ http_server/queries/guilds/guild_select_by_name.php
@@ -9,15 +9,16 @@ function guild_select_by_name($pdo, $guild_name)
         LIMIT 1
     ');
     $stmt->bindValue(':guild_name', $guild_name, PDO::PARAM_STR);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
-        throw new Exception('Could not fetch guild by name');
+        throw new Exception('Could not perform query guild_select_by_name.');
     }
 
     $guild = $stmt->fetch(PDO::FETCH_OBJ);
+    
     if ($guild === false) {
-        throw new Exception('Guild not found');
+        throw new Exception('Could not find a guild with that name.');
     }
 
     return $guild;
