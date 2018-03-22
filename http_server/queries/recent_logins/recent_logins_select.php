9c1df929c39f6d9457c740ff3bf0be4a5570683e
Ben <bls1999@users.noreply.github.com>
2018-03-21 23:02:51 -0400

    add $start

--- http_server/queries/recent_logins/recent_logins_select.php
+++ http_server/queries/recent_logins/recent_logins_select.php
@@ -1,21 +1,24 @@
 <?php
 
-function recent_logins_select($pdo, $user_id, $suppress_error = false, $count = 100)
+function recent_logins_select($pdo, $user_id, $suppress_error = false, $start = 0, $count = 100)
 {
+    $start = (int) $start;
     $count = (int) $count;
+    
     $stmt = $pdo->prepare('
         SELECT * FROM recent_logins
         WHERE user_id = :user_id
         ORDER BY date DESC
-        LIMIT 0 , :count
+        LIMIT :start , :count
     ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->bindValue(':start', $start, PDO::PARAM_INT);
     $stmt->bindValue(':count', $count, PDO::PARAM_INT);
     $result = $stmt->execute();
 
-    if($result === false) {
+    if ($result === false) {
         if ($suppress_error === false) {
-            throw new Exception("Could not find any recent logins for this user.");
+            throw new Exception("Could not perform query recent_logins_select.");
         } else {
             return false;
         }
