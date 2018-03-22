49084da8582cc4624b9fa49e8e43b95c8c271993
Ben <bls1999@users.noreply.github.com>
2018-03-22 15:38:34 -0400

    fix query

--- http_server/www/mod/reported_messages.php
+++ http_server/www/mod/reported_messages.php
@@ -2,13 +2,13 @@
 
 require_once __DIR__ . '/../../fns/all_fns.php';
 require_once __DIR__ . '/../../fns/output_fns.php';
+require_once __DIR__ . '/../../queries/messages_reported/messages_reported_select.php';
 
 $start = (int) default_get('start', 0);
 $count = (int) default_get('count', 25);
 
 $next_form_id = 1;
 $mod_ip = get_ip();
-
 
 try {
     // rate limiting
