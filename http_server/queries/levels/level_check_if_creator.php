9d934170553cdb057c7e548b5c59b13a6b7ccac5
Ben <bls1999@users.noreply.github.com>
2018-03-24 15:09:24 -0400

    Create level_check_if_creator.php

---
+++ http_server/queries/levels/level_check_if_creator.php
@@ -0,0 +1,27 @@
+<?php
+
+function level_check_if_creator($pdo, $user_id, $level_id)
+{
+    $stmt = $pdo->prepare('
+        SELECT level_id
+        FROM pr2_levels
+        WHERE user_id = :user_id
+        AND level_id = :level_id
+        LIMIT 1
+    ');
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $stmt->bindValue(':level_id', $level_id, PDO::PARAM_INT);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception('Could not perform query level_check_if_creator.');
+    }
+    
+    $level = $stmt->fetch(PDO::FETCH_OBJ);
+    
+    if ((int) $level == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
