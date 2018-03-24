ca8499c4d7dd3e53991d07fdd4897124c150f2ed
Ben <bls1999@users.noreply.github.com>
2018-03-23 20:09:26 -0400

    Create guild_count_members.php

---
+++ http_server/queries/guilds/guild_count_members.php
@@ -0,0 +1,24 @@
+<?php
+
+function guild_count_members($pdo, $guild_id)
+{
+    $stmt = $pdo->prepare('
+        SELECT COUNT(*)
+        FROM users
+        WHERE guild = :guild_id
+    ');
+    $stmt->bindValue(':guild_id', $guild_id, PDO::PARAM_INT);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception("Could not count the number of members in guild $guild_id.");
+    }
+
+    $count = $stmt->fetchColumn();
+    
+    if ((int) $count == 0 || $count == false) {
+        return 0;
+    }
+    
+    return $count;
+}
