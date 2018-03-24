ecdd82ff3fea4a877439b96f3dee1628d21b3100
Ben <bls1999@users.noreply.github.com>
2018-03-23 20:26:40 -0400

    add value for a member count update
    It's not pretty, but it works.

--- http_server/queries/guilds/guild_update.php
+++ http_server/queries/guilds/guild_update.php
@@ -1,10 +1,19 @@
 <?php
 
-function guild_update($pdo, $guild_id, $guild_name, $emblem, $note, $owner_id)
+function guild_update($pdo, $guild_id, $guild_name, $emblem, $note, $owner_id, $member_count = NULL)
 {
+    // check for member count passed to this function
+    $memcount_sql = '';
+    if ($member_count != NULL) {
+        $member_count = (int) $member_count;
+        $memcount_sql = 'member_count = :member_count,';
+    }
+    
+    // do it
     $stmt = $pdo->prepare('
         UPDATE guilds
            SET guild_name = :name,
+               $memcount_sql
                emblem = :emblem,
                note = :note,
                owner_id = :owner_id
@@ -16,6 +25,9 @@ function guild_update($pdo, $guild_id, $guild_name, $emblem, $note, $owner_id)
     $stmt->bindValue(':note', $note, PDO::PARAM_STR);
     $stmt->bindValue(':owner_id', $owner_id, PDO::PARAM_INT);
     $stmt->bindValue(':guild_id', $guild_id, PDO::PARAM_INT);
+    if ($member_count != NULL) {
+        $stmt->bindValue(':member_count', $member_count, PDO::PARAM_INT);
+    }
     $result = $stmt->execute();
     
     if ($result === false) {
