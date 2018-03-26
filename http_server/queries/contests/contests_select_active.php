ac6b673bce078b16df4a032ff6cfa7891033da72
Ben <bls1999@users.noreply.github.com>
2018-03-26 00:18:18 -0400

    Create contests_select_active.php

---
+++ http_server/queries/contests/contests_select_active.php
@@ -0,0 +1,23 @@
+<?php
+
+function contests_select_active($pdo)
+{
+    $stmt = $pdo->prepare('
+        SELECT contest_id, contest_name, description, url
+        FROM contests
+        WHERE active = 1
+    ');
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception('Could not select active contests.');
+    }
+    
+    $contests = $stmt->fetchAll(PDO::FETCH_OBJ);
+    
+    if (empty($contests)) {
+        return false;
+    }
+    
+    return $contests;
+}
