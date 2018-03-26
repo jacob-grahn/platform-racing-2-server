bc7bd2f821b9ecafc777c16064c06f4f262b6068
Ben <bls1999@users.noreply.github.com>
2018-03-26 01:12:36 -0400

    Create prize_delete.php

---
+++ http_server/queries/contest_prizes/prize_delete.php
@@ -0,0 +1,17 @@
+<?php
+
+function prize_delete($pdo, $prize_id)
+{
+    $stmt = $pdo->prepare('
+        DELETE FROM contest_prizes
+        WHERE prize_id = :prize_id
+    ');
+    $stmt->bindValue(':prize_id', $prize_id, PDO::PARAM_INT);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception('Could not delete prize.');
+    }
+    
+    return true;
+}
