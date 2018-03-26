31ddfad90da4d78c06a3c2843d232e5dd92dc755
Ben <bls1999@users.noreply.github.com>
2018-03-25 23:31:05 -0400

    add $active

--- http_server/queries/contests/contest_update.php
+++ http_server/queries/contests/contest_update.php
@@ -1,19 +1,21 @@
 <?php
 
-function contest_insert($pdo, $name, $desc, $url, $host_id, $contest_id)
+function contest_insert($pdo, $name, $desc, $url, $host_id, $active, $contest_id)
 {
     $stmt = $pdo->prepare('
         UPDATE contests
            SET contest_name = :name,
                description = :desc,
                url = :url,
-               user_id = :host_id
+               user_id = :host_id,
+               active = :active
          WHERE contest_id = :contest_id
     ');
     $stmt->bindValue(':name', $name, PDO::PARAM_STR);
     $stmt->bindValue(':desc', $desc, PDO::PARAM_STR);
     $stmt->bindValue(':url', $url, PDO::PARAM_STR);
     $stmt->bindValue(':host_id', $host_id, PDO::PARAM_INT);
+    $stmt->bindValue(':active', $active, PDO::PARAM_INT);
     $stmt->bindValue(':contest_id', $contest_id, PDO::PARAM_INT);
     $result = $stmt->execute();
     
