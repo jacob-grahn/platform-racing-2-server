e2afbafdbf55536eee972d5bfd31f803b18943c1
Ben <bls1999@users.noreply.github.com>
2018-03-26 16:40:46 -0400

    is this needed -> stats_select_all

--- http_server/queries/fah/stats/stats_select_all.php
+++ http_server/queries/fah/stats/stats_select_all.php
@@ -1,12 +1,13 @@
 <?php
 
+// TO-DO: is this needed?
 function stats_select_all($pdo)
 {
     $stmt = $pdo->prepare('SELECT * FROM stats');
-
     $result = $stmt->execute();
+    
     if ($result === false) {
-        throw new Exception('Could not select fah stats');
+        throw new Exception('Could not select all folding at home stats.');
     }
 
     return $stmt->fetchAll(PDO::FETCH_OBJ);
