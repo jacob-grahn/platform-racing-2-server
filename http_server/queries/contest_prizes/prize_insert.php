ced78caae27bd51f66b7e64619e16b8f265ba96c
Ben <bls1999@users.noreply.github.com>
2018-03-26 01:07:32 -0400

    Update prize_insert.php

--- http_server/queries/contest_prizes/prize_insert.php
+++ http_server/queries/contest_prizes/prize_insert.php
@@ -1,24 +1,24 @@
 <?php
 
-function prize_insert($pdo, $contest_id, $prize_type, $prize_id)
+function prize_insert($pdo, $contest_id, $part_type, $part_id)
 {
-    $prize_type = strtolower($prize_type);
+    $part_type = strtolower($part_type);
 
     $stmt = $pdo->prepare('
         INSERT INTO contest_prizes
                 SET contest_id = :contest_id,
-                    prize_type = :type,
-                    prize_id = :prize_id,
+                    part_type = :part_type,
+                    part_id = :part_id,
                     added = :added
     ');
     $stmt->bindValue(':contest_id', $contest_id, PDO::PARAM_INT);
-    $stmt->bindValue(':type', $prize_type, PDO::PARAM_STR);
-    $stmt->bindValue(':prize_id', $prize_id, PDO::PARAM_INT);
+    $stmt->bindValue(':part_type', $part_type, PDO::PARAM_STR);
+    $stmt->bindValue(':part_id', $part_id, PDO::PARAM_INT);
     $stmt->bindValue(':added', time(), PDO::PARAM_INT);
     $result = $stmt->execute();
     
     if ($result === false) {
-        throw new Exception("Could not add prize to contest $contest_id.");
+        throw new Exception("Could not add prize to contest #$contest_id.");
     }
     
     return true;
