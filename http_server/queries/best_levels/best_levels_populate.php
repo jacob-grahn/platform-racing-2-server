c4795a0c157229b16c12d417d3fb1dab9f535354
Ben <bls1999@users.noreply.github.com>
2018-03-26 16:22:06 -0400

    Update best_levels_populate.php

--- http_server/queries/best_levels/best_levels_populate.php
+++ http_server/queries/best_levels/best_levels_populate.php
@@ -2,16 +2,15 @@
 
 function best_levels_populate($pdo)
 {
-    $stmt = $pdo->prepare('
+    $result = $pdo->exec('
         INSERT INTO best_levels
             SELECT level_id
             FROM pr2_levels
             WHERE live = 1
             AND votes > 1000
             AND rating > 4.3
+            LIMIT 100
     ');
-    
-    $result = $stmt->execute();
 
     if ($result === false) {
         throw new Exception('Could not populate all-time best levels.');
