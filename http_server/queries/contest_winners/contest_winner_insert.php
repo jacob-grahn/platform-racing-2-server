7b943cb135eadb736b35de2371d0e5b0573d4e7e
Ben <bls1999@users.noreply.github.com>
2018-03-26 01:22:05 -0400

    Create contest_winner_insert.php

---
+++ http_server/queries/contest_winners/contest_winner_insert.php
@@ -0,0 +1,21 @@
+<?php
+
+function contest_winner_insert($pdo, $contest_id, $pr2_name)
+{
+    $stmt = $pdo->prepare('
+        INSERT INTO contest_winners
+                SET contest_id = :contest_id,
+                    pr2_name = :pr2_name,
+                    win_time = :win_time,
+    ');
+    $stmt->bindValue(':contest_id', $contest_id, PDO::PARAM_INT);
+    $stmt->bindValue(':pr2_name', $pr2_name, PDO::PARAM_STR);
+    $stmt->bindValue(':win_time', time(), PDO::PARAM_INT);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception('Could not record winner.');
+    }
+    
+    return true;
+}
