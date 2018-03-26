7ff47d8af8edfc58d50ff332550313171eb35418
Ben <bls1999@users.noreply.github.com>
2018-03-26 18:37:45 -0400

    format -> server_insert

--- http_server/queries/servers/server_insert.php
+++ http_server/queries/servers/server_insert.php
@@ -4,13 +4,13 @@ function server_insert($pdo, $server_name, $address, $port, $expire_date, $salt,
 {
     $stmt = $pdo->prepare('
         INSERT INTO servers
-        SET server_name = :server_name,
-    		address = :address,
-    		port = :port,
-    		expire_date = FROM_UNIXTIME(:expire_date),
-    		active = true,
-    		salt = :salt,
-    		guild_id = :guild_id
+           SET server_name = :server_name,
+               address = :address,
+               port = :port,
+               expire_date = FROM_UNIXTIME(:expire_date),
+               active = true,
+               salt = :salt,
+               guild_id = :guild_id
     ');
     $stmt->bindValue(':server_name', $server_name, PDO::PARAM_STR);
     $stmt->bindValue(':address', $address, PDO::PARAM_STR);
@@ -18,8 +18,8 @@ function server_insert($pdo, $server_name, $address, $port, $expire_date, $salt,
     $stmt->bindValue(':expire_date', $expire_date, PDO::PARAM_STR);
     $stmt->bindValue(':salt', $salt, PDO::PARAM_STR);
     $stmt->bindValue(':guild_id', $guild_id, PDO::PARAM_INT);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
         throw new Exception('Could not create server.');
     }
