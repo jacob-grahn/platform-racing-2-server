3fd8d35d324afd22f8f68ce216e508cb1036d267
Ben <bls1999@users.noreply.github.com>
2018-03-26 17:21:31 -0400

    is this needed, format -> ignored_select

--- http_server/queries/ignored/ignored_select.php
+++ http_server/queries/ignored/ignored_select.php
@@ -1,23 +1,27 @@
 <?php
 
+// TO-DO: Is this needed?
 function ignored_select($pdo, $user_id, $ignore_id, $suppress_error = false)
 {
     $stmt = $pdo->prepare('
-        SELECT * FROM ignored
-        WHERE ignore_id = :ignore_id
-        AND user_id = :user_id
-        LIMIT 1');
+        SELECT *
+          FROM ignored
+         WHERE ignore_id = :ignore_id
+           AND user_id = :user_id
+         LIMIT 1
+    ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':ignore_id', $ignore_id, PDO::PARAM_INT);
     $result = $stmt->execute();
 
     if ($result === false) {
-        throw new Exception('Could not perform query to select ignored');
+        throw new Exception('Could not perform query to select an ignored user.');
     }
 
     $row = $stmt->fetch(PDO::FETCH_OBJ);
-    if ($row === false && $suppress_error === false) {
-        throw new Exception('Could not find ignored row for this user');
+    
+    if (empty($row) && $suppress_error === false) {
+        throw new Exception('It seems you haven\'t ignored user #$ignore_id.');
     }
 
     return $row;
