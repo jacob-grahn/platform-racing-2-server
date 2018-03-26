1b91522e41475b93e7109761c9f7fa8c9f0e5f8c
Ben <bls1999@users.noreply.github.com>
2018-03-26 19:45:22 -0400

    format -> user_update_email

--- http_server/queries/users/user_update_email.php
+++ http_server/queries/users/user_update_email.php
@@ -4,18 +4,18 @@ function user_update_email($pdo, $user_id, $old_email, $new_email)
 {
     $stmt = $pdo->prepare('
         UPDATE users
-        SET email = :new_email
-        WHERE user_id = :user_id
-        AND email = :old_email
-        LIMIT 1
+           SET email = :new_email
+         WHERE user_id = :user_id
+           AND email = :old_email
+         LIMIT 1
     ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':new_email', $new_email, PDO::PARAM_STR);
     $stmt->bindValue(':old_email', $old_email, PDO::PARAM_STR);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
-        throw new Exception('Could not update user email');
+        throw new Exception("Could not update the email address of user #$user_id.");
     }
 
     return $result;
