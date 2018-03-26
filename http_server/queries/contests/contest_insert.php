c0e157cf024c493c62eab1ac1a461e7154b64313
Ben <bls1999@users.noreply.github.com>
2018-03-25 23:55:19 -0400

    Update contest_insert.php

--- http_server/queries/contests/contest_insert.php
+++ http_server/queries/contests/contest_insert.php
@@ -8,13 +8,15 @@ function contest_insert($pdo, $name, $desc, $url, $host_id, $active)
                     description = :desc,
                     url = :url,
                     user_id = :host_id,
-                    active = :active
+                    active = :active,
+                    updated = :updated
     ');
     $stmt->bindValue(':name', $name, PDO::PARAM_STR);
     $stmt->bindValue(':desc', $desc, PDO::PARAM_STR);
     $stmt->bindValue(':url', $url, PDO::PARAM_STR);
     $stmt->bindValue(':host_id', $host_id, PDO::PARAM_INT);
     $stmt->bindValue(':active', $active, PDO::PARAM_INT);
+    $stmt->bindValue(':updated', time(), PDO::PARAM_INT);
     $result = $stmt->execute();
     
     if ($result === false) {
