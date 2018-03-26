17536203a5e99d4f8dc75ef45ef3c8ace9fd1904
Ben <bls1999@users.noreply.github.com>
2018-03-26 18:21:54 -0400

    format -> pr2_select

--- http_server/queries/pr2/pr2_select.php
+++ http_server/queries/pr2/pr2_select.php
@@ -2,22 +2,28 @@
 
 function pr2_select($pdo, $user_id, $suppress_error = false)
 {
-    $stmt = $pdo->prepare('SELECT * FROM pr2 WHERE user_id = :user_id LIMIT 1');
+    $stmt = $pdo->prepare('
+        SELECT *
+          FROM pr2
+         WHERE user_id = :user_id
+         LIMIT 1
+    ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $result = $stmt->execute();
     
     if ($result === false) {
-        throw new Exception('Could not perform query to select all from pr2');
+        throw new Exception('Could not perform query pr2_select.');
     }
 
-    $row = $stmt->fetch(PDO::FETCH_OBJ);
-    if ($row === false) {
+    $pr2 = $stmt->fetch(PDO::FETCH_OBJ);
+    
+    if (empty($pr2)) {
         if ($suppress_error === false) {
-            throw new Exception('Could not find a pr2 row for this user');
+            throw new Exception('Could not find PR2 player data for this user.');
         } else {
             return false;
         }
     }
 
-    return $row;
+    return $pr2;
 }
