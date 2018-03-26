1af4f344a8ed35e4a31fd540bb6a7eb529756c7b
Ben <bls1999@users.noreply.github.com>
2018-03-26 18:34:30 -0400

    format -> recent_logins_insert

--- http_server/queries/recent_logins/recent_logins_insert.php
+++ http_server/queries/recent_logins/recent_logins_insert.php
@@ -4,10 +4,10 @@ function recent_logins_insert($pdo, $user_id, $ip, $country_code)
 {
     $stmt = $pdo->prepare('
         INSERT INTO recent_logins
-                SET user_id = :user_id,
-                    ip = :ip,
-                    country = :country_code,
-                    date = NOW()
+          SET user_id = :user_id,
+              ip = :ip,
+              country = :country_code,
+              date = NOW()
         ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':ip', $ip, PDO::PARAM_STR);
