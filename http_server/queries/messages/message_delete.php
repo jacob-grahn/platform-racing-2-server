e77f893837f5ea5adeb9701b115c2c8fcfbb2876
Ben <bls1999@users.noreply.github.com>
2018-03-26 17:49:52 -0400

    format -> message_delete

--- http_server/queries/messages/message_delete.php
+++ http_server/queries/messages/message_delete.php
@@ -4,14 +4,14 @@ function message_delete($pdo, $user_id, $message_id)
 {
     $stmt = $pdo->prepare('
         DELETE FROM messages
-        WHERE to_user_id = :to_user_id
-        AND message_id = :message_id
-        LIMIT 1
+         WHERE to_user_id = :to_user_id
+           AND message_id = :message_id
+         LIMIT 1
     ');
     $stmt->bindValue(':to_user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':message_id', $message_id, PDO::PARAM_INT);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
         throw new Exception('Could not delete message.');
     }
