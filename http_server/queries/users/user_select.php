cc048f4e5e1b8d90174a7d19b3b399624f30e577
Ben <bls1999@users.noreply.github.com>
2018-03-26 19:34:21 -0400

    add error -> user_select

--- http_server/queries/users/user_select.php
+++ http_server/queries/users/user_select.php
@@ -20,12 +20,17 @@ function user_select($pdo, $user_id)
          LIMIT 1
     ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-    $stmt->execute();
-    $result = $stmt->fetch(PDO::FETCH_OBJ);
-
+    $result = $stmt->execute();
+    
     if ($result === false) {
+        throw new Exception('Could not perform query user_select.');
+    }
+    
+    $user = $stmt->fetch(PDO::FETCH_OBJ);
+
+    if (empty($user)) {
         throw new Exception('Could not find a user with that ID.');
     }
 
-    return $result;
+    return $user;
 }
