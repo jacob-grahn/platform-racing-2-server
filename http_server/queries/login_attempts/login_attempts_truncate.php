09ebe596ed7be1ac915220ae4087238ea8c32f91
Ben <bls1999@users.noreply.github.com>
2018-03-26 17:49:11 -0400

    err msg -> login_attempts_truncate

--- http_server/queries/login_attempts/login_attempts_truncate.php
+++ http_server/queries/login_attempts/login_attempts_truncate.php
@@ -5,7 +5,7 @@ function login_attempts_truncate($pdo)
     $result = $pdo->exec('TRUNCATE TABLE login_attempts');
 
     if ($result === false) {
-        throw new Exception('could not truncate login_attempts');
+        throw new Exception('Could not truncate table login_attempts.');
     }
 
     return $result;
