f81d661d93fdac7b8465d54c31b4a0cc6d14d8b1
Ben <bls1999@users.noreply.github.com>
2018-03-26 18:30:06 -0400

    err msg -> rank_token_rentals_delete_old

--- http_server/queries/rank_token_rentals/rank_token_rentals_delete_old.php
+++ http_server/queries/rank_token_rentals/rank_token_rentals_delete_old.php
@@ -2,10 +2,13 @@
 
 function rank_token_rentals_delete_old($pdo)
 {
-    $result = $pdo->exec('DELETE FROM rank_token_rentals WHERE date < DATE_SUB( NOW(), INTERVAL 1 WEEK )');
+    $result = $pdo->exec('
+        DELETE FROM rank_token_rentals
+        WHERE date < DATE_SUB(NOW(), INTERVAL 1 WEEK)
+    ');
 
     if ($result === false) {
-        throw new Exception('Could not delete expired rank_token_rentals');
+        throw new Exception('Could not delete expired rank token rentals.');
     }
 
     return $result;
