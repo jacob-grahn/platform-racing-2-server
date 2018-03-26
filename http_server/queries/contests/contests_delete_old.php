9d6a6c6878bcdb58c5abe2c62f36ec592507e783
Ben <bls1999@users.noreply.github.com>
2018-03-26 00:08:09 -0400

    Create contests_delete_old.php

---
+++ http_server/queries/contests/contests_delete_old.php
@@ -0,0 +1,12 @@
+<?php
+
+function contests_delete_old($pdo)
+{
+    $year = time() - 31536000;
+    $stmt = $pdo->prepare("
+        DELETE FROM contests
+              WHERE active = 0
+                AND updated < $year
+    ");
+    $stmt->execute();
+}
