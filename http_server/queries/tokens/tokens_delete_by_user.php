f3477b1e55f00ffd0946651ba1e0a88e387abee4
Ben <bls1999@users.noreply.github.com>
2018-03-26 19:01:29 -0400

    format -> tokens_delete_by_user

--- http_server/queries/tokens/tokens_delete_by_user.php
+++ http_server/queries/tokens/tokens_delete_by_user.php
@@ -2,12 +2,15 @@
 
 function tokens_delete_by_user($pdo, $user_id)
 {
-    $stmt = $pdo->prepare('DELETE FROM tokens WHERE user_id = :user_id');
+    $stmt = $pdo->prepare('
+        DELETE FROM tokens
+        WHERE user_id = :user_id
+    ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
-        throw new Exception('Could not delete login tokens for user');
+        throw new Exception('Could not delete this user\'s login tokens.');
     }
 
     return $result;
