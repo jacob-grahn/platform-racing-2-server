7ae4188091581a516c88a5eb5e26d243e22d2415
Ben <bls1999@users.noreply.github.com>
2018-03-26 17:07:33 -0400

    separate queries -> guild_delete.php

--- http_server/queries/guilds/guild_delete.php
+++ http_server/queries/guilds/guild_delete.php
@@ -2,23 +2,56 @@
 
 function guild_delete($pdo, $guild_id)
 {
+    guild_delete_from_guilds($pdo, $guild_id);
+    guild_delete_from_users($pdo, $guild_id);
+    guild_delete_from_gp($pdo, $guild_id);
+}
+
+function guild_delete_from_guilds($pdo, $guild_id)
+{
     $stmt = $pdo->prepare('
         DELETE FROM guilds
-        WHERE guild_id = :guild_id
-        LIMIT 1;
-
-        UPDATE users
-        SET guild = 0
-        WHERE guild = :guild_id;
-
-        DELETE FROM gp
-        WHERE guild_id = :guild_id;
+         WHERE guild_id = :guild_id
+         LIMIT 1
     ');
     $stmt->bindValue(':guild_id', $guild_id, PDO::PARAM_INT);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
-        throw new Exception('Could not delete guild');
+    	throw new Exception("Could not perform query guild_delete_from_guilds.");
+    }
+
+	return $result;
+}
+
+function guild_delete_from_users($pdo, $guild_id)
+{
+	$stmt = $pdo->prepare('
+        UPDATE users
+        SET guild = 0
+        WHERE guild = :guild_id
+    ');
+    $stmt->bindValue(':guild_id', $guild_id, PDO::PARAM_INT);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+    	throw new Exception("Could not perform query guild_delete_from_users.");
+    }
+
+	return $result;
+}
+
+function guild_delete_from_gp($pdo, $guild_id)
+{
+	$stmt = $pdo->prepare('
+        DELETE FROM gp
+        WHERE guild_id = :guild_id
+    ');
+    $stmt->bindValue(':guild_id', $guild_id, PDO::PARAM_INT);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception('Could not perform query guild_delete_from_gp.');
     }
 
     return $result;
