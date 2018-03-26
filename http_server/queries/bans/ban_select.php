11563c741dbbca0c696057df311cdb48e62ee939
Ben <bls1999@users.noreply.github.com>
2018-03-26 16:12:27 -0400

    format, err msg -> ban_select.php

--- http_server/queries/bans/ban_select.php
+++ http_server/queries/bans/ban_select.php
@@ -8,15 +8,16 @@ function ban_select($pdo, $ban_id)
         LIMIT 1
     ');
     $stmt->bindValue(':ban_id', $ban_id, PDO::PARAM_INT);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
-        throw new Exception('Could not select ban');
+        throw new Exception('Could not select ban.');
     }
 
     $ban = $stmt->fetch(PDO::FETCH_OBJ);
-    if (!$ban) {
-        throw new Exception('Ban not found');
+    
+    if (empty($ban)) {
+        throw new Exception('Could not find a ban with that ID.');
     }
 
     return $ban;
