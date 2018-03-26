7a8fffa5ff67969c0eaa34b13d72d29a6a6bca19
Ben <bls1999@users.noreply.github.com>
2018-03-25 23:30:19 -0400

    Add active field

--- http_server/queries/contests/contest_insert.php
+++ http_server/queries/contests/contest_insert.php
@@ -1,18 +1,20 @@
 <?php
 
-function contest_insert($pdo, $name, $desc, $url, $host_id)
+function contest_insert($pdo, $name, $desc, $url, $host_id, $active)
 {
     $stmt = $pdo->prepare('
         INSERT INTO contests
                 SET contest_name = :name,
                     description = :desc,
                     url = :url,
-                    user_id = :host_id
+                    user_id = :host_id,
+                    active = :active
     ');
     $stmt->bindValue(':name', $name, PDO::PARAM_STR);
     $stmt->bindValue(':desc', $desc, PDO::PARAM_STR);
     $stmt->bindValue(':url', $url, PDO::PARAM_STR);
     $stmt->bindValue(':host_id', $host_id, PDO::PARAM_INT);
+    $stmt->bindValue(':active', $active, PDO::PARAM_INT);
     $result = $stmt->execute();
     
     if ($result === false) {
