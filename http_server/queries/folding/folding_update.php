4808b4d4bb3a808d2703bddc405d1a056f858d83
Ben <bls1999@users.noreply.github.com>
2018-03-26 16:47:12 -0400

    err msg -> folding_update.php

--- http_server/queries/folding/folding_update.php
+++ http_server/queries/folding/folding_update.php
@@ -4,16 +4,16 @@ function folding_update($pdo, $user_id, $column_name)
 {
     $stmt = $pdo->prepare('
         UPDATE folding_at_home
-        SET :column_name = 1
-        WHERE user_id = :user_id
-        LIMIT 1
+           SET :column_name = 1
+         WHERE user_id = :user_id
+         LIMIT 1
     ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':column_name', $column_name, PDO::PARAM_STR);
     $result = $stmt->execute();
 
     if ($result === false) {
-        throw new Exception('Could not update folding_at_home row');
+        throw new Exception("Could not update folding_at_home entry for user #$user_id at column $column_name.");
     }
 
     return $result;
