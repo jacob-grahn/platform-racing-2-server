b1e17f80a32440fe54dc0dc9472e37a2fa6f490c
Ben <bls1999@users.noreply.github.com>
2018-03-26 19:51:24 -0400

    format -> user_update_temp_pass

--- http_server/queries/users/user_update_temp_pass.php
+++ http_server/queries/users/user_update_temp_pass.php
@@ -4,15 +4,15 @@ function user_update_temp_pass($pdo, $user_id, $temp_pass_hash)
 {
     $stmt = $pdo->prepare('
         UPDATE users
-        SET temp_pass_hash = :temp_pass_hash
-        WHERE user_id = :user_id
+           SET temp_pass_hash = :temp_pass_hash
+         WHERE user_id = :user_id
     ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':temp_pass_hash', $temp_pass_hash, PDO::PARAM_STR);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
-        throw new Exception('Could not update user temporary password');
+        throw new Exception("Could not set a temporary password for user #$user_id.");
     }
 
     return $result;
