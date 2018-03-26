0473c94b7b3224ce10bdfb298aead83bd835ccba
Ben <bls1999@users.noreply.github.com>
2018-03-26 19:33:00 -0400

    format -> user_insert

--- http_server/queries/users/user_insert.php
+++ http_server/queries/users/user_insert.php
@@ -4,13 +4,13 @@ function user_insert($pdo, $name, $pass_hash, $ip, $time, $email)
 {
     $stmt = $pdo->prepare('
         INSERT INTO users
-        SET name = :name,
-            pass_hash = :pass_hash,
-            register_ip = :ip,
-            ip = :ip,
-            time = :time,
-            register_time = :time,
-            email = :email
+           SET name = :name,
+               pass_hash = :pass_hash,
+               register_ip = :ip,
+               ip = :ip,
+               time = :time,
+               register_time = :time,
+               email = :email
     ');
     $stmt->bindValue(':name', $name, PDO::PARAM_STR);
     $stmt->bindValue(':pass_hash', $pass_hash, PDO::PARAM_STR);
@@ -20,7 +20,7 @@ function user_insert($pdo, $name, $pass_hash, $ip, $time, $email)
     $result = $stmt->execute();
     
     if ($result === false) {
-        throw new Exception('Could not insert new user.');
+        throw new Exception('Could not create a new account.');
     }
 
     return $result;
