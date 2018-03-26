1e4ef57009282da9b98c333f9c0d31703be3ef59
Ben <bls1999@users.noreply.github.com>
2018-03-26 18:01:13 -0400

    format -> messages_reported_select

--- http_server/queries/messages_reported/messages_reported_select.php
+++ http_server/queries/messages_reported/messages_reported_select.php
@@ -4,18 +4,18 @@ function messages_reported_select($pdo, $start, $count)
 {
     $stmt = $pdo->prepare('
         SELECT messages_reported.*, u1.name as from_name, u2.name as to_name
-        FROM messages_reported, users u1, users u2
-        WHERE to_user_id = u2.user_id
-        AND from_user_id = u1.user_id
-        ORDER by reported_time desc
-        LIMIT :start, :count
+          FROM messages_reported, users u1, users u2
+         WHERE to_user_id = u2.user_id
+           AND from_user_id = u1.user_id
+         ORDER BY reported_time desc
+         LIMIT :start, :count
     ');
     $stmt->bindValue(':start', $start, PDO::PARAM_INT);
     $stmt->bindValue(':count', $count, PDO::PARAM_INT);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
-        throw new Exception('Could not fetch reported message list.');
+        throw new Exception('Could not fetch the list of reported messages.');
     }
 
     return $stmt->fetchAll(PDO::FETCH_OBJ);
