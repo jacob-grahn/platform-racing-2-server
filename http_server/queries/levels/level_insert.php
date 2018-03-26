259e903a5d4dc5e1f21a9c473ec97125c8b7390b
Ben <bls1999@users.noreply.github.com>
2018-03-26 17:33:03 -0400

    format -> level_insert.php

--- http_server/queries/levels/level_insert.php
+++ http_server/queries/levels/level_insert.php
@@ -3,17 +3,17 @@
 function level_insert($pdo, $title, $note, $live, $time, $ip, $min_level, $song, $user_id, $pass, $type)
 {
     $stmt = $pdo->prepare('
-    	INSERT INTO pr2_levels
-    	SET title = :title,
-			note = :note,
-			live = :live,
-			time = :time,
-			ip = :ip,
-			min_level = :min_level,
-			song = :song,
-			user_id = :user_id,
-			pass = :pass,
-			type = :type
+        INSERT INTO pr2_levels
+           SET title = :title,
+               note = :note,
+               live = :live,
+               time = :time,
+               ip = :ip,
+               min_level = :min_level,
+               song = :song,
+               user_id = :user_id,
+               pass = :pass,
+               type = :type
     ');
     $stmt->bindValue(':title', $title, PDO::PARAM_STR);
     $stmt->bindValue(':note', $note, PDO::PARAM_STR);
@@ -25,10 +25,10 @@ function level_insert($pdo, $title, $note, $live, $time, $ip, $min_level, $song,
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
     $stmt->bindValue(':type', $type, PDO::PARAM_STR);
-
     $result = $stmt->execute();
+
     if ($result === false) {
-        throw new Exception('Could not insert level');
+        throw new Exception('Could not save the new level.');
     }
 
     return $result;
