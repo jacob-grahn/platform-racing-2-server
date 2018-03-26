6d08066c93bb5a30c15892b67ce31f92801b4f67
Ben <bls1999@users.noreply.github.com>
2018-03-26 19:02:18 -0400

    format -> tokens_delete_old

--- http_server/queries/tokens/tokens_delete_old.php
+++ http_server/queries/tokens/tokens_delete_old.php
@@ -2,10 +2,13 @@
 
 function tokens_delete_old($pdo)
 {
-    $result = $pdo->exec('DELETE FROM tokens WHERE Date(time) < date_sub(NOW(), interval 1 month)');
+    $result = $pdo->exec('
+        DELETE FROM tokens
+        WHERE Date(time) < DATE_SUB(NOW(), INTERVAL 1 MONTH)
+    ');
 
     if ($result === false) {
-        throw new Exception('Could not delete old login tokens');
+        throw new Exception('Could not delete expired login tokens.');
     }
 
     return $result;
