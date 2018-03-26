ed5c5e92199052164c20413b6ac40677bf8d8ef7
Ben <bls1999@users.noreply.github.com>
2018-03-26 16:20:22 -0400

    separate queries -> atb

--- http_server/queries/best_levels/best_levels_populate.php
+++ http_server/queries/best_levels/best_levels_populate.php
@@ -2,20 +2,19 @@
 
 function best_levels_populate($pdo)
 {
-    $stmt = $pdo->prepare('TRUNCATE TABLE best_levels;
-    
-    INSERT INTO best_levels
-        SELECT level_id
-        FROM pr2_levels
-        WHERE live = 1
+    $stmt = $pdo->prepare('
+        INSERT INTO best_levels
+            SELECT level_id
+            FROM pr2_levels
+            WHERE live = 1
             AND votes > 1000
-            AND rating > 4.3;
-            ');
+            AND rating > 4.3
+    ');
     
     $result = $stmt->execute();
 
     if ($result === false) {
-        throw new Exception('Could not populate all-time best levels');
+        throw new Exception('Could not populate all-time best levels.');
     }
 
     return $result;
