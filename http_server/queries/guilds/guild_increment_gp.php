4bb44ee45e577ba359cb00dab5c0fe210068aa1e
Ben <bls1999@users.noreply.github.com>
2018-03-26 17:11:24 -0400

    format, err msg -> guild_increment_gp

--- http_server/queries/guilds/guild_increment_gp.php
+++ http_server/queries/guilds/guild_increment_gp.php
@@ -4,17 +4,17 @@ function guild_increment_gp($pdo, $guild_id, $gp)
 {
     $stmt = $pdo->prepare('
         UPDATE guilds
-        SET gp_today = gp_today + :gp,
-            gp_total = gp_total + :gp
-        WHERE guild_id = :guild_id
-        LIMIT 1
+           SET gp_today = gp_today + :gp,
+               gp_total = gp_total + :gp
+         WHERE guild_id = :guild_id
+         LIMIT 1
     ');
     $stmt->bindValue(':guild_id', $guild_id, PDO::PARAM_INT);
     $stmt->bindValue(':gp', $gp, PDO::PARAM_INT);
     $result = $stmt->execute();
 
     if ($result === false) {
-        throw new Exception('could not increment guild gp');
+        throw new Exception('Could not record earned GP.');
     }
 
     return $result;
