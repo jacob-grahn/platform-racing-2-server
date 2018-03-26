dd2f558145e94f4f3aa92c75b80edde4e31ad1ef
Ben <bls1999@users.noreply.github.com>
2018-03-26 16:48:47 -0400

    err msg -> friend_insert.php

--- http_server/queries/friends/friend_insert.php
+++ http_server/queries/friends/friend_insert.php
@@ -5,14 +5,14 @@ function friend_insert($pdo, $user_id, $friend_id)
     $stmt = $pdo->prepare('
         INSERT IGNORE INTO friends
         SET user_id = :user_id,
-    		friend_id = :friend_id
+            friend_id = :friend_id
     ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $stmt->bindValue(':friend_id', $friend_id, PDO::PARAM_INT);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
-        throw new Exception('Could not insert friend');
+        throw new Exception('Could not add friend to your friends list.');
     }
 
     return $result;
