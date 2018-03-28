615f6fdc33c8153f5cecb49a08979ac7c72c7568
Ben <bls1999@users.noreply.github.com>
2018-03-28 01:24:50 -0400

    add host_ip to contest_winner_insert

--- http_server/queries/contest_winners/contest_winner_insert.php
+++ http_server/queries/contest_winners/contest_winner_insert.php
@@ -1,17 +1,19 @@
 <?php
 
-function contest_winner_insert($pdo, $contest_id, $winner_name, $comment)
+function contest_winner_insert($pdo, $contest_id, $winner_name, $host_ip, $comment)
 {
     $stmt = $pdo->prepare('
         INSERT INTO contest_winners
                 SET contest_id = :contest_id,
                     winner_name = :winner_name,
                     win_time = :win_time,
-                    comment = :comment
+                    host_ip = :host_ip,
+                    comment = :comment,
     ');
     $stmt->bindValue(':contest_id', $contest_id, PDO::PARAM_INT);
     $stmt->bindValue(':winner_name', $winner_name, PDO::PARAM_STR);
     $stmt->bindValue(':win_time', time(), PDO::PARAM_INT);
+    $stmt->bindValue(':host_ip', $host_ip, PDO::PARAM_STR);
     $stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
     $result = $stmt->execute();
     
