960215ec1f376118e1d7004bda165509a7dd3b01
Ben <bls1999@users.noreply.github.com>
2018-03-28 01:01:56 -0400

    Create user_select_guest_name.php

---
+++ http_server/queries/users/user_select_guest_name.php
@@ -0,0 +1,25 @@
+<?php
+
+function user_select_guest_name($pdo)
+{
+    $stmt = $pdo->prepare('
+        SELECT name
+          FROM users
+         WHERE power = 0
+           AND status = "offline"
+         LIMIT 1
+    ');
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception('Could not perform query user_select_guest.');
+    }
+
+    $guest = $stmt->fetch(PDO::FETCH_OBJ);
+    
+    if (empty($guest)) {
+        throw new Exception('Could not find a suitable guest account. Try again later, or create a new account instead.');
+    }
+
+    return $guest->name;
+}
