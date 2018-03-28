3a811dcc7fa3fe5d6d71e3b11c764a09f633b4bf
Ben <bls1999@users.noreply.github.com>
2018-03-28 17:27:55 -0400

    restructure code -> server_owner.php

--- multiplayer_server/fns/server_owner.php
+++ multiplayer_server/fns/server_owner.php
@@ -14,18 +14,19 @@ function promote_server_mod($name, $owner, $promoted)
         return false;
     }
 
+    // sanity check: is the user online?
     if (!isset($promoted)) {
-        $owner->write("message`Error: Could not find a user named $safe_name on this server.");
+        $owner->write("message`Error: Could not find a user with the name \"$safe_name\" on this server.");
         return false;
     }
 
-    // if the player being promoted is an admin or staff member, end the function
-    if ($promoted->group == 3) {
-        $owner->write("message`Error: I'm not sure what would happen if you promoted an admin to a moderator, but it would probably make the world explode.");
+    // sanity check: is the user being promoted already a staff member?
+    if ($promoted->group >= 2 && $promoted->temp_mod == false) {
+        $owner->write("message`Error: I'm not sure what would happen if you promoted a staff member to a server moderator, but it would probably make the world explode.");
         return false;
     }
 
-    // if the person being promoted is a guest, end the function
+    // sanity check: is the person being promoted a guest? (this should never happen on a private server...)
     if ($promoted->group == 0) {
         $owner->write("message`Error: Guests can't be promoted to server moderators.");
         return false;
@@ -37,13 +38,17 @@ function promote_server_mod($name, $owner, $promoted)
         return false;
     }
 
-    // now that we've determined that the user is able to do what they're trying to do, let's finish
-    $promoted->become_temp_mod();
-    $owner->write("message`$name has been promoted to a server moderator! They'll remain a moderator until you type /demod *their name* or until they log out.");
-    if (isset($owner->chat_room)) {
-        $owner->chat_room->send_chat("systemChat`$safe_owner has promoted $safe_name to a server moderator! Your private peace-keeping is greatly appreciated! You'll have your mod powers until you log out or are demoted.");
+    // if they're the server owner and have gotten this far, promote the user to a server mod
+    if ($owner->server_owner == true) {
+        $promoted->become_temp_mod();
+        $owner->write("message`$name has been promoted to a server moderator! They'll remain a moderator until you type /demod *their name* or until they log out.");
+        if (isset($owner->chat_room)) {
+            $owner->chat_room->send_chat("systemChat`$safe_owner has promoted $safe_name to a server moderator! Your private peace-keeping is greatly appreciated! You'll have your mod powers until you log out or are demoted.");
+        }
+        return true;
     }
-    return true;
+    
+    return false;
 }
 
 function demote_server_mod($name, $owner, $demoted)
@@ -53,29 +58,32 @@ function demote_server_mod($name, $owner, $demoted)
     // safety first
     $safe_name = htmlspecialchars($name);
 
-    // if the user isn't the on the server, kill the function (2nd line of defense)
+    // sanity check: does the user own the server?
     if ($owner->group < 3 || $owner->server_owner == false || $owner->user_id != $guild_owner) {
         $owner->write("message`Error: You lack the power to promote $safe_name to a server moderator.");
         return false;
     }
 
-    // don't let the server owner demote themselves
+    // sanity check: is the server owner trying to demote themselves? lol
     if ($demoted->user_id == $guild_owner) {
-        $owner->write("message`Error: The server owner reigns supreme!");
+        $owner->write("message`Error: You can't demote yourself on your own private server, silly!");
         return false;
     }
 
-    // let the server owner demote temps
-    if ($owner->server_owner == true) {
-        if (isset($demoted) && $demoted->temp_mod == true) {
-            $demoted->group = 1;
-            $demoted->write('setGroup`1');
-            $demoted->temp_mod = false;
-            $owner->write("message`$safe_name has been demoted.");
-            return true;
-        } else {
-            $owner->write("message`Could not find a server moderator with the name \"$safe_name\" on this server.");
-            return false;
-        }
+    // sanity check: is the user online? are they a temp?
+    if (!isset($demoted) || $demoted->temp_mod == false) {
+        $owner->write("message`Error: Could not find a server moderator with the name \"$safe_name\" on this server.");
+        return false;
     }
+
+    // if they're the server owner and have gotten this far, demote the user
+    if ($owner->server_owner == true) {
+        $demoted->group = 1;
+        $demoted->write('setGroup`1');
+        $demoted->temp_mod = false;
+        $owner->write("message`$safe_name has been demoted.");
+        return true;
+    }
+    
+    return false;
 }
