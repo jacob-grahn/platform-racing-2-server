96795c6e3364543863f1d7bff4c4c8798b6d4779
Ben <bls1999@users.noreply.github.com>
2018-03-28 16:49:09 -0400

    optimizations -> artifact_first_check.php

--- multiplayer_server/fns/artifact_first_check.php
+++ multiplayer_server/fns/artifact_first_check.php
@@ -19,10 +19,8 @@ function artifact_first_check($player)
         $artifact = artifact_location_select($pdo);
         $first_finder = $artifact->first_finder;
 
+        // they found it first? time to get jiggy with it!
         if ($first_finder === $user_id) {
-            /* What are we gonna tell the player when they win?
-            How about display a prize window with the bubble head and the name "Bubble Set" */
-
             // make a prize array for the game to show the user
             $artifact_first_prize_popup = json_encode(
                 array(
@@ -40,17 +38,13 @@ function artifact_first_check($player)
             $player->gain_part("feet", 28, true);
 
             // tell the world
-            echo "Awarded bubble set to $safe_user_name for finding the artifact first.";
             $player->write('winPrize`' . $artifact_first_prize_popup);
 
             // pm the user (finishing touch!)
-            $artifact_first_pm = 'Dear '.$safe_user_name.',
-
-I\'d like to sincerely congratulate you for finding the artifact first! To commemorate this momentous occasion, you\'ve been awarded with your very own bubble set.
-
-Thanks for playing Platform Racing 2!
-
-- Jiggmin';
+            $artifact_first_pm = "Dear $safe_user_name,\n\n"
+                ."Congratulations for finding the artifact first! To commemorate this momentous occasion, you've been awarded with your very own bubble set.\n\n"
+                ."Thanks for playing Platform Racing 2!\n\n"
+                ."- Jiggmin";
 
             message_insert($pdo, $user_id, 1, $artifact_first_pm, '0');
         }
