12412316e49681d6fd635495c8b7cfa12d6d1273
Ben <bls1999@users.noreply.github.com>
2018-03-28 15:22:05 -0400

    int -> ban_lift.php

--- http_server/queries/bans/ban_lift.php
+++ http_server/queries/bans/ban_lift.php
@@ -13,9 +13,10 @@ function ban_lift($pdo, $ban_id, $lifted_by, $lifted_reason)
     $stmt->bindValue(':ban_id', $ban_id, PDO::PARAM_INT);
     $stmt->bindValue(':lifted_by', $lifted_by, PDO::PARAM_STR);
     $stmt->bindValue(':lifted_reason', $lifted_reason, PDO::PARAM_STR);
-
     $result = $stmt->execute();
+    
     if ($result === false) {
+        $ban_id = (int) $ban_id;
         throw new Exception("Could not lift ban #$ban_id.");
     }
 
