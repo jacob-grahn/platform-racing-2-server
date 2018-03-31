08d112b257b603237c20a4e034df6f435c33b60e
Ben <bls1999@users.noreply.github.com>
2018-03-31 00:15:51 -0400

    show users that don't have rank tokens

--- http_server/queries/users/users_select_top.php
+++ http_server/queries/users/users_select_top.php
@@ -3,15 +3,16 @@
 function users_select_top($pdo, $start, $count)
 {
     $stmt = $pdo->prepare('
-        SELECT users.name as name,
-               users.power as power,
-               (rank_tokens.used_tokens + pr2.rank) AS active_rank,
+        SELECT users.name AS name,
+               users.power AS power,
+               pr2.rank AS rank,
                pr2.hat_array AS hats
-          FROM users, pr2, rank_tokens
-         WHERE users.user_id = pr2.user_id
-           AND pr2.user_id = rank_tokens.user_id
-           AND rank > 44
-        HAVING active_rank > 49
+               rank_tokens.used_tokens AS used_tokens,
+          FROM users
+          LEFT JOIN pr2 ON pr2.user_id = users.user_id
+          LEFT JOIN rank_tokens ON rank_tokens.user_id = pr2.user_id
+         WHERE rank > 44
+        HAVING SUM(pr2.rank + rank_tokens.used_tokens) > 49
          ORDER BY active_rank DESC, name ASC
          LIMIT :start, :count
     ');
