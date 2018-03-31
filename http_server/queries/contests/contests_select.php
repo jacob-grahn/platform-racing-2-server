f751f96a9be3ec052c1795125d7830b9975495ec
Ben <bls1999@users.noreply.github.com>
2018-03-30 23:26:07 -0400

    organize contests

--- http_server/queries/contests/contests_select.php
+++ http_server/queries/contests/contests_select.php
@@ -2,18 +2,18 @@
 
 function contests_select($pdo, $active_only = true)
 {
+    $sql = '';
+    $err = 'all ';
     if ($active_only === true) {
         $sql = 'WHERE active = 1';
         $err = 'active ';
-    } else {
-        $sql = '';
-        $err = 'all ';
     }
     
     $stmt = $pdo->prepare("
         SELECT contest_id, contest_name, description, url, user_id, active
-        FROM contests
-        $sql
+          FROM contests
+          $sql
+         ORDER BY active DESC, contest_name ASC
     ");
     $result = $stmt->execute();
     
