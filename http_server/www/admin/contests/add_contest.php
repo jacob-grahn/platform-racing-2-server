2c195c5e231c0cc95cd261e32ed3c3550dae4fee
Ben <bls1999@users.noreply.github.com>
2018-03-31 02:28:22 -0400

    update maxlengths

--- http_server/www/admin/contests/add_contest.php
+++ http_server/www/admin/contests/add_contest.php
@@ -58,11 +58,11 @@ function output_form()
     echo "Add Contest<br><br>";
 
     echo 'Contest Name: <input type="text" name="contest_name" maxlength="100"> (the name of the contest, max: 100 characters)<br>';
-    echo 'Description: <input type="text" name="description"> (short description of what the contest involves, max: 255 characters)<br>';
-    echo 'Contest URL: <input type="text" name="url"> (link to contest homepage, max: 255 characters)<br>';
-    echo 'Host User ID: <input type="text" name="host_id"> (the user ID of the PR2 player that is hosting this contest, max: 10 numbers)<br>';
-    echo 'Awarding: <input type="text" name="awarding"> (summary of the prizes the contest is awarding, max: 255 characters)<br>';
-    echo 'Max Awards (per week): <input type="text" name="max_awards"> (max times a contest owner can award prizes per week, suggested: 1-3, min: 1, max: 50)<br>';
+    echo 'Description: <input type="text" name="description" maxlength="255"> (short description of what the contest involves, max: 255 characters)<br>';
+    echo 'Contest URL: <input type="text" name="url" maxlength="255"> (link to contest homepage, max: 255 characters)<br>';
+    echo 'Host User ID: <input type="text" name="host_id" maxlength="10"> (the user ID of the PR2 player that is hosting this contest, max: 10 numbers)<br>';
+    echo 'Awarding: <input type="text" name="awarding" maxlength="255"> (summary of the prizes the contest is awarding, max: 255 characters)<br>';
+    echo 'Max Awards (per week): <input type="text" name="max_awards" maxlength="2"> (max times a contest owner can award prizes per week, suggested: 1-3, min: 1, max: 50)<br>';
     echo 'Active: <input type="checkbox" name="active" checked="checked"> (contest visible and prizes able to be awarded)<br>';
 
     echo '<input type="hidden" name="action" value="add">';
