0fea74aed09fe42189c579c9f7df7e5af2e15e73
Ben <bls1999@users.noreply.github.com>
2018-04-01 13:41:55 -0400

    winner_name -> winner_id

--- http_server/queries/contest_winners/contest_winner_insert.php
+++ http_server/queries/contest_winners/contest_winner_insert.php
@@ -1,18 +1,18 @@
 <?php
 
-function contest_winner_insert($pdo, $contest_id, $winner_name, $host_ip, $awarded_by, $comment)
+function contest_winner_insert($pdo, $contest_id, $winner_id, $host_ip, $awarded_by, $comment)
 {
     $stmt = $pdo->prepare('
         INSERT INTO contest_winners
                 SET contest_id = :contest_id,
-                    winner_name = :winner_name,
+                    winner_id = :winner_id,
                     win_time = :win_time,
                     host_ip = :host_ip,
                     awarded_by = :awarded_by,
                     comment = :comment
     ');
     $stmt->bindValue(':contest_id', $contest_id, PDO::PARAM_INT);
-    $stmt->bindValue(':winner_name', $winner_name, PDO::PARAM_STR);
+    $stmt->bindValue(':winner_id', $winner_id, PDO::PARAM_INT);
     $stmt->bindValue(':win_time', time(), PDO::PARAM_INT);
     $stmt->bindValue(':host_ip', $host_ip, PDO::PARAM_STR);
     $stmt->bindValue(':awarded_by', $awarded_by, PDO::PARAM_STR);
