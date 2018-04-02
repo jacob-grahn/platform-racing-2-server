1b68895f6353183182185197a9b224aa9459eaba
Ben <bls1999@users.noreply.github.com>
2018-04-01 22:32:51 -0400

    Update users_select_by_ip.php

--- http_server/queries/users/users_select_by_ip.php
+++ http_server/queries/users/users_select_by_ip.php
@@ -4,7 +4,7 @@ function users_select_by_ip($pdo, $ip)
 {
     $count = (int) $count;
     $stmt = $pdo->prepare('
-          SELECT user_id, name, active_date, power
+          SELECT user_id
             FROM users
            WHERE ip = :ip
               OR register_ip = :ip
