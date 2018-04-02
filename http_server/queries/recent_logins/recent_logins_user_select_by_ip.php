01b461fa73efee8eb35bb1a87594aedbc7ccc7a5
Ben <bls1999@users.noreply.github.com>
2018-04-01 22:34:03 -0400

    Update recent_logins_user_select_by_ip.php

--- http_server/queries/recent_logins/recent_logins_user_select_by_ip.php
+++ http_server/queries/recent_logins/recent_logins_user_select_by_ip.php
@@ -1,17 +1,14 @@
 <?php
 
-function recent_logins_user_select_by_ip($pdo, $ip, $start = 0, $count = 100)
+function recent_logins_user_select_by_ip($pdo, $ip)
 {
     $count = (int) $count;
     $stmt = $pdo->prepare('
         SELECT DISTINCT user_id
           FROM recent_logins
          WHERE ip = :ip
-         LIMIT :start, :count
     ');
     $stmt->bindValue(':ip', $ip, PDO::PARAM_STR);
-    $stmt->bindValue(':start', $start, PDO::PARAM_INT);
-    $stmt->bindValue(':count', $count, PDO::PARAM_INT);
     $result = $stmt->execute();
 
     if ($result === false) {
