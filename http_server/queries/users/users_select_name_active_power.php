0fb73903a2ec2c0ff10c0bf3fd699e6bf89b95a9
Ben <bls1999@users.noreply.github.com>
2018-04-01 22:48:10 -0400

    Create users_select_name_active_power.php

---
+++ http_server/queries/users/users_select_name_active_power.php
@@ -0,0 +1,30 @@
+<?php
+
+function users_select_name_active_power($pdo, $user_id, $suppress_error = false)
+{
+    $count = (int) $count;
+    $stmt = $pdo->prepare('
+          SELECT name, active_date, power
+            FROM users
+           WHERE user_id = :user_id
+           LIMIT 1
+    ');
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $result = $stmt->execute();
+    
+    if ($result === false) {
+        throw new Exception('Could not perform query users_select_name_active_power.');
+    }
+    
+    $user = $stmt->fetch(PDO::FETCH_OBJ);
+    
+    if (empty($user)) {
+        if ($suppress_error == false) {
+            throw new Exception('Could not find any users with that ID.');
+        } else {
+            return false;
+        }
+    }
+    
+    return $user;
+}
