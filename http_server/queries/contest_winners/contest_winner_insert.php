a55871e9963aef0c50cd0057124854aa9d754280
Ben <bls1999@users.noreply.github.com>
2018-04-07 02:40:38 -0400

    host_ip -> awarder_ip

--- http_server/queries/contest_winners/contest_winner_insert.php
+++ http_server/queries/contest_winners/contest_winner_insert.php
@@ -1,13 +1,13 @@
 <?php
 
-function contest_winner_insert($pdo, $contest_id, $winner_id, $host_ip, $awarded_by, $prizes_awarded, $comment)
+function contest_winner_insert($pdo, $contest_id, $winner_id, $awarder_ip, $awarded_by, $prizes_awarded, $comment)
 {
     $stmt = $pdo->prepare('
         INSERT INTO contest_winners
                 SET contest_id = :contest_id,
                     winner_id = :winner_id,
                     win_time = :win_time,
-                    host_ip = :host_ip,
+                    awarder_ip = :awarder_ip,
                     awarded_by = :awarded_by,
                     prizes_awarded = :prizes_awarded,
                     comment = :comment
@@ -15,7 +15,7 @@ function contest_winner_insert($pdo, $contest_id, $winner_id, $host_ip, $awarded
     $stmt->bindValue(':contest_id', $contest_id, PDO::PARAM_INT);
     $stmt->bindValue(':winner_id', $winner_id, PDO::PARAM_INT);
     $stmt->bindValue(':win_time', time(), PDO::PARAM_INT);
-    $stmt->bindValue(':host_ip', $host_ip, PDO::PARAM_STR);
+    $stmt->bindValue(':awarder_ip', $awarder_ip, PDO::PARAM_STR);
     $stmt->bindValue(':awarded_by', $awarded_by, PDO::PARAM_STR);
     $stmt->bindValue(':prizes_awarded', $prizes_awarded, PDO::PARAM_STR);
     $stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
