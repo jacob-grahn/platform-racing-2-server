00c6060616b81164d2e7ededc0aa6fb20365b352
Ben <bls1999@users.noreply.github.com>
2018-04-08 14:03:35 -0400

    Update users_count_from_ip_expanded.php

--- http_server/queries/staff/admin/users_count_from_ip_expanded.php
+++ http_server/queries/staff/admin/users_count_from_ip_expanded.php
@@ -4,7 +4,7 @@ function users_count_from_ip_expanded($pdo, $search_ip)
 {
     $stmt = $pdo->prepare("
         SELECT DISTINCT
-          COUNT(u.*) as 'count',
+          u.user_id
         FROM
           users u
           LEFT JOIN recent_logins rl ON u.user_id = rl.user_id
@@ -18,5 +18,5 @@ function users_count_from_ip_expanded($pdo, $search_ip)
         throw new Exception('Could not perform query users_count_from_ip_expanded.');
     }
     
-    return (int) $stmt->fetchColumn();
+    return (int) count($stmt->fetchAll(PDO::FETCH_OBJ));
 }
