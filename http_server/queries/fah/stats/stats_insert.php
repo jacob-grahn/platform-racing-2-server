8e203e5080809c183aa520c6dd0258e36094bc05
Jacob Grahn <contact@jacobgrahn.com>
2018-04-10 15:14:40 -0400

    I think the gains table is now unused

--- http_server/queries/fah/stats/stats_insert.php
+++ http_server/queries/fah/stats/stats_insert.php
@@ -3,19 +3,6 @@
 function stats_insert($pdo, $name, $wu, $points, $rank)
 {
     $stmt = $pdo->prepare('
-        DECLARE _last_wu INT(11) DEFAULT 0;
-        DECLARE _last_points INT(11) DEFAULT 0;
-        DECLARE _wu_gain INT(11) DEFAULT 0;
-        DECLARE _point_gain INT(11) DEFAULT 0;
-
-        SELECT wu, points INTO _last_wu, _last_points
-        FROM stats
-        WHERE fah_name = :name
-        LIMIT 0, 1;
-
-        SET _wu_gain = :wu - _last_wu;
-        SET _point_gain = :points - _last_points;
-
         INSERT INTO stats
         SET fah_name = :name,
                 wu = :wu,
@@ -24,17 +11,7 @@ function stats_insert($pdo, $name, $wu, $points, $rank)
         ON DUPLICATE KEY UPDATE
                 wu = :wu,
                 points = :points,
-                rank = :rank;
-
-        IF (_wu_gain + _point_gain > 0) THEN
-            INSERT INTO gains
-            SET fah_name = :name,
-                    wu = _wu_gain,
-                    points = _point_gain,
-                    date = NOW();
-        END IF;
-
-        SELECT _wu_gain, _point_gain;
+                rank = :rank
     ');
     $stmt->bindValue(':name', $name, PDO::PARAM_STR);
     $stmt->bindValue(':wu', $wu, PDO::PARAM_INT);
