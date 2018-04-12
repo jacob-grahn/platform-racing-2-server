3f1992238894adfa6a27fcdf5782f0e5ad12d2a1
Jacob Grahn <jacob@grahn.io>
2018-04-12 13:47:18 -0400

    Simplify send_chat

--- multiplayer_server/rooms/Game.php
+++ multiplayer_server/rooms/Game.php
@@ -863,27 +863,14 @@ class Game extends Room
     
     public function send_chat($message, $user_id)
     {
-        try {
-            $chat_message = substr($message, 5);
-            
-            // check if trying to execute a socket command other than chat
-            if (strpos($message, 'chat`') !== 0) {
-                throw new Exception('Illegal operation.');
-            } // check for backticks
-            if (strpos($chat_message, '`') !== false) {
-                throw new Exception('Illegal character in message.');
-            } // send the message
+        // any added backticks will cut off the end of $text
+        list($command, $name, $power, $text) = explode('`', $message);
+
+        // send the message
+        if ($command === 'chat') {
             foreach ($this->player_array as $player) {
                 if (!$player->is_ignored_id($user_id)) {
-                    $player->socket->write('chat`' . $chat_message);
-                }
-            }
-        } catch (Exception $e) {
-            $err = $e->getMessage();
-            foreach($this->player_array as $player) {
-                if ($player->user_id == $user_id) {
-                    $player->socket->write("message`Error: $err");
-                    $player->socket->remove();
+                    $player->socket->write("$command`$name`$power`$text");
                 }
             }
         }
