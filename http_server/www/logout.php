339242a33927b32ab71b221faa3d7c0ca5783a0e
Dex47 <theplayer232@gmail.com>
2018-04-12 20:00:52 +0300

    Fix for TokenEater force logout exploit
    Fix for the exploit https://github.com/Dex472/TokenEater

--- http_server/www/logout.php
+++ http_server/www/logout.php
@@ -5,6 +5,22 @@ require_once __DIR__ . '/../queries/tokens/token_delete.php';
 
 header("Content-type: text/plain");
 
+function is_from_game(){
+ if (isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && !empty($_SERVER["HTTP_X_REQUESTED_WITH"])){
+  if (strpos($_SERVER["HTTP_X_REQUESTED_WITH"], "ShockwaveFlash/") !== 0){
+   return true;
+  }
+     else
+  {
+      return false;   
+  }
+ }
+ else
+ {
+    return false;
+ }
+}
+
 $ip = get_ip();
 
 try {
@@ -12,6 +28,10 @@ try {
     rate_limit('logout-'.$ip, 5, 2, 'Please wait at least 5 seconds before attempting to log out again.');
     rate_limit('logout-'.$ip, 60, 10, 'Only 10 logout requests per minute per IP are accepted.');
 
+    if (is_from_game() !== true){
+     throw new Exception("For security reasons logout can only be performed from game");   
+    }
+    
     if (isset($_COOKIE['token'])) {
         // connect to the db
         $pdo = pdo_connect();
