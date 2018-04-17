a11043a35a99a1ca5b7cd309dbcec658d5a5a7fa
Ben <bls1999@users.noreply.github.com>
2018-04-17 18:04:00 -0400

    appease travis -> logout.php

--- http_server/www/logout.php
+++ http_server/www/logout.php
@@ -14,8 +14,8 @@ try {
 
     if (is_from_game() !== true) {
         throw new Exception(
-            "It looks like you're not using PR2 to log out. "
-           ."For security reasons, you may only log out from a PR2 client."
+            "It looks like you're not using PR2 to log out. ".
+            "For security reasons, you may only log out from a PR2 client."
         );
     }
 
