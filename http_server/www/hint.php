e99a9da4317396206e1ad2c322ee3311085130f5
Ben <bls1999@users.noreply.github.com>
2018-04-17 18:26:33 -0400

    Clean hint.php
    Remove unnecessary try/catch, add last updated time.

--- http_server/www/hint.php
+++ http_server/www/hint.php
@@ -4,22 +4,38 @@ require_once __DIR__ . '/../fns/output_fns.php';
 
 output_header('Artifact Hint');
 
-echo '<center><font face="Gwibble" class="gwibble">-- Artifact Hint --</font>
-	<br/>
-	<img src="img/artifact.png" width="80px" height="70px"></img>
-	<br/>';
-try {
-    $decode = json_decode(file_get_contents("https://pr2hub.com/files/artifact_hint.txt"));
-    echo "Here's what Fred can remember: " . htmlspecialchars($decode->hint) . "<br />";
-    if ($decode->finder_name == "") {
-        echo "<i><b><br />This artifact hasn't been found yet!</i>";
-    } else {
-        echo "<br /> The first person to find this artifact was " . htmlspecialchars($decode->finder_name) . "!!</i>";
-    }
-} catch (Exception $msg) {
-    $error_message = htmlspecialchars("Error: " . $msg->getMessage());
-    echo $error_message;
-}
+echo '<center>'
+    .'<font face="Gwibble" class="gwibble">-- Artifact Hint --</font>'
+    .'<br>'
+    .'<img src="img/artifact.png" width="80px" height="70px"></img>'
+    .'<br>';
 
+// get data, make variables
+$decode = json_decode(file_get_contents("https://pr2hub.com/files/artifact_hint.txt"));
+$safe_hint = htmlspecialchars($decode->hint);
+$safe_finder = htmlspecialchars($decode->finder_name);
+
+// echo hint
+echo "Here's what Fred can remember: $safe_hint"
+    .'<br>';
+
+// echo finder if one, not found yet if none
+if (is_empty($decode->finder_name)) {
+    echo "<i><b>This artifact hasn't been found yet!</b></i>";
+} else {
+    echo "<i>The first person to find this artifact was <b>$safe_finder</b>!!</i>";
+}
+    
+// get last updated time
+$timestamp = (int) (time() - $decode->updated_time);
+$formatted_date = date('M j, Y g:i A', $timestamp);
+$formatted_time = format_duration($timestamp);
+
+// echo last updated time
+echo '<br><br>'
+    ."Fred lost this artifact <span title='$formatted_date'>approximately $formatted_time ago</span>.";
+
+// seeya
 echo '</center>';
 output_footer();
+die();
