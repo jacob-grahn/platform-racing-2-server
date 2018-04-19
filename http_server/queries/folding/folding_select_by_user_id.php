db54babf44ff15275b87033fc19ddccc5065a1a2
Ben <bls1999@users.noreply.github.com>
2018-04-19 03:54:12 -0400

    fix query -> folding_select_by_user_id.php

--- http_server/queries/folding/folding_select_by_user_id.php
+++ http_server/queries/folding/folding_select_by_user_id.php
@@ -2,7 +2,13 @@
 
 function folding_select_by_user_id($pdo, $user_id, $suppress_error = false)
 {
-    $stmt = $pdo->prepare('SELECT * FROM folding_at_home WHERE user_id = :user_id LIMIT 1');
+    $stmt = $pdo->prepare('
+        SELECT fah.*, u.name, u.status
+          FROM folding_at_home fah, users u
+         WHERE u.user_id = fah.user_id
+           AND u.user_id = :user_id
+         LIMIT 1
+    ');
     $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $result = $stmt->execute();
     
@@ -14,9 +20,9 @@ function folding_select_by_user_id($pdo, $user_id, $suppress_error = false)
 
     if (empty($row)) {
         if ($suppress_error === false) {
-            throw new Exception('Could not find a folding_at_home entry for user #$user_id.');
+            throw new Exception("Could not find a folding_at_home entry for user #$user_id.");
         } else {
-            return false;
+            return NULL;
         }
     }
 
