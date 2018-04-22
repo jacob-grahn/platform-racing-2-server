0f2b071b30fbad24788c569247ed546e1ca90a22
Ben <bls1999@users.noreply.github.com>
2018-04-22 18:08:31 -0400

    Consolidate welcome pm to message_send_welcome

--- http_server/queries/users/register_user.php
+++ http_server/queries/users/register_user.php
@@ -11,17 +11,6 @@ function register_user($pdo, $name, $ip, $time, $email)
     $user_id = name_to_id($pdo, $name);
     pr2_insert($pdo, $user_id);
 
-    // compose a welcome pm
-    $safe_name = htmlspecialchars($name);
-    $welcome_message = "Welcome to Platform Racing 2, $safe_name!\n\n"
-        ."<a href='https://grahn.io' target='_blank'><u><font color='#0000FF'>"
-        ."Click here</font></u></a> to read about the latest Platform Racing news on my blog.\n\n"
-        ."If you have any questions or comments, send me an email at "
-        ."<a href='mailto:jacob@grahn.io?subject=Questions or Comments about "
-        ."PR2' target='_blank'><u><font color='#0000FF'>jacob@grahn.io</font></u></a>.\n\n"
-        ."Thanks for playing, I hope you enjoy.\n\n"
-        ."- Jacob";
-
     // welcome them
-    message_insert($pdo, $user_id, 1, $welcome_message, '0');
+    message_send_welcome($pdo, $name, $user_id);
 }
