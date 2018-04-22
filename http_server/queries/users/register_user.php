704f9d920673e903740c6fc843518aa020b781b1
Ben <bls1999@users.noreply.github.com>
2018-04-22 18:15:49 -0400

    add require links

--- http_server/queries/users/register_user.php
+++ http_server/queries/users/register_user.php
@@ -1,6 +1,11 @@
 <?php
 
-function register_user($pdo, $name, $ip, $time, $email)
+require_once __DIR__ . '/../fns/to_hash.php';
+require_once __DIR__ . '/../queries/users/user_insert.php';
+require_once __DIR__ . '/../queries/pr2/pr2_insert.php';
+require_once __DIR__ . '/../queries/messages/message_send_welcome.php';
+
+function register_user($pdo, $name, $password, $ip, $time, $email)
 {
     // user insert
     $pass_hash = to_hash($password);
