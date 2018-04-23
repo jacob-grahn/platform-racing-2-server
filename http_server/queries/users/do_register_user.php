2f9e6e4309c72eb0f720c930e7146efa5dc0bb00
Ben <bls1999@users.noreply.github.com>
2018-04-22 23:48:31 -0400

    appease travis pt. 2

--- http_server/queries/users/do_register_user.php
+++ http_server/queries/users/do_register_user.php
@@ -1,10 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../fns/to_hash.php';
-require_once __DIR__ . '/../queries/users/user_insert.php';
-require_once __DIR__ . '/../queries/pr2/pr2_insert.php';
-require_once __DIR__ . '/../queries/messages/message_send_welcome.php';
-
 function do_register_user($pdo, $name, $password, $ip, $time, $email)
 {
     // user insert
