fe4ac8a8adcf7dec5d4c94fd0cd896a760c3b08a
Ben <bls1999@users.noreply.github.com>
2018-04-24 03:26:03 -0400

    add ensure_awards to hourly cron

--- http_server/cron/hourly.php
+++ http_server/cron/hourly.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once __DIR__ . '/../fns/all_fns.php';
+require_once __DIR__ . '/../queries/part_awards/part_awards_select_list.php';
+require_once __DIR__ . '/../queries/part_awards/part_awards_delete_old.php';
+require_once __DIR__ . '/../queries/part_awards/ensure_awards.php';
 
 $pdo = pdo_connect();
 
@@ -8,5 +11,6 @@ generate_level_list($pdo, 'newest');
 generate_level_list($pdo, 'best');
 generate_level_list($pdo, 'best_today');
 generate_level_list($pdo, 'campaign');
+ensure_awards($pdo);
 
 echo 'result=ok';
