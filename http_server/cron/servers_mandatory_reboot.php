e82f1b80378330d564a3177b9d51457662138e23
Ben <bls1999@users.noreply.github.com>
2018-04-25 10:52:17 -0400

    Update servers_mandatory_reboot.php

--- http_server/cron/servers_mandatory_reboot.php
+++ http_server/cron/servers_mandatory_reboot.php
@@ -3,23 +3,25 @@
 require_once __DIR__ . '/../fns/all_fns.php';
 require_once __DIR__ . '/../queries/servers/servers_select.php';
 
-//--- load all servers
+// tell the command line
+$time = date('r');
+output("Mandatory server reboot CRON starting at $time...");
+
+// load all servers
 $pdo = pdo_connect();
 $servers = servers_select($pdo);
 
-//--- test all active servers at this address
+// shut down all active servers
 foreach ($servers as $server) {
-    output("Shutting down $server->server_name ($server->server_id)");
+    output("Shutting down $server->server_name (ID: #$server->server_id)...");
     try {
         $reply = talk_to_server($server->address, $server->port, $server->salt, 'shut_down`', true);
         output("Reply: $reply");
+        output("$server->server_name (ID #$server->server_id) shut down successful.");
     } catch (Exception $e) {
         output($e->getMessage());
     }
-
-    output('');
 }
 
-
-//--- tell it to the world
-output('done');
+// tell the command line
+output('Mandatory reboot finished.');
