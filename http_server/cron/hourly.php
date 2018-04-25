d9803c8399dfbf56b5aace8c05bb38de19ddfe54
Ben <bls1999@users.noreply.github.com>
2018-04-25 10:36:18 -0400

    Update hourly.php

--- http_server/cron/hourly.php
+++ http_server/cron/hourly.php
@@ -1,10 +1,18 @@
 <?php
 
+// all fns
 require_once __DIR__ . '/../fns/all_fns.php';
+
+// ensure part awards
 require_once __DIR__ . '/../queries/part_awards/part_awards_select_list.php';
 require_once __DIR__ . '/../queries/part_awards/part_awards_delete_old.php';
 require_once __DIR__ . '/../queries/part_awards/ensure_awards.php';
 
+// tell the command line
+$time = date('r');
+output("Hourly CRON starting at $time...");
+
+// connect
 $pdo = pdo_connect();
 
 generate_level_list($pdo, 'newest');
@@ -13,4 +21,5 @@ generate_level_list($pdo, 'best_today');
 generate_level_list($pdo, 'campaign');
 ensure_awards($pdo);
 
-output('result=ok');
+// tell the command line
+output('Hourly CRON successful.');
