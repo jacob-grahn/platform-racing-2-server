679d5d423081244acdf92ec6d207dd47fc80f5a4
Ben <bls1999@users.noreply.github.com>
2018-04-25 10:32:55 -0400

    Update delete_old_accounts.php

--- http_server/cron/delete_old_accounts.php
+++ http_server/cron/delete_old_accounts.php
@@ -11,8 +11,9 @@ $pdo = pdo_connect();
 $min_time = time() - (60 * 60 * 24 * 30 * 12 * 3); //three years
 
 $users = users_select_old($pdo);
+$num_users = number_format(count($users));
 
-echo(number_format(count($users)) . " accounts have not been logged into recently. \n");
+output("$num_users accounts have not been logged into recently.");
 
 foreach ($users as $row) {
     $user_id = $row->user_id;
@@ -22,9 +23,10 @@ foreach ($users as $row) {
 
     $str = "$user_id has $play_count level plays and is rank $rank.";
     if ($play_count > 100 || $rank > 15) {
-        output("$str SPARE \n");
+        output("$str Spared!");
     } else {
-        output("$str DELETE \n");
+        output("$str DELETING...");
         user_delete($pdo, $user_id);
+        output("$user_id was successfully deleted.");
     }
 }
