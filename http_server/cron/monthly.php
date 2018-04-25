a790626d831d07413d55c85edc47efffd286b28c
Ben <bls1999@users.noreply.github.com>
2018-04-25 10:49:02 -0400

    Update monthly.php

--- http_server/cron/monthly.php
+++ http_server/cron/monthly.php
@@ -5,10 +5,16 @@ require_once __DIR__ . '/../queries/best_levels/best_levels_monthly.php';
 require_once __DIR__ . '/../queries/messages/messages_delete_old.php';
 require_once __DIR__ . '/../queries/misc/all_optimize.php';
 
+// tell the command line
+$time = date('r');
+output("Monthly CRON starting at $time...");
+
+// connect
 $pdo = pdo_connect();
 
 best_levels_monthly($pdo);
 messages_delete_old($pdo);
 all_optimize($pdo);
 
-output('result=ok');
+// tell the command line
+output('Monthly CRON successful.');
