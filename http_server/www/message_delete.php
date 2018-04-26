d67c9c5bf018d366152fa66d1e1ce875af333744
Ben <bls1999@users.noreply.github.com>
2018-04-26 01:20:10 -0400

    guest check -> message_delete.php

--- http_server/www/message_delete.php
+++ http_server/www/message_delete.php
@@ -25,6 +25,13 @@ try {
 
     // check their login
     $user_id = token_login($pdo);
+    $power = user_select_power($pdo, $user_id);
+    if ($power <= 0) {
+        throw new Exception(
+            "Guests can't use the private messaging system. ".
+            "To access this feature, please create your own account."
+        );
+    }
 
     // more rate limiting
     rate_limit('message-delete'.$user_id, 5, 2, "Please wait at least 5 seconds before trying to delete another PM.");
