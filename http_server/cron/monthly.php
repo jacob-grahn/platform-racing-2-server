285fd730957b52215181b6019cc719d2755c2678
Ben <bls1999@users.noreply.github.com>
2018-04-26 22:34:19 -0400

    add delete_old_accounts to monthly.php

--- http_server/cron/monthly.php
+++ http_server/cron/monthly.php
@@ -1,9 +1,22 @@
 <?php
 
+// all fns
 require_once __DIR__ . '/../fns/all_fns.php';
+
+// atb reset
 require_once __DIR__ . '/../queries/best_levels/best_levels_monthly.php';
+
+// delete old messages
 require_once __DIR__ . '/../queries/messages/messages_delete_old.php';
+
+// optimize tables
 require_once __DIR__ . '/../queries/misc/all_optimize.php';
+
+// delete old accounts
+require_once __DIR__ . '/../queries/users/delete_old_accounts.php';
+require_once __DIR__ . '/../queries/users/users_select_old.php';
+require_once __DIR__ . '/../queries/users/user_select_level_plays.php';
+require_once __DIR__ . '/../queries/users/user_delete.php';
 
 // tell the command line
 $time = date('r');
@@ -15,6 +28,7 @@ $pdo = pdo_connect();
 best_levels_monthly($pdo);
 messages_delete_old($pdo);
 all_optimize($pdo);
+delete_old_accounts($pdo);
 
 // tell the command line
 output('Monthly CRON successful.');
