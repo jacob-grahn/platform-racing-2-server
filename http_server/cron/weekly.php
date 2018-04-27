592d02e1e11e1f4f9e177ad5295293f1908d142f
Ben <bls1999@users.noreply.github.com>
2018-04-26 22:57:47 -0400

    move a bunch of things from daily to weekly
     - level_backups_delete_old (it's not the end of the world if people can recover their levels for a few more days)
     - new_levels_delete_old (newest can never make page 9, this should fix that)
     - bans_delete_old (this doesn't have to be done daily)
     - users_reset_status (^)
     - servers_restart_all (this already happens weekly)

--- http_server/cron/weekly.php
+++ http_server/cron/weekly.php
@@ -3,11 +3,21 @@
 // all fns
 require_once __DIR__ . '/../fns/all_fns.php';
 
+// delete old...
+require_once __DIR__ . '/../queries/level_backups/level_backups_delete_old.php'; // level backups
+require_once __DIR__ . '/../queries/new_levels/new_levels_delete_old.php'; // newest levels
+require_once __DIR__ . '/../queries/messages/messages_delete_old.php'; // messages
+require_once __DIR__ . '/../queries/bans/bans_delete_old.php'; // expired/old bans
+
+// reset statuses (users who are offline should appear offline)
+require_once __DIR__ . '/../queries/users/users_reset_status.php';
+
 // atb reset
 require_once __DIR__ . '/../queries/best_levels/best_levels_reset.php';
 
-// delete old messages
-require_once __DIR__ . '/../queries/messages/messages_delete_old.php';
+// restart the servers
+require_once __DIR__ . '/../queries/servers/servers_select.php';
+require_once __DIR__ . '/../queries/servers/servers_restart_all.php';
 
 // optimize tables
 require_once __DIR__ . '/../queries/misc/all_optimize.php';
@@ -20,15 +30,20 @@ require_once __DIR__ . '/../queries/users/user_delete.php';
 
 // tell the command line
 $time = date('r');
-output("Monthly CRON starting at $time...");
+output("Weekly CRON starting at $time...");
 
 // connect
 $pdo = pdo_connect();
 
-best_levels_reset($pdo);
+level_backups_delete_old($pdo);
+new_levels_delete_old($pdo);
 messages_delete_old($pdo);
+bans_delete_old($pdo);
+users_reset_status($pdo);
+best_levels_reset($pdo);
+servers_restart_all($pdo);
 all_optimize($pdo);
 delete_old_accounts($pdo);
 
 // tell the command line
-output('Monthly CRON successful.');
+output('Weekly CRON successful.');
