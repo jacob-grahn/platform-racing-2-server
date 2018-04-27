fd24ae7b4578f22a65b167d8cce17841e770a4d0
Ben <bls1999@users.noreply.github.com>
2018-04-27 08:58:33 -0400

    remove pdo_fah_connect

--- http_server/fns/pdo_connect.php
+++ http_server/fns/pdo_connect.php
@@ -10,14 +10,3 @@ function pdo_connect()
         throw new Exception('Could not connect to the database.');
     }
 }
-
-function pdo_fah_connect()
-{
-    try {
-        global $DB_ADDRESS, $DB_FAH_USER, $DB_FAH_PASS, $DB_FAH_NAME, $DB_PORT;
-        return new PDO("mysql:host=$DB_ADDRESS;port=$DB_PORT;dbname=$DB_FAH_NAME", $DB_FAH_USER, $DB_FAH_PASS);
-    } catch (PDOException $e) {
-        // throw a custom error to make sure an error containing db info is not shown
-        throw new Exception('Could not connect to the database.');
-    }
-}
