f9b4e6b75702cea9a5019959db45739ebdda8696
Ben <bls1999@users.noreply.github.com>
2018-05-01 23:04:56 -0400

    Create multi_queries.php

---
+++ multi_queries.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/http_server/queries/artifact_locations/artifact_location_select.php';
+require_once __DIR__ . '/http_server/queries/artifact_locations/artifact_location_update_first_finder.php';
+require_once __DIR__ . '/http_server/queries/artifacts_found/artifacts_found_select_time.php';
+require_once __DIR__ . '/http_server/queries/artifacts_found/artifacts_found_insert.php';
+require_once __DIR__ . '/http_server/queries/campaign/campaign_select.php';
+require_once __DIR__ . '/http_server/queries/epic_upgrades/epic_upgrades_upsert.php';
+require_once __DIR__ . '/http_server/queries/exp_today/exp_today_add.php';
+require_once __DIR__ . '/http_server/queries/guilds/guild_select.php';
+require_once __DIR__ . '/http_server/queries/messages/message_insert.php';
+require_once __DIR__ . '/http_server/queries/mod_powers/mod_power_delete.php';
+require_once __DIR__ . '/http_server/queries/mod_powers/mod_power_insert.php';
+require_once __DIR__ . '/http_server/queries/pr2/pr2_update.php';
+require_once __DIR__ . '/http_server/queries/purchases/purchases_select_recent.php';
+require_once __DIR__ . '/http_server/queries/promotion_logs/promotion_log_count.php';
+require_once __DIR__ . '/http_server/queries/promotion_logs/promotion_log_insert.php';
+require_once __DIR__ . '/http_server/queries/rank_tokens/rank_token_update.php';
+require_once __DIR__ . '/http_server/queries/staff/actions/admin_action_insert.php';
+require_once __DIR__ . '/http_server/queries/staff/actions/mod_action_insert.php';
+require_once __DIR__ . '/http_server/queries/staff/actions/admin_action_insert.php';
+require_once __DIR__ . '/http_server/queries/servers/server_select.php';
+require_once __DIR__ . '/http_server/queries/users/user_update_status.php';
+require_once __DIR__ . '/http_server/queries/users/user_select.php';
+require_once __DIR__ . '/http_server/queries/users/user_select_by_name.php';
+require_once __DIR__ . '/http_server/queries/users/user_update_power.php';
