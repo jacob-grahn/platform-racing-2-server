41abd08deb62addbf37db3851f8e89011f9d8898
Ben <bls1999@users.noreply.github.com>
2018-05-01 23:30:54 -0400

    Move: vault fns+shutdown_server -> utils, organize

--- multiplayer_server/fns/utils.php
+++ multiplayer_server/fns/utils.php
@@ -1,5 +1,147 @@
 <?php
 
+// get dat dar login id
+function get_login_id()
+{
+    static $cur_login_id = 0;
+    $cur_login_id++;
+    return $cur_login_id;
+}
+
+
+// takes an id and returns a player
+function id_to_player($id, $throw_exception = true)
+{
+    global $player_array;
+    $player = @$player_array[$id];
+    if (!isset($player) && $throw_exception === true) {
+        throw new Exception('Could not find a player with that ID.');
+    }
+    return $player;
+}
+
+
+// takes a name and returns a player
+function name_to_player($name)
+{
+    global $player_array;
+    $return_player = null;
+    foreach ($player_array as $player) {
+        if (strtolower($player->name) == strtolower($name)) {
+            $return_player = $player;
+            break;
+        }
+    }
+    return $return_player;
+}
+
+
+// get an existing chat room, or make a new one
+function get_chat_room($chat_room_name)
+{
+    global $chat_room_array;
+    if (isset($chat_room_array[$chat_room_name])) {
+        return $chat_room_array[$chat_room_name];
+    } else {
+        $chat_room = new \pr2\multi\ChatRoom($chat_room_name);
+        return $chat_room;
+    }
+}
+
+
+// accept bans from other servers
+function apply_bans($bans)
+{
+    global $player_array;
+
+    foreach ($bans as $ban) {
+        foreach ($player_array as $player) {
+            if ($player->ip == $ban->banned_ip || $player->user_id == $ban->banned_user_id) {
+                $player->remove();
+            }
+        }
+    }
+}
+
+
+// send new pm notifications out
+function pm_notify($pms)
+{
+    global $player_array;
+
+    foreach ($pms as $pm) {
+        if (isset($player_array[ $pm->to_user_id ])) {
+            $player = $player_array[ $pm->to_user_id ];
+            $player->write('pmNotify`' . $pm->message_id);
+        }
+    }
+}
+
+
+// place the artifact
+function place_artifact($artifact)
+{
+    output("place_artifact: " . json_encode($artifact));
+    \pr2\multi\Artifact::$level_id = $artifact->level_id;
+    \pr2\multi\Artifact::$x = $artifact->x;
+    \pr2\multi\Artifact::$y = $artifact->y;
+    \pr2\multi\Artifact::$updated_time = strtotime($artifact->updated_time);
+    \pr2\multi\Artifact::$first_finder = $artifact->first_finder;
+}
+
+
+// get the plays we've been holding
+function drain_plays()
+{
+    global $play_count_array;
+    $cup = array();
+
+    foreach ($play_count_array as $course => $plays) {
+        $cup[ $course ] = $plays;
+    }
+
+    $play_count_array = array();
+
+    return $cup;
+}
+
+
+// get server population
+function get_population()
+{
+    global $player_array;
+    $pop = count($player_array);
+    return $pop;
+}
+
+
+// get server status
+function get_status()
+{
+    global $player_array;
+    global $max_players;
+    $status = 'open';
+    if (count($player_array) >= $max_players) {
+        $status = 'full';
+    }
+    return $status;
+}
+
+
+// graceful shutdown
+function shutdown_server()
+{
+    global $player_array;
+    foreach ($player_array as $player) {
+        $player->write('message`The server is restarting, hold on a sec...');
+        $player->remove();
+    }
+    sleep(1);
+    exit();
+}
+
+
+// send to all online players
 function sendToAll_players($str)
 {
     global $player_array;
@@ -8,6 +150,8 @@ function sendToAll_players($str)
     }
 }
 
+
+// send to all members of a guild
 function send_to_guild($guild_id, $str)
 {
     global $player_array;
@@ -17,6 +161,44 @@ function send_to_guild($guild_id, $str)
         }
     }
 }
+
+
+// vault: start a perk
+function start_perk($slug, $user_id, $guild_id)
+{
+    output("start_perk - slug: $slug, user_id: $user_id, guild_id: $guild_id");
+    $seconds_duration = 3700;
+    if ($slug == \pr2\multi\Perks::GUILD_FRED) {
+        assign_guild_part('body', 29, $user_id, $guild_id, $seconds_duration);
+    } elseif ($slug == \pr2\multi\Perks::GUILD_GHOST) {
+        assign_guild_part('head', 31, $user_id, $guild_id, $seconds_duration);
+        assign_guild_part('body', 30, $user_id, $guild_id, $seconds_duration);
+        assign_guild_part('feet', 27, $user_id, $guild_id, $seconds_duration);
+    } elseif ($slug == \pr2\multi\Perks::GUILD_ARTIFACT) {
+        assign_guild_part('hat', 14, $user_id, $guild_id, $seconds_duration);
+        assign_guild_part('eHat', 14, $user_id, $guild_id, $seconds_duration);
+    } elseif ($slug == \pr2\multi\Perks::HAPPY_HOUR) {
+        \pr2\multi\HappyHour::activate();
+    }
+}
+
+
+// vault: assign a part bought for a guild
+function assign_guild_part($type, $part_id, $user_id, $guild_id, $seconds_duration)
+{
+    global $player_array;
+
+    \pr2\multi\TemporaryItems::add($type, $part_id, $user_id, $guild_id, $seconds_duration);
+
+    foreach ($player_array as $player) {
+        if ($player->guild_id == $guild_id) {
+            // $player->gainPart('e'.ucfirst($type), $part_id);
+            $player->setPart($type, $part_id);
+            $player->sendCustomizeInfo();
+        }
+    }
+}
+
 
 // limit the amount of times an action can be performed in a certain time period
 function rate_limit($key, $interval, $max, $display_error = false, $player = null, $error = 'Slow down a bit, yo.')
@@ -39,159 +221,7 @@ function rate_limit($key, $interval, $max, $display_error = false, $player = nul
 }
 
 
-//--- takes an id and returns a player ---------------------------------------
-function id_to_player($id, $throw_exception = true)
-{
-    global $player_array;
-    $player = @$player_array[$id];
-    if (!isset($player) && $throw_exception) {
-        throw new Exception('Player id does not exist.');
-    }
-    return $player;
-}
-
-
-
-//--- takes a name and returns a player --------------------------------------
-function name_to_player($name)
-{
-    global $player_array;
-    $return_player = null;
-    foreach ($player_array as $player) {
-        if (strtolower($player->name) == strtolower($name)) {
-            $return_player = $player;
-            break;
-        }
-    }
-    return $return_player;
-}
-
-
-
-//--- checks to see if an ip is banned ---------------------------------------
-function get_banned_ip($ip)
-{
-    global $banned_ip_array;
-    $banned_ip = @$banned_ip_array[$ip];
-    if (isset($banned_ip)) {
-        if ($banned_ip->time > time()) {
-            return $banned_ip;
-        } else {
-            $banned_ip->remove();
-            return false;
-        }
-    } else {
-        return false;
-    }
-}
-
-
-//--- get dat dar login id --------------------------------------------------
-function get_login_id()
-{
-    static $cur_login_id = 0;
-    $cur_login_id++;
-    return $cur_login_id;
-}
-
-
-//--- get an existing chat room, or make a new one --------------------------
-function get_chat_room($chat_room_name)
-{
-    global $chat_room_array;
-    if (isset($chat_room_array[$chat_room_name])) {
-        return $chat_room_array[$chat_room_name];
-    } else {
-        $chat_room = new \pr2\multi\ChatRoom($chat_room_name);
-        return $chat_room;
-    }
-}
-
-
-//--- accept bans from other servers ----------------------------------------
-function apply_bans($bans)
-{
-    global $player_array;
-
-    foreach ($bans as $ban) {
-        foreach ($player_array as $player) {
-            if ($player->ip == $ban->banned_ip || $player->user_id == $ban->banned_user_id) {
-                $player->remove();
-            }
-        }
-    }
-}
-
-
-
-//--- send new pm notifications out -----------------------------------------
-function pm_notify($pms)
-{
-    global $player_array;
-
-    foreach ($pms as $pm) {
-        if (isset($player_array[ $pm->to_user_id ])) {
-            $player = $player_array[ $pm->to_user_id ];
-            $player->write('pmNotify`' . $pm->message_id);
-        }
-    }
-}
-
-
-
-//---
-function place_artifact($artifact)
-{
-    output("place_artifact: " . json_encode($artifact));
-    \pr2\multi\Artifact::$level_id = $artifact->level_id;
-    \pr2\multi\Artifact::$x = $artifact->x;
-    \pr2\multi\Artifact::$y = $artifact->y;
-    \pr2\multi\Artifact::$updated_time = strtotime($artifact->updated_time);
-    \pr2\multi\Artifact::$first_finder = $artifact->first_finder;
-}
-
-
-
-//--- get the plays we've been holding ------------------------------------------
-function drain_plays()
-{
-    global $play_count_array;
-    $cup = array();
-
-    foreach ($play_count_array as $course => $plays) {
-        $cup[ $course ] = $plays;
-    }
-
-    $play_count_array = array();
-
-    return $cup;
-}
-
-
-
-//--- get population
-function get_population()
-{
-    global $player_array;
-    $pop = count($player_array);
-    return $pop;
-}
-
-
-
-//--- get status
-function get_status()
-{
-    global $player_array;
-    global $max_players;
-    $status = 'open';
-    if (count($player_array) >= $max_players) {
-        $status = 'full';
-    }
-    return $status;
-}
-
-
+// handy output function; never leave home without it!
 function output($str)
 {
     echo "* $str\n";
