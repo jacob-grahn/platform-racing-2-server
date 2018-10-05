1adfae4be9b9372636dd26b113b83118245112e8
Anthony <35195062+BrokenBulb78@users.noreply.github.com>
2018-10-05 15:21:03 +0300

    display name regardless of type

--- http_server/www/bans/bans.php
+++ http_server/www/bans/bans.php
@@ -44,42 +44,38 @@ try {
         $banned_user_id = $row->banned_user_id;
         $time = $row->time;
         $expire_time = $row->expire_time;
-        $reason = $row->reason;
-        $mod_name = $row->mod_name;
-        $banned_name = $row->banned_name;
+        $reason = htmlspecialchars($row->reason);
+        $mod_name = htmlspecialchars($row->mod_name);
+        $banned_name = htmlspecialchars($row->banned_name);
         $ip_ban = $row->ip_ban;
         $account_ban = $row->account_ban;
 
         $formatted_time = date('M j, Y g:i A', $time);
         $duration = $expire_time - $time;
 
-        $display_name = '';
-        if ($account_ban == 1) {
-            $display_name .= $banned_name;
-        }
         if ($ip_ban == 1 && $is_mod) {
-            if ($display_name != '') {
-                $display_name .= ' ';
+            if ($banned_name != '') {
+                $banned_name .= ' ';
             }
-            $display_name .= "[$banned_ip]";
+            $banned_name .= "[$banned_ip]";
         }
 
         $reason = htmlspecialchars($reason);
         $f_duration = format_duration($duration);
 
         echo "<p>$formatted_time
-    			<a href='show_record.php?ban_id=$ban_id'>
-    			".htmlspecialchars($mod_name)." banned ".htmlspecialchars($display_name)." for $f_duration.</a><br/>
-    			Reason: ".htmlspecialchars($reason)."
-    			</p>";
+                <a href='show_record.php?ban_id=$ban_id'>
+                $mod_name banned $banned_name for $f_duration.</a><br/>
+                Reason: $reason
+                </p>";
     }
 
     echo '<p>---</p>';
     output_pagination($start, $count);
-    output_footer();
 } catch (Exception $e) {
     $error = $e->getMessage();
     output_header('Error');
     echo "Error: $error";
+} finally {
     output_footer();
 }
