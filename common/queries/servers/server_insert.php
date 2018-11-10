dd3898b73bb8d7ff65c34b658d04263a51cd0e03
Jacob Grahn <contact@jacobgrahn.com>
2018-11-10 11:46:56 -0500

    fix vault private servers some more

--- common/queries/servers/server_insert.php
+++ common/queries/servers/server_insert.php
@@ -1,13 +1,14 @@
 <?php
 
-function server_insert($pdo, $server_name, $address, $port, $expire_date, $salt, $guild_id)
+function server_insert($pdo, $server_name, $address, $port, $expire_timestamp, $salt, $guild_id)
 {
+    $expire_date = date("Y-m-d H:i:s", $expire_timestamp);
     $stmt = $pdo->prepare('
         INSERT INTO servers
            SET server_name = :server_name,
                address = :address,
                port = :port,
-               expire_date = FROM_UNIXTIME(:expire_date),
+               expire_date = :expire_date,
                active = true,
                salt = :salt,
                guild_id = :guild_id
@@ -19,7 +20,7 @@ function server_insert($pdo, $server_name, $address, $port, $expire_date, $salt,
     $stmt->bindValue(':salt', $salt, PDO::PARAM_STR);
     $stmt->bindValue(':guild_id', $guild_id, PDO::PARAM_INT);
     $result = $stmt->execute();
-    
+
     if ($result === false) {
         throw new Exception('Could not create server.');
     }
--- common/queries/servers/server_update_expire_date.php
+++ common/queries/servers/server_update_expire_date.php
@@ -1,19 +1,20 @@
 <?php
 
-function server_update_expire_date($pdo, $server_id, $expire_date, $server_name)
+function server_update_expire_date($pdo, $server_id, $expire_timestamp, $server_name)
 {
+    $expire_date = date("Y-m-d H:i:s", $expire_timestamp);
     $stmt = $pdo->prepare('
         UPDATE servers
-           SET expire_date = FROM_UNIXTIME(:expire_date),
+           SET expire_date = :expire_date,
                server_name = :server_name,
                active = 1
          WHERE server_id = :server_id
     ');
     $stmt->bindValue(':server_id', $server_id, PDO::PARAM_INT);
     $stmt->bindValue(':server_name', $server_name, PDO::PARAM_STR);
-    $stmt->bindValue(':expire_date', $expire_date, PDO::PARAM_INT);
+    $stmt->bindValue(':expire_date', $expire_date, PDO::PARAM_STR);
     $result = $stmt->execute();
-    
+
     if ($result === false) {
         throw new Exception('Could not update server expire date.');
     }
