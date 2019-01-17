8d0bf9be96df39dabea7a5f568981770e7160a7b
Ben <bls1999@users.noreply.github.com>
2019-01-16 23:29:34 -0500

    redundant

--- http_server/contests/award_prize.php
+++ http_server/contests/award_prize.php
@@ -28,7 +28,7 @@ try {
     $pdo = pdo_connect();
 
     // determine user id
-    $user_id = (int) token_login($pdo, true);
+    $user_id = (int) token_login($pdo);
     $is_staff = is_staff($pdo, $user_id, false);
     $is_mod = $is_staff->mod;
     $is_admin = $is_staff->admin;
