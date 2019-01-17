17c4c28d03ec6bdba5408d9d03bc75d52d18a946
Ben <bls1999@users.noreply.github.com>
2019-01-16 21:43:06 -0500

    forgot a file! sorry!

--- config.php
+++ config.php
@@ -7,20 +7,22 @@ define('COMMON_DIR', $directory . '/common'); // common
 define('QUERIES_DIR', $directory . '/common/queries'); // queries
 
 define('PR2_ROOT', $directory . '/multiplayer_server'); // socket server root
-define('PR2_FNS_DIR', $directory . '/multiplayer_server/fns'); // socket server fns
+define('WWW_ROOT', $directory . '/http_server'); // pr2hub root directory folder
 
-define('HTTP_FNS', $directory . '/functions/http_fns'); // http_server/fns
-define('WWW_ROOT', $directory . '/http_server'); // main folder
+define('FNS_DIR', $directory . '/functions'); // fns folder
+define('HTTP_FNS', $directory . '/functions/http_fns'); // fns/http_fns
+define('PR2_FNS', $directory . '/functions/multi_fns'); // socket server fns
+define('GEN_HTTP_FNS', HTTP_FNS . '/gen_http_fns.php');
+define('ALL_MULTI_FNS', PR2_FNS . '/all_multi_fns.php');
 
 define('SOCKET_DAEMON_FILES', $directory . '/vend/socket/index.php'); // files for phpSocketDaemon
 
 define('FRED', 4291976); // fred id
 $special_ids = array(FRED, 5321458, 5451130); // fred, sir, clint
+$group_colors = ['676666', '047b7b', '1c369f', '870a6f']; // group colors as defined in the client
 
 // call globally needed files
 require_once COMMON_DIR . '/env.php';
 require_once COMMON_DIR . '/pdo_connect.php';
 require_once COMMON_DIR . '/s3_connect.php';
 require_once ROOT_DIR . '/vend/S3.php';
-
-?>
