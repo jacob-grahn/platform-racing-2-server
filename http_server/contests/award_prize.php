76f4f275c2edb1af79d5fc4dae00627c4415a16e
Ben <bls1999@users.noreply.github.com>
2019-01-16 23:26:32 -0500

    Update award_prize.php

--- http_server/contests/award_prize.php
+++ http_server/contests/award_prize.php
@@ -29,7 +29,7 @@ try {
 
     // determine user id
     $user_id = (int) token_login($pdo, true);
-    $is_staff = is_staff($pdo, $user_id);
+    $is_staff = is_staff($pdo, $user_id, false);
     $is_mod = $is_staff->mod;
     $is_admin = $is_staff->admin;
 
