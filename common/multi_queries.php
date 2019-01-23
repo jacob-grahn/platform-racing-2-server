31c489e7b65c30c9928ccf2d0c6108bdaa0cd7dc
Ben <bls1999@users.noreply.github.com>
2019-01-22 23:08:36 -0500

    alphabetize

--- common/multi_queries.php
+++ common/multi_queries.php
@@ -1,19 +1,19 @@
 <?php
 
+require_once QUERIES_DIR . '/admin_actions.php';
 require_once QUERIES_DIR . '/artifact_location.php';
 require_once QUERIES_DIR . '/artifacts_found.php';
+require_once QUERIES_DIR . '/bans.php';
 require_once QUERIES_DIR . '/campaigns.php';
 require_once QUERIES_DIR . '/epic_upgrades.php';
 require_once QUERIES_DIR . '/exp_today.php';
 require_once QUERIES_DIR . '/guilds.php';
 require_once QUERIES_DIR . '/messages.php';
+require_once QUERIES_DIR . '/mod_actions.php';
 require_once QUERIES_DIR . '/mod_power.php';
 require_once QUERIES_DIR . '/pr2.php';
 require_once QUERIES_DIR . '/purchases.php';
 require_once QUERIES_DIR . '/promotion_log.php';
 require_once QUERIES_DIR . '/rank_tokens.php';
-require_once QUERIES_DIR . '/admin_actions.php';
-require_once QUERIES_DIR . '/mod_actions.php';
 require_once QUERIES_DIR . '/servers.php';
 require_once QUERIES_DIR . '/users.php';
-require_once QUERIES_DIR . '/bans.php';
