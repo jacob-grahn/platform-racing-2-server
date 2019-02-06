ce0832aaa37ccc55e73e024aaf458d48cea881f2
Ben <bls1999@users.noreply.github.com>
2019-02-06 15:33:47 -0500

    move fn

--- common/queries/part_awards.php
+++ common/queries/part_awards.php
@@ -1,33 +1,6 @@
 <?php
 
 
-function ensure_awards($pdo)
-{
-    // select all records, they get cleared out weekly or somesuch
-    $awards = part_awards_select_list($pdo);
-
-    // give users their awards
-    foreach ($awards as $row) {
-        if ($row->part == 0) {
-            $part = '*';
-        } else {
-            $part = $row->part;
-        }
-        $type = $row->type;
-        $user_id = $row->user_id;
-        try {
-            award_part($pdo, $user_id, $type, $part, false);
-            echo "user_id: $user_id, type: $type, part: $part \n";
-        } catch (Exception $e) {
-            echo "Error: $e \n";
-        }
-    }
-
-    // delete older records
-    part_awards_delete_old($pdo);
-}
-
-
 function part_awards_delete_old($pdo)
 {
     $result = $pdo->exec('
