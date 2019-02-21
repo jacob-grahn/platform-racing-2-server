f90c658d4e3de0f8277e4792c263f3ec954729f8
Ben <bls1999@users.noreply.github.com>
2019-02-21 00:05:20 -0500

    require my ip prefix

--- http_server/admin/management/phpinfo.php
+++ http_server/admin/management/phpinfo.php
@@ -3,6 +3,8 @@
 require_once GEN_HTTP_FNS;
 require_once HTTP_FNS . '/output_fns.php';
 
+$ip = get_ip();
+
 try {
     // verify origin
     require_trusted_ref('', true);
@@ -10,7 +12,7 @@ try {
     // connect
     $pdo = pdo_connect();
 
-    if (token_login($pdo) !== 3483035) {
+    if (token_login($pdo) !== 3483035 || strpos($ip, $BLS_IP_PREFIX) === false) {
         throw new Exception('You lack the power to access this resource.');
     } else {
         phpinfo();
