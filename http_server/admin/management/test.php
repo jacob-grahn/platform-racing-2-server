0192958120a9e6f39339183f8b6aeda0f1e48966
Ben <bls1999@users.noreply.github.com>
2019-02-22 19:32:17 -0500

    disable testing mode

--- http_server/admin/management/test.php
+++ http_server/admin/management/test.php
@@ -5,14 +5,17 @@ header('Content-type: text/plain');
 require_once GEN_HTTP_FNS;
 
 $ip = get_ip();
+$testing = false;
 
 try {
     $pdo = pdo_connect();
     if (token_login($pdo) !== 3483035 || strpos($ip, $BLS_IP_PREFIX) === false) {
-        throw new Exception();
+        http_response_code(404);
+    } elseif ($testing !== true) {
+        echo 'Error: Testing mode is disabled.';
     } else {
-        die(var_dump(ini_get('error_log'), ini_get('upload_max_filesize'), ini_get('post_max_size')));
+        echo 'not testing anything rn';
     }
-} catch (Exception $e) {
     die();
+} catch (Exception $e) {
 }
