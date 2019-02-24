950f6dc2b53abbd50458cc7ac903e172cdcde8a5
Jacob Grahn <jacob@grahn.io>
2019-02-24 08:36:10 -0500

    add script to fetch client (#563)

---
+++ common/cron/fetch-latest-client.sh
@@ -0,0 +1,30 @@
+# Validate settings.
+[ -f ~/.secrets ] && source ~/.secrets
+[ "$GITHUB_API_TOKEN" ] || { echo "Error: Please define GITHUB_API_TOKEN variable." >&2; exit 1; }
+[ $# -ne 3 ] && { echo "Usage: $0 [owner] [repo] [name]"; exit 1; }
+[ "$TRACE" ] && set -x
+read owner repo name <<<$@
+
+# Define variables.
+GH_API="https://api.github.com"
+GH_REPO="$GH_API/repos/$owner/$repo"
+GH_TAGS="$GH_REPO/releases/latest"
+AUTH="Authorization: token $GITHUB_API_TOKEN"
+WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
+CURL_ARGS="-LJO#"
+
+# Validate token.
+curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
+
+# Read asset tags.
+response=$(curl -sH "$AUTH" $GH_TAGS)
+# Get ID of the asset based on given name.
+eval $(echo "$response" | grep -C3 "name.:.\+$name" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+#id=$(echo "$response" | jq --arg name "$name" '.assets[] | select(.name == $name).id') # If jq is installed, this can be used instead.
+[ "$id" ] || { echo "Error: Failed to get asset id, response: $response" | awk 'length($0)<100' >&2; exit 1; }
+GH_ASSET="$GH_REPO/releases/assets/$id"
+
+# Download asset file.
+echo "Downloading asset..." >&2
+curl $CURL_ARGS -H 'Accept: application/octet-stream' "$GH_ASSET?access_token=$GITHUB_API_TOKEN"
+echo "$0 done." >&2
