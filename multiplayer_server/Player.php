5fcf5c2a1315599e022602a202e713d18f9fff70
Ben <bls1999@users.noreply.github.com>
2019-05-02 16:36:41 -0400

    anniversary message

--- multiplayer_server/Player.php
+++ multiplayer_server/Player.php
@@ -184,6 +184,17 @@ class Player
             $this->verifyStats();
             $this->verifyParts();
         }
+
+        if (date('M j') === 'May 2') {
+            $message = "PR2 turns 11 years old today! Happy anniversary!\n\n"
+                ."To celebrate, Fred will give you double EXP on all servers today "
+                ."(in addition to any other bonuses in effect). "
+                ."He also set up a sale in the vault (which is only available on "
+                .urlify('https://kongregate.com/games/Jiggmin/platform-racing-2', 'Kongregate')
+                ."). Thanks, Fred! :)\n\n"
+                ."Thanks for your support over the years. Here's to many more!\n- Jiggmin";
+            $this->write("message`$message");
+        }
     }
 
     private function safeExplode($str_arr)
