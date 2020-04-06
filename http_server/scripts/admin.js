6ff258e81ed83513371ec974cac00809c881e72a
Ben <bls1999@users.noreply.github.com>
2020-04-06 00:02:40 -0400

    wtf, travis screams about js now??

--- http_server/scripts/admin.js
+++ http_server/scripts/admin.js
@@ -1,11 +1,11 @@
-$(function() {
+$(function () {
     if (window.location.pathname.substr(0, 25) === '/admin/update_account.php') {
         // hide pass reset checkbox
         $('label').hide();
 
         // check for a change in email value
         var origEmail = $('input[name="email"]').val();
-        $('input[name="email"]').keyup(function() {
+        $('input[name="email"]').keyup(function () {
             if (origEmail !== $(this).val()) {
                 $('label').show();
             } else {
