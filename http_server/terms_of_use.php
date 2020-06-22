4537b758a9b61a00eded7c8fbe2df05a834f8206
Ben <bls1999@users.noreply.github.com>
2020-06-22 01:36:15 -0400

    Update terms_of_use.php
    formatting, organize into link vars

--- http_server/terms_of_use.php
+++ http_server/terms_of_use.php
@@ -4,17 +4,22 @@ require_once HTTP_FNS . '/output_fns.php';
 
 output_header('Terms of Use');
 
+$rules_link = '<a href="https://pr2hub.com/rules">these rules ("The Rules")</a>';
+$ip_link = '<a href="https://tools.keycdn.com/geo">IP Location Finder by KeyCDN</a>';
+$kong_link = '<a href="https://kongregate.com/">Kongregate</a>';
+$kong_ua_link = '<a href="https://www.kongregate.com/pages/terms">Kongregate\'s User Agreement</a>';
+$kong_feedback_link = '<a href="https://www.kongregate.com/feedbacks/new">contact Kongregate</a>';
+
 echo 'By accessing this site ("PR2 Hub") and/or Platform Racing 2 ("PR2"), '
     .'you are agreeing to be bound by the terms and conditions of this Agreement. '
     .'If you do not agree to the terms of this Agreement, do not use and/or access PR2 Hub and/or PR2.'
     .'<br><br>';
 
-echo 'By accessing PR2 Hub and/or PR2, you agree to be bound by '
-    .'<a href="https://pr2hub.com/rules">these rules ("The Rules")</a>. '
+echo "By accessing PR2 Hub and/or PR2, you agree to be bound by $rules_link. "
     .'Access of PR2 Hub and/or PR2 is defined as using any service related to PR2 '
     .'(including but not limited to logging in, accessing any file or resource on the PR2 Hub Domain ("pr2hub.com"), '
-    .'and purchasing items in the Vault of Magics (in-game store, "The VoM")). '
-    .'Some data may be collected in order to aid enforcement of The Rules, '
+    .'and purchasing items in the Vault of Magics (in-game store, "The VoM")). Some data may be collected and analyzed '
+    ."(courtesy, in part, of $ip_link) in order to aid enforcement of The Rules, "
     .'including (but not limited to) IP addresses. This data is never shared with third parties.<br><br>';
 
 echo 'You agree that failure to comply with The Rules may result in '
@@ -25,10 +30,9 @@ echo 'You agree that failure to comply with The Rules may result in '
     .'created guilds, and private messages).<br><br>';
 
 echo 'The VoM sells premium items that may be rented or bought '
-    .'using digital currency ("Kreds") sold and distributed exclusively by '
-    .'<a href="https://kongregate.com/">Kongregate</a>. '
-    .'For support regarding the purchase and use of Kreds, <a href="https://www.kongregate.com/feedbacks/new">contact '
-    .'Kongregate</a>. Kongregate has its own set of laws governing the purchase and use of Kreds. '
-    .'These laws can be found in <a href="https://www.kongregate.com/pages/terms">Kongregate\'s User Agreement</a>.';
+    ."using digital currency (\"Kreds\") sold and distributed exclusively by $kong_link. "
+    .'Kongregate has its own set of laws governing the purchase and use of Kreds. '
+    ."These laws can be found in $kong_ua_link. "
+    ."For support regarding the purchase and use of Kreds, $kong_feedback_link. ";
 
 output_footer();
