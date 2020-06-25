c3489d88c05cca809e76db2f8e3de0317dfb2d20
Ben <bls1999@users.noreply.github.com>
2020-06-25 05:00:17 -0400

    change fn
    turns out phpcs was already disabled on this line! go figure

--- http_server/ban_user.php
+++ http_server/ban_user.php
@@ -103,7 +103,7 @@ try {
 
     // ban the user
     // phpcs:disable
-    $ban_id = (int) ban_user($pdo, $ban_ip, $ban_uid, $mod_uid, $ends, $reason, $log, $ban_name, $mod_name, $is_ip, $is_acc, $scope);
+    $ban_id = (int) ban_insert($pdo, $ban_ip, $ban_uid, $mod_uid, $ends, $reason, $log, $ban_name, $mod_name, $is_ip, $is_acc, $scope);
     // phpcs:enable
     if ($ban_id === 0) {
         throw new Exception('Could not record ban.');
