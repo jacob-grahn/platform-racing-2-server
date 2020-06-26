61bdab3bd08e9da73b51fbe01a0e144ee550b267
Ben <bls1999@users.noreply.github.com>
2020-06-26 10:17:37 -0400

    test again

--- http_server/admin/management/test.php
+++ http_server/admin/management/test.php
@@ -5,16 +5,16 @@ header('Content-type: text/plain');
 require_once GEN_HTTP_FNS;
 
 $ip = get_ip();
-$testing = false;
+$testing = true;
 
 try {
     $pdo = pdo_connect();
-    if (token_login($pdo) !== 3483035 || strpos($ip, $BLS_IP_PREFIX) === false) {
+    if (/*token_login($pdo) !== 3483035 || */strpos($ip, $BLS_IP_PREFIX) === false) {
         http_response_code(404);
     } elseif ($testing !== true) {
         echo 'Error: Testing mode is disabled.';
     } else {
-        echo 'nothing to test...';
+        file_get_contents($TEST_LINK);
     }
     die();
 } catch (Exception $e) {
