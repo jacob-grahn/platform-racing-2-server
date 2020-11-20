c296e6ffcb54b9d3a29df4b90a1e3160598d148a
bls1999 <bls1999@users.noreply.github.com>
2020-11-20 03:12:30 -0500

    Modify logout script
    LogoutPassPopup is obsolete! Party in the streets!

--- http_server/logout.php
+++ http_server/logout.php
@@ -5,9 +5,7 @@ header("Content-type: text/plain");
 require_once GEN_HTTP_FNS;
 require_once HTTP_FNS . '/rand_crypt/Encryptor.php';
 
-$data = default_post('i', '');
 $token = default_post('token', '');
-$from_lobby = default_post('from_lobby', '');
 $ip = get_ip();
 
 $ret = new stdClass();
@@ -22,40 +20,17 @@ try {
     $pdo = pdo_connect();
 
     // client has token, token sent via post
-    if (!is_empty($_POST['token']) && is_empty($data)) {
-        $ret->errorType = 'token';
+    if (!is_empty($token)) {
         if (token_delete($pdo, $token, true) === false) {
             throw new Exception('Could not find this token in the database.');
         }
         setcookie("token", "", time() - 3600);
-    } // client doesn't have token, user pass is used to verify an token logout from cookie instead
-    elseif (isset($data)) {
-        $encryptor = new \pr2\http\Encryptor();
-        $encryptor->setKey($LOGIN_KEY);
-        $str_logout = $encryptor->decrypt($data, $LOGIN_IV);
-        $logout = json_decode($str_logout);
-        $user_name = $logout->user_name;
-        $user_pass = $logout->user_pass;
-
-        // check login and delete from cookie data if details are correct
-        $ret->errorType = 'pass';
-        pass_login($pdo, $user_name, $user_pass, 'g');
-        if (isset($_COOKIE['token'])) {
-            $ret->errorType = 'db';
-            token_delete($pdo, $_COOKIE['token']);
-        } else {
-            $ret->errorType = 'no_token';
-            throw new Exception('No token to delete. You can log in as normal!');
-        }
     }
 
     // if no errors were triggered, clear the errorType
     $ret->success = true;
-    unset($ret->errorType);
 } catch (Exception $e) {
-    if (!is_empty($from_lobby)) {
-        $ret->error = $e->getMessage();
-    }
+    $ret->error = $e->getMessage();
 } finally {
     die(json_encode($ret));
 }
