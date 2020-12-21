6f5efdc11267ae027404c6f20e57a3df26d64af4
bls1999 <bls1999@users.noreply.github.com>
2020-12-21 06:30:04 -0500

    Make responsive
    Because PayPal docs say so

--- functions/http_fns/output_fns.php
+++ functions/http_fns/output_fns.php
@@ -31,6 +31,8 @@ function output_header($title = '', $staff_nav = false, $show_admin = false)
         ."<html xmlns='http://www.w3.org/1999/xhtml'>"
         ."<head>"
             ."<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />"
+            ."<meta name='viewport' content='width=device-width, initial-scale=1'>"
+            ."<meta http-equiv='X-UA-Compatible' content='IE=edge' />"
             ."<title>PR2 Hub - $title</title>"
             ."<link href='/style/gwibble.css' rel='stylesheet' type='text/css' />"
             ."<link href='/style/pr2hub.css' rel='stylesheet' type='text/css'/>";
