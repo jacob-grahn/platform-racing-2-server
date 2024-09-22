6d7c152f74106099329da46e2443170d111aaae1
Jacob Grahn <jacob@grahn.io>
2024-09-22 03:50:17 -0400

    add process logging

--- functions/multi_fns/client/client_misc_fns.php
+++ functions/multi_fns/client/client_misc_fns.php
@@ -6,6 +6,7 @@ function client_become_process($socket, $data)
 {
     global $PROCESS_PASS, $PROCESS_IP;
     
+    output('Ip is attempting to become process: ' . $socket->ip);
     if ($data === $PROCESS_PASS && ($socket->ip === $PROCESS_IP || $socket->ip === '127.0.0.1')) {
         $socket->process = true;
     }
