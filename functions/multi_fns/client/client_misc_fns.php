ae8a6686f8c1396654e9cdc8f27a6988d1bc4874
Jacob Grahn <jacob@grahn.io>
2024-09-22 04:00:18 -0400

    support more than one process ip

--- functions/multi_fns/client/client_misc_fns.php
+++ functions/multi_fns/client/client_misc_fns.php
@@ -7,7 +7,7 @@ function client_become_process($socket, $data)
     global $PROCESS_PASS, $PROCESS_IP;
     
     output('Ip is attempting to become process: ' . $socket->ip);
-    if ($data === $PROCESS_PASS && ($socket->ip === $PROCESS_IP || $socket->ip === '127.0.0.1')) {
+    if ($data === $PROCESS_PASS && (preg_match($PROCESS_IP, $socket->ip) || $socket->ip === '127.0.0.1')) {
         $socket->process = true;
     }
 }
